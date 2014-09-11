3ec169b38a5ece25471b68953d4d43f03a148dba
Matt Blair <blair1618@gmail.com>
2014-09-11 17:45:19 -0400

    Add comments and convenience functions

--- core/util/shaderProgram.h
+++ core/util/shaderProgram.h
@@ -5,6 +5,10 @@
 #include <vector>
 #include <unordered_map>
 
+/*
+ * ShaderProgram - utility class representing an OpenGL shader program
+ */
+
 class ShaderProgram {
 
 public:
@@ -12,11 +16,34 @@ public:
     ShaderProgram();
     virtual ~ShaderProgram();
 
+    /* Getters */
     GLint getGlProgram() { return m_glProgram; };
+    GLint getGlFragmentShader() { return m_glFragmentShader; };
+    GLint getGlVertexShader() { return m_glVertexShader; };
+
+    /*
+     * getAttribLocation - fetches the location of a shader attribute, caching the result
+     */
     GLint getAttribLocation(const std::string& _attribName);
 
+    /*
+     * buildFromSourceStrings - attempts to compile a fragment shader and vertex shader from
+     * strings representing the source code for each, then links them into a complete program;
+     * if compiling or linking fails it prints the compiler log, returns false, and keeps the
+     * program's previous state; if successful it returns true.
+     */
     bool buildFromSourceStrings(const std::string& _fragSrc, const std::string& _vertSrc);
 
+    // TODO: Once we have file system abstractions, provide a method to build a program from file names
+
+    /*
+     * isValid - returns true if this object represents a valid OpenGL shader program
+     */
+    bool isValid() { return m_glProgram != 0; };
+
+    /*
+     * use - binds the program in openGL if it is not already bound.
+     */
     void use();
 
 private:
