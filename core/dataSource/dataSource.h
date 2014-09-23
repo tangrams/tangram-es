b5c59dc4f826ad458dc4eac3ffb7cb03f0256fdf
Varun Talwar <tallytalwar@gmail.com>
2014-09-23 19:44:52 -0400

    moved static string manipulation func from cpp to header

--- core/dataSource/dataSource.h
+++ core/dataSource/dataSource.h
@@ -9,6 +9,7 @@
 #include <vector>
 #include <map>
 #include <memory>
+#include <sstream>
 
 #include "glm/glm.hpp"
 
@@ -26,10 +27,10 @@ protected:
 
 public:
     virtual bool LoadTile(std::vector<glm::ivec3> _tileCoords) = 0;
-    virtual std::shared_ptr<Json::Value>
-                	GetData(std::string _tileID) = 0;
+    virtual std::shared_ptr<Json::Value> GetData(std::string _tileID) = 0;
     virtual bool CheckDataExists(std::string _tileID) = 0;
     void ClearGeoRoots();
+    size_t JsonRootSize();
     DataSource() {}
     virtual ~DataSource() {
         m_JsonRoots.clear();
@@ -41,8 +42,34 @@ class MapzenVectorTileJson: public DataSource {
 public:
     MapzenVectorTileJson() {}
     virtual bool LoadTile(std::vector<glm::ivec3> _tileCoords) override;
-    virtual std::shared_ptr<Json::Value>
-                GetData(std::string _tileID) override;
-    virtual bool CheckDataExists(std::string _tileID);
+    virtual std::shared_ptr<Json::Value> GetData(std::string _tileID) override;
+    virtual bool CheckDataExists(std::string _tileID) override;
     virtual ~MapzenVectorTileJson() {}
 };
+
+//---- tileID and url construction----
+
+//constructs a string from the tile coodinates
+
+//constructs a mapzen vectortile json url from the tile coordinates
+//TODO: Use regex to do this better.
+static std::unique_ptr<std::string> constructURL(glm::ivec3 _tileCoord) {
+    std::ostringstream strStream;
+    strStream<<"http://vector.mapzen.com/osm/all/"<<_tileCoord.z
+                <<"/"<<_tileCoord.x<<"/"<<_tileCoord.y<<".json";
+    std::unique_ptr<std::string> url(new std::string(strStream.str()));
+    return std::move(url);
+}
+
+//TODO: Use regex to do this better.
+// Hacking to extract id from url
+static std::string extractIDFromUrl(std::string _url) {
+    int x,y,z;
+    std::string baseURL("http://vector.mapzen.com/osm/all/");
+    std::string jsonStr(".json");
+    std::string tmpID = _url.replace(0, baseURL.length(), "");
+    std::size_t jsonPos = tmpID.find(jsonStr);
+    tmpID = tmpID.replace(jsonPos, jsonStr.length(), "");
+    std::replace(tmpID.begin(), tmpID.end(), '/','_');
+    return tmpID;
+}
