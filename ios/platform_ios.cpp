1132e97e6da03b489317c9755bf54622a9455586
Karim Naaji <karim.naaji@gmail.com>
2014-09-23 18:22:50 -0400

    fixed missing files

---
+++ ios/platform_ios.cpp
@@ -0,0 +1,14 @@
+#ifdef PLATFORM_IOS
+
+#include "platform.h"
+
+void logMsg(const char* fmt, ...) {
+
+    va_list args;
+    va_start(args, fmt);
+    vfprintf(stderr, fmt, args);
+    va_end(args);
+
+}
+
+#endif
