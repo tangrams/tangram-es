811a7f09211393fad16ddce8dd73b14b38f787cd
Matt Blair <blair1618@gmail.com>
2014-10-01 14:18:05 -0400

    Add a tileID type to explicitly represent tile quadtree coordinates (this nomenclature needs to be updated in other places too); Change glm types to double precision where appropriate; Map tile geometry to style name strings instead of style objects to prevent excess copies

--- core/mapTile/mapTile.cpp
+++ core/mapTile/mapTile.cpp
@@ -1,10 +1,10 @@
 #include "mapTile.h"
 
-MapTile::MapTile(glm::ivec3 _address, const Projection& _projection) : m_address(_address) {
+MapTile::MapTile(tileID _id, const Projection& _projection) : m_id(_id) {
 
-    glm::vec4 tileBounds = _projection.tileBounds(_address);
-    m_tileOrigin = glm::vec3(tileBounds.x, tileBounds.y);
-    m_modelMatrix = glm::translate(glm::mat4(1.0), tileBounds.x, tileBounds.y, 0); //Use the lower-left corner for the 'model position'
+    glm::dvec4 tileBounds = _projection.tileBounds(_address);
+    m_tileOrigin = glm::dvec2(tileBounds.x, tileBounds.y);
+    m_modelMatrix = glm::translate(glm::dmat4(1.0), tileBounds.x, tileBounds.y, 0.0); //Use the lower-left corner for the 'model position'
 
 }
 
@@ -16,20 +16,20 @@ MapTile::~MapTile() {
 
 void MapTile::addGeometry(const Style& _style, unique_ptr<VboMesh>&& _mesh) {
 
-    m_geometry[_style] = unique_ptr<VboMesh>(_mesh); // Move-construct a unique_ptr at the value associated with the given style
+    m_geometry[_style.getStyleName()] = unique_ptr<VboMesh>(_mesh); // Move-construct a unique_ptr at the value associated with the given style
 
 }
 
-void MapTile::draw(const Style& _style, const glm::mat4& _viewProjMatrix) {
+void MapTile::draw(const Style& _style, const glm::dmat4& _viewProjMatrix) {
 
-    std::unique_ptr<VboMesh> styleMesh = m_geometry[_style];
+    std::unique_ptr<VboMesh> styleMesh = m_geometry[_style.getStyleName()];
 
     if (styleMesh) {
 
         ShaderProgram& shader = _style.getShaderProgram();
 
         glm::mat4 modelViewProjMatrix = m_modelMatrix * _viewProjMatrix;
-        shader.setUniform("u_modelViewProj", modelViewProjMatrix);
+        shader.setUniformMatrix4f("u_modelViewProj", glm::value_ptr(modelViewProjMatrix));
 
         styleMesh->draw(shader);
 
--- core/mapTile/mapTile.h
+++ core/mapTile/mapTile.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "glm/fwd.hpp"
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "util/vboMesh.h"
@@ -11,15 +12,25 @@ class MapTile {
 
 public:
     
-    MapTile(glm::ivec3 _address, const Projection& _projection);
+    MapTile(tileID _id, const Projection& _projection);
 
     virtual ~MapTile();
 
+    struct tileID {
+        
+        tileID(int _x, int _y, int _z) : x(_x) y(_y) z(_z) {};
+
+        const int x;
+        const int y;
+        const int z;
+
+    };
+
     /*
-     * compareTileID provides a strict, weak ordering on 3-component tile IDs; returns true if the first ID precedes the second
+     * tileIDComparator provides a strict, weak ordering on 3-component tile IDs; returns true if the first ID precedes the second
      */
     static struct tileIDComparator {
-        bool operator() (const glm::ivec3& _a, const glm::ivec3& _b) const {
+        bool operator() (const tileID& _a, const tileID& _b) const {
             if ( _a.x != _b.x) {
                 return _a.x < _b.x;
             } else if (_a.y != _b.y) {
@@ -30,22 +41,22 @@ public:
         }
     };
 
-    const glm::ivec3& getAddress() const { return m_address; };
+    const tileID& getID() const { return m_id; };
 
-    const glm::vec2& getTileOrigin() const { return m_tileOrigin; };
+    const glm::dvec2& getTileOrigin() const { return m_tileOrigin; };
 
     void addGeometry(const Style& _style, unique_ptr<VboMesh>&& _mesh);
 
-    void draw(const Style& _style, const glm::mat4& _viewProjMatrix);
+    void draw(const Style& _style, const glm::dmat4& _viewProjMatrix);
 
 private:
 
-    glm::ivec3 m_address;
+    tileID m_id;
 
-    glm::vec2 m_tileOrigin; // Lower-left corner of the tile in 2D projection space in meters (e.g. mercator meters)
+    glm::dvec2 m_tileOrigin; // Lower-left corner of the tile in 2D projection space in meters (e.g. mercator meters)
 
-    glm::mat4 m_modelMatrix; // Translation matrix from world origin to tile origin
+    glm::dmat4 m_modelMatrix; // Translation matrix from world origin to tile origin
 
-    std::unordered_map<Style, std::unique_ptr<VboMesh>> m_geometry;
+    std::unordered_map<std::string, std::unique_ptr<VboMesh>> m_geometry;
 
 };
