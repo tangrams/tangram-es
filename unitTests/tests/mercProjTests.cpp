3cb2c097ed0dbbbad8abb7af6ec65c82a680e86b
Varun Talwar <tallytalwar@gmail.com>
2014-10-01 21:34:55 -0400

    added basic unit tests for mercator projection. More coming..

--- unitTests/tests/mercProjTests.cpp
+++ unitTests/tests/mercProjTests.cpp
@@ -10,10 +10,11 @@
 
 
 TEST_CASE( "Testing some functionality for mercator projection", "[MERCATOR][PROJECTION]" ) {
-    MercProjection mercProjection = MercProjection();
-    glm::dvec2 latLon = glm::dvec2(0.0,0.0);
+    MercatorProjection mercProjection = MercatorProjection();
+    glm::dvec2 lonLat = glm::dvec2(0.0,0.0);
     glm::dvec2 worldCoord = glm::dvec2(0.0, 0.0);
     glm::ivec3 tileCoord = glm::ivec3(2, 2, 3);
+    double epsilon = 0.000000000000000001;
     //check if all the test tileCoordinates are loaded
     /*REQUIRE(dataSource.JsonRootSize() == 3);
     //check if all the test tiles have data in the jsonRoots data structure
@@ -21,7 +22,10 @@ TEST_CASE( "Testing some functionality for mercator projection", "[MERCATOR][PRO
     REQUIRE(dataSource.CheckDataExists("16_19293_24641") == true);
     REQUIRE(dataSource.CheckDataExists("0_0_0") == true);
     REQUIRE(dataSource.CheckDataExists("0_1_0") == false);*/
-    glm::dvec2 testMeters = mercProjection.LatLonToMeters(latLon);
-    glm::dvec2 testMeters2 = mercProjection.MetersToLatLon(worldCoord);
-    printf("%f\t%f", testMeters.x, testMeters.y);
+    glm::dvec2 testMeters = mercProjection.LonLatToMeters(lonLat);
+    glm::dvec2 testLonLat = mercProjection.MetersToLonLat(worldCoord);
+    REQUIRE( (testMeters.x - worldCoord.x) < epsilon);
+    REQUIRE( (testMeters.y - worldCoord.y) < epsilon);
+    REQUIRE( (testLonLat.x - lonLat.x) < epsilon);
+    REQUIRE( (testLonLat.y - lonLat.y) < epsilon);
 }
