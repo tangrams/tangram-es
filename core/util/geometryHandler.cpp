48ccdff01a6de2e813d3d76bd7eb66ff21f1a9da
Varun Talwar <tallytalwar@gmail.com>
2014-10-11 01:15:18 -0400

    implementation of geometryHandler proposed by matt. More types comming soon...

--- core/util/geometryHandler.cpp
+++ core/util/geometryHandler.cpp
@@ -1,6 +1,71 @@
-#include "util/geometryHandler.h"
+#include "geometryHandler.h"
 
-GeometryHandler::GeometryHandler(const MapProjection& _mapProjection) : m_mapProjection(&_mapProjection) {
-    m_tess = tessNewTess(nullptr);
+//static TESStesselator* GeometryHandler::tesselator = nullptr;
+
+void GeometryHandler::init() {
+    // might not need this, can just check if tesselator is valid in each context that uses it
+    if (tesselator == nullptr) {
+        //create a tesselator with null allocator (default implementation as per libtess2)
+        tesselator = tessNewTess(nullptr);
+    }
 }
 
+void GeometryHandler::cleanup() {
+    if (tesselator != nullptr) {
+        tessDeleteTess(tesselator);
+    }
+}
+
+void GeometryHandler::buildPolygon(const std::vector<glm::vec3>& _pointsIn, const std::vector<int>& _ringSizes, std::vector<glm::vec3>& _pointsOut, std::vector<glm::vec3>& _normalOut, std::vector<GLushort>& _indicesOut) {
+    
+    //Get the number of vertices already added
+    size_t vertexDataOffset = _pointsOut.size();
+    
+    // add polygon contour for every ring
+    for(int i = 0; i < _ringSizes.size(); i++) {
+        int ringIndex;
+        if(i == 0) {
+            ringIndex = 0;
+        }
+        else {
+            ringIndex = _ringSizes[i-1];
+        }
+        tessAddContour(tesselator, 3, &_pointsIn[ringIndex].x, sizeof(glm::vec3), _ringSizes[i]);
+    }
+
+    //call the tesselator
+    if( tessTesselate(tesselator, TessWindingRule::TESS_WINDING_NONZERO, TessElementType::TESS_POLYGONS, 3, 3, nullptr) ) {
+
+        const int numIndices = tessGetElementCount(tesselator);
+        const TESSindex* tessIndices = tessGetElements(tesselator);
+        for(int i = 0; i < numIndices; i++) {
+            const TESSindex* tessIndex = &tessIndices[i * 3];
+            for(int j = 0; j < 3; j++) {
+                _indicesOut.push_back(GLushort(tessIndex[j] + vertexDataOffset));
+            }
+        }
+
+        const int numVertices = tessGetVertexCount(tesselator);
+        const float* tessVertices = tessGetVertices(tesselator);
+        for(int i = 0; i < numVertices; i++) {
+            logMsg("\tTesselated vertex: (%f, %f, %f)\n", tessVertices[3*i], tessVertices[3*i+1], tessVertices[3*i+2]);
+            _pointsOut.push_back(glm::vec3(tessVertices[3*i], tessVertices[3*i+1], tessVertices[3*i+2]));
+            _normalOut.push_back(glm::vec3(0.0, 0.0, 1.0));
+        }
+    }
+    else {
+        logMsg("\t\t***tessTesselate returns false. Can not tesselate.****\n");
+    }
+}
+
+void GeometryHandler::buildPolygonExtrusion(const std::vector<glm::vec3>& _pointsIn, const std::vector<int>& _ringSizes, std::vector<glm::vec3>& _pointsOut, std::vector<glm::vec3>& _normalOut, std::vector<GLushort>& _indicesOut) {
+
+}
+
+void GeometryHandler::buildPolyLine(const std::vector<glm::vec3>& _pointsIn, float width, std::vector<glm::vec3>& _pointsOut, std::vector<GLushort>& _indicesOut) {
+
+}
+
+void GeometryHandler::buildQuadAtPoint(const glm::vec3& _pointIn, const glm::vec3& _normal, float width, float height, std::vector<glm::vec3>& _pointsOut, std::vector<GLushort>& _indicesOut) {
+
+}
--- core/util/geometryHandler.h
+++ core/util/geometryHandler.h
@@ -2,143 +2,31 @@
 
 #include <vector>
 
-#include "json/json.h"
-#include "tesselator.h"
+#include "glm/fwd.hpp"
 #include "glm/glm.hpp"
+#include "tesselator.h"
 
-#include "util/projection.h"
 #include "platform.h"
 
+namespace GeometryHandler {
 
-class GeometryHandler {
-    TESStesselator* m_tess;
-    const MapProjection* m_mapProjection;
+    static TESStesselator* tesselator = nullptr;
 
-public:
-    GeometryHandler(const MapProjection& m_mapProjection);
+    void init();
 
-    template<typename T>
-    void polygonAddData(const Json::Value& _geomCoordinates, std::vector<T>& _vertices, std::vector<GLushort>& _indices, const GLuint _abgr, const glm::dvec2& _tileOffset, double _height, double _minHeight);
+    void cleanup();
 
-    virtual ~GeometryHandler() {
-        tessDeleteTess(m_tess);
-    }
-};
+    /*
+     * _pointsIn: set of rings from jsonCoordinates or other formats.
+     * _pointsOut: a vector of tesselated vertex coordinates
+     * _normalsOut: 
+     */
+    void buildPolygon(const std::vector<glm::vec3>& _pointsIn, const std::vector<int>& _ringSizes, std::vector<glm::vec3>& _pointsOut, std::vector<glm::vec3>& _normalsOut, std::vector<GLushort>& _indicesOut);
 
-// defining template member function
-template<typename T>
-void GeometryHandler::polygonAddData(const Json::Value& _geomCoordinates, std::vector<T>& _vertices, std::vector<GLushort>& _indices, const GLuint _abgr, const glm::dvec2& _tileOffset, double _height, double _minHeight) {
-    //Get the size of the data already added
-    size_t vertexDataOffset = _vertices.size();
+    void buildPolygonExtrusion(const std::vector<glm::vec3>& _pointsIn, const std::vector<int>& _ringSizes, std::vector<glm::vec3>& _pointsOut, std::vector<glm::vec3>& _normalsOut, std::vector<GLushort>& _indicesOut);
+
+    void buildPolyLine(const std::vector<glm::vec3>& _pointsIn, float width, std::vector<glm::vec3>& _pointsOut, std::vector<GLushort>& _indicesOut);
+
+    void buildQuadAtPoint(const glm::vec3& _pointIn, const glm::vec3& _normal, float width, float height, std::vector<glm::vec3>& _pointsOut, std::vector<GLushort>& _indicesOut);
     
-    // Iterate through rings to setup the tesselator
-    for(int i = 0; i < _geomCoordinates.size(); i++) {
-        //extract coordinates and transform to merc (x,y) using m_mapProjection
-        int ringSize = _geomCoordinates[0].size();
-        std::vector<glm::vec3> ringCoords;
-
-        for(int j = 0; j < ringSize; j++) {
-            glm::dvec2 tmp = m_mapProjection->LonLatToMeters(glm::dvec2(_geomCoordinates[0][j][0].asFloat(), _geomCoordinates[0][j][1].asFloat()));
-            glm::dvec2 meters = tmp - _tileOffset;
-            ringCoords.push_back(glm::vec3(meters.x, meters.y, _height));
-        }
-
-        // Extrude poly
-        if(_height != _minHeight) {
-            glm::vec3 upVector(0.0f,0.0f,1.0f);
-            glm::vec3 normalVector;
-
-            for(int j = 0; j < (ringSize-1); j++) {
-                normalVector = glm::cross(upVector, ringCoords[i+1] - ringCoords[i]);
-                /*
-                 * NOTE: Converting double to floats for the data to be sent to GPU
-                 */
-                // 1st vertex top
-                _vertices.push_back((T){
-                                        static_cast<GLfloat>(ringCoords[i].x), 
-                                        static_cast<GLfloat>(ringCoords[i].y), 
-                                        static_cast<GLfloat>(ringCoords[i].z), 
-                                        static_cast<GLfloat>(normalVector.x), 
-                                        static_cast<GLfloat>(normalVector.y), 
-                                        static_cast<GLfloat>(normalVector.z), 
-                                        _abgr
-                                      });
-                // 2nd vertex top
-                _vertices.push_back((T){
-                                        static_cast<GLfloat>(ringCoords[i+1].x), 
-                                        static_cast<GLfloat>(ringCoords[i+1].y), 
-                                        static_cast<GLfloat>(ringCoords[i+1].z), 
-                                        static_cast<GLfloat>(normalVector.x), 
-                                        static_cast<GLfloat>(normalVector.y), 
-                                        static_cast<GLfloat>(normalVector.z), 
-                                        _abgr
-                                      });
-                // 1st vertex bottom
-                _vertices.push_back((T){
-                                        static_cast<GLfloat>(ringCoords[i].x), 
-                                        static_cast<GLfloat>(ringCoords[i].y), 
-                                        static_cast<GLfloat>(_minHeight), 
-                                        static_cast<GLfloat>(normalVector.x), 
-                                        static_cast<GLfloat>(normalVector.y), 
-                                        static_cast<GLfloat>(normalVector.z),
-                                        _abgr
-                                      });
-                // 2nd vertex bottom
-                _vertices.push_back((T){
-                                        static_cast<GLfloat>(ringCoords[i+1].x), 
-                                        static_cast<GLfloat>(ringCoords[i+1].y), 
-                                        static_cast<GLfloat>(_minHeight), 
-                                        static_cast<GLfloat>(normalVector.x), 
-                                        static_cast<GLfloat>(normalVector.y), 
-                                        static_cast<GLfloat>(normalVector.z), 
-                                        _abgr
-                                      });
-                
-                //Start the index from the previous state of the vertex Data
-                _indices.push_back(vertexDataOffset);
-                _indices.push_back(vertexDataOffset + 2);
-                _indices.push_back(vertexDataOffset + 1);
-
-                _indices.push_back(vertexDataOffset + 1);
-                _indices.push_back(vertexDataOffset + 2);
-                _indices.push_back(vertexDataOffset + 3);
-
-                vertexDataOffset += 4;
-            }
-        }
-        //Setup the tesselator
-        tessAddContour(m_tess, 3, &ringCoords[0].x, sizeof(glm::vec3), ringSize);
-    }
-
-    //Call the tesselator to tesselate polygon into triangles
-    if( tessTesselate(m_tess, TessWindingRule::TESS_WINDING_NONZERO, TessElementType::TESS_POLYGONS, 3, 3, nullptr)) {
-        const int numIndices = tessGetElementCount(m_tess);
-        const TESSindex* tessIndices = tessGetElements(m_tess);
-
-        for(int i = 0; i < numIndices; i++) {
-            const TESSindex* tessIndex = &tessIndices[i * 3];
-            for(int j = 0; j < 3; j++) {
-                _indices.push_back(GLushort(tessIndex[j]) + vertexDataOffset);
-            }
-        }
-
-        const int numVertices = tessGetVertexCount(m_tess);
-        const float* tessVertices = tessGetVertices(m_tess);
-
-        
-        for(int i = 0; i < numVertices; i++) {
-            logMsg("\tTesselated vertex: (%f,%f,%f)\n", tessVertices[3*i], tessVertices[3*i+1], tessVertices[3*i+2]);
-            _vertices.push_back((T){
-                                    tessVertices[3*i],
-                                    tessVertices[3*i+1],
-                                    tessVertices[3*i+2],
-                                    0.0f, 0.0f, 1.0f,
-                                    _abgr
-                                   });
-        }
-    }
-    else {
-        logMsg("\t\t****tessTessalate returns false. Can not tesselate.****\n");
-    }
 }
-
