d531a67e91e9585608deef5cf7d4bbcfb11598b3
Varun Talwar <tallytalwar@gmail.com>
2014-10-16 18:20:34 -0400

    fixed tests based on changes done to dataSource

--- unitTests/tests/curlTests.cpp
+++ unitTests/tests/curlTests.cpp
@@ -3,36 +3,40 @@
 #include <iostream>
 
 #include "dataSource/dataSource.h"
+#include "mapTile/mapTile.h"
 #include "glm/glm.hpp"
+#include "util/tileID.h"
 
 
 TEST_CASE( "URL Name Check for MapzenVectorTileJson", "[CURL][DataSource][MapzenVectorTileJson]" ) {
-    glm::ivec3 tileCoord = glm::ivec3(0,0,0);
-    REQUIRE( *constructURL(tileCoord) == "http://vector.mapzen.com/osm/all/0/0/0.json");
-    tileCoord = glm::ivec3(19293,24641,16);
-    REQUIRE( *constructURL(tileCoord) == "http://vector.mapzen.com/osm/all/16/19293/24641.json");
-    tileCoord = glm::ivec3(19293,24641,14);
-    REQUIRE( *constructURL(tileCoord) == "http://vector.mapzen.com/osm/all/14/19293/24641.json");
+    MapzenVectorTileJson dataSource;
+    TileID tileCoord = TileID(0,0,0);
+    REQUIRE( *dataSource.constructURL(tileCoord) == "http://vector.mapzen.com/osm/all/0/0/0.json");
+    TileID tileCoord2 = TileID(19293,24641,16);
+    REQUIRE( *dataSource.constructURL(tileCoord2) == "http://vector.mapzen.com/osm/all/16/19293/24641.json");
+    TileID tileCoord3 = TileID(19293,24641,14);
+    REQUIRE( *dataSource.constructURL(tileCoord3) == "http://vector.mapzen.com/osm/all/14/19293/24641.json");
 }
 
 TEST_CASE( "Extract tile coordinates from URL check for MapzenVectorTileJson", "[CURL][DataSource][MapzenVectorTileJson]" ) {
-    REQUIRE( extractIDFromUrl("http://vector.mapzen.com/osm/all/16/19293/24641.json") == "16_19293_24641" );
-    REQUIRE( extractIDFromUrl("http://vector.mapzen.com/osm/all/0/0/0.json") == "0_0_0" );
-    REQUIRE( extractIDFromUrl("http://vector.mapzen.com/osm/all/14/19293/24641.json") == "14_19293_24641" );
+    MapzenVectorTileJson dataSource;
+    REQUIRE( dataSource.extractIDFromUrl("http://vector.mapzen.com/osm/all/16/19293/24641.json") == TileID(19293, 24641, 16) );
+    REQUIRE( dataSource.extractIDFromUrl("http://vector.mapzen.com/osm/all/0/0/0.json") == TileID(0,0,0) );
+    REQUIRE( dataSource.extractIDFromUrl("http://vector.mapzen.com/osm/all/14/19293/24641.json") == TileID(19293, 24641, 14) );
 }
 
 TEST_CASE( "MapzenVectorTileJson::LoadTile check", "[CURL][DataSource][MapzenVectorTileJson]" ) {
     MapzenVectorTileJson dataSource;
-    std::vector<glm::ivec3> tileCoords;
-    tileCoords.push_back(glm::ivec3(19293,24641,16));
-    tileCoords.push_back(glm::ivec3(19293,24641,14));
-    tileCoords.push_back(glm::ivec3(0,0,0));
+    std::vector<TileID> tileCoords;
+    tileCoords.push_back(TileID(19293,24641,16));
+    tileCoords.push_back(TileID(19293,24641,14));
+    tileCoords.push_back(TileID(0,0,0));
     dataSource.LoadTile(tileCoords);
     //check if all the test tileCoordinates are loaded
     REQUIRE(dataSource.JsonRootSize() == 3);
     //check if all the test tiles have data in the jsonRoots data structure
-    REQUIRE(dataSource.CheckDataExists("14_19293_24641") == true);
-    REQUIRE(dataSource.CheckDataExists("16_19293_24641") == true);
-    REQUIRE(dataSource.CheckDataExists("0_0_0") == true);
-    REQUIRE(dataSource.CheckDataExists("0_1_0") == false);
+    REQUIRE(dataSource.CheckDataExists(TileID(19293, 24641, 14)) == true);
+    REQUIRE(dataSource.CheckDataExists(TileID(19293, 24641, 16)) == true);
+    REQUIRE(dataSource.CheckDataExists(TileID(0, 0, 0)) == true);
+    REQUIRE(dataSource.CheckDataExists(TileID(0, 1, 0)) == false);
 }
