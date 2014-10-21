32816e50edc0196812062636db14a3b1f8549a93
Matt Blair <blair1618@gmail.com>
2014-10-21 16:00:40 -0400

    Change OSX build configuration to use glfw3 as an external dependency

--- build/toolchains/darwin.cmake
+++ build/toolchains/darwin.cmake
@@ -5,15 +5,6 @@ set(EXECUTABLE_NAME "tangram.out")
 
 add_definitions(-DPLATFORM_OSX)
 
-# load glfw3 library
-option(GLFW_BUILD_EXAMPLES OFF)
-option(GLFW_BUILD_TESTS OFF)
-option(GLFW_BUILD_DOCS OFF)
-option(GLFW_INSTALL OFF)
-
-add_subdirectory(${PROJECT_SOURCE_DIR}/osx/lib/glfw3)
-include_directories(${PROJECT_SOURCE_DIR}/osx/lib/glfw3/include)
-
 # load core library
 include_directories(${PROJECT_SOURCE_DIR}/core/include/)
 add_subdirectory(${PROJECT_SOURCE_DIR}/core)
@@ -26,8 +17,21 @@ find_sources_and_include_directories(
 
 # link and build functions
 function(link_libraries)
-    check_and_link_libraries(${EXECUTABLE_NAME} curl OpenGL)
-    target_link_libraries(${EXECUTABLE_NAME} glfw core ${GLFW_LIBRARIES})
+    find_library(OPENGL_FRAMEWORK OpenGL)
+    find_library(COCOA_FRAMEWORK Cocoa)
+    find_library(IOKIT_FRAMEWORK IOKit)
+    find_library(CORE_FOUNDATION_FRAMEWORK CoreFoundation)
+    find_library(CORE_VIDEO_FRAMEWORK CoreVideo)
+    
+    list(APPEND GLFW_LIBRARY -lglfw3
+                             ${OPENGL_FRAMEWORK}
+                             ${COCOA_FRAMEWORK}
+                             ${IOKIT_FRAMEWORK}
+                             ${CORE_FOUNDATION_FRAMEWORK}
+                             ${CORE_VIDEO_FRAMEWORK})
+
+    check_and_link_libraries(${EXECUTABLE_NAME} curl)
+    target_link_libraries(${EXECUTABLE_NAME} ${GLFW_LIBRARY} core)
 endfunction()
 
 function(build) 
