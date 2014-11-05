883c08fe6a393c96bc034dabcdcb13b66e040505
Matt Blair <blair1618@gmail.com>
2014-11-05 18:31:41 -0500

    Document platform functions

--- core/src/platform.h
+++ core/src/platform.h
@@ -21,6 +21,16 @@ void setAssetManager(JNIEnv* _jniEnv, jobject _assetManager);
 
 #include <string>
 
+/* Print a formatted message to the console
+ *
+ * Uses printf syntax to write a string to stderr (or logcat, on Android)
+ */
 void logMsg(const char* fmt, ...);
 
+/* Read a bundled resource file as a string
+ * 
+ * Opens the file at the given relative path and returns a string with its contents.
+ * _path is the location of the file within the core/resources folder. If the file
+ * cannot be found or read, the returned string is empty. 
+ */
 std::string stringFromResource(const char* _path);
