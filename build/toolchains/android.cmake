8f94278c29d87023fde2486783b358dd88b36550
Matt Blair <blair1618@gmail.com>
2014-11-04 22:48:03 -0500

    Add linker flag to android system libraries, for AssetManager

--- build/toolchains/android.cmake
+++ build/toolchains/android.cmake
@@ -9,7 +9,7 @@ else()
 endif()
 
 # configurations
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -std=c++11 -pedantic -llog -lz")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -std=c++11 -pedantic -llog -lz -landroid")
 set(CXX_FLAGS_DEBUG "${CXX_FLAGS_DEBUG} -g -O0")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -pedantic -fPIC")
 set(LIBCURL_PRECOMPILED_LIB ${CMAKE_SOURCE_DIR}/android/jni/precompiled/${ANDROID_ABI}/libcurl.a)
