61cbc20fd07017accb7f354c774ea47b7bf62466
Varun Talwar <tallytalwar@gmail.com>
2014-11-10 10:09:01 -0500

    Added gesture recognizers to the view:     tap     doubleTap (as tap with 2 tap set)     pan     pinch

--- ios/src/ViewController.mm
+++ ios/src/ViewController.mm
@@ -8,13 +8,23 @@
 
 #import "ViewController.h"
 
-@interface ViewController () {
+#define GLM_FORCE_RADIANS
+#include <glm/fwd.hpp>
+#include <glm/glm.hpp>
 
+#include "util/gestures.h"
+
+@interface ViewController () {
+    
 }
 @property (strong, nonatomic) EAGLContext *context;
 
 - (void)setupGL;
 - (void)tearDownGL;
+- (void)respondToTapGesture:(UITapGestureRecognizer *)tapRecognizer;
+- (void)respondToDoubleTapGesture:(UITapGestureRecognizer *)doubleTapRecognizer;
+- (void)respondToPanGesture:(UIPanGestureRecognizer *)panRecognizer;
+- (void)respondToPinchGesture:(UIPanGestureRecognizer *)pinchRecognizer;
 
 @end
 
@@ -34,7 +44,65 @@
     view.context = self.context;
     view.drawableDepthFormat = GLKViewDrawableDepthFormat24;
     
+    /* Construct Gesture Recognizers */
+    //1. Tap
+    UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc]
+                                             initWithTarget:self action:@selector(respondToTapGesture:)];
+    tapRecognizer.numberOfTapsRequired = 1;
+    // TODO: Figure a way to have a delay set for it not to tap gesture not to wait long enough for a doubletap gesture to be recognized
+    tapRecognizer.delaysTouchesEnded = NO;
+    
+    //2. DoubleTap
+    UITapGestureRecognizer *doubleTapRecognizer = [[UITapGestureRecognizer alloc]
+                                             initWithTarget:self action:@selector(respondToDoubleTapGesture:)];
+    doubleTapRecognizer.numberOfTapsRequired = 2;
+    // Distanle single tap when double tap occurs
+    [tapRecognizer requireGestureRecognizerToFail:doubleTapRecognizer];
+    
+    //3. Pan
+    UIPanGestureRecognizer *panRecognizer = [[UIPanGestureRecognizer alloc]
+                                            initWithTarget:self action:@selector(respondToPanGesture:)];
+    
+    //4. Pinch
+    UIPinchGestureRecognizer *pinchRecognizer = [[UIPinchGestureRecognizer alloc]
+                                                 initWithTarget:self action:@selector(respondToPinchGesture:)];
+    
+    
+    /* Setup gesture recognizers */
+    [self.view addGestureRecognizer:tapRecognizer];
+    [self.view addGestureRecognizer:doubleTapRecognizer];
+    [self.view addGestureRecognizer:panRecognizer];
+    [self.view addGestureRecognizer:pinchRecognizer];
+    
     [self setupGL];
+    
+}
+
+- (void)respondToTapGesture:(UITapGestureRecognizer *)tapRecognizer {
+    // Get the location of the tap
+    CGPoint location = [tapRecognizer locationInView:self.view];
+    CGPoint viewCenter = tapRecognizer.view.center;
+    //handleGestures(Tangram::Gesturestures::Invalid);
+    handleGestures(Tangram::Gestures::Tap, glm::vec2(location.x - viewCenter.x, -(location.y - viewCenter.y)));
+}
+
+- (void)respondToDoubleTapGesture:(UITapGestureRecognizer *)doubleTapRecognizer {
+    //Get the location of the double tap
+    CGPoint location = [doubleTapRecognizer locationInView:self.view];
+    handleGestures(Tangram::Gestures::DoubleTap, glm::vec2(location.x, location.y));
+}
+
+- (void)respondToPanGesture:(UIPanGestureRecognizer *)panRecognizer {
+    //velocity is relative to previous drag location
+    //not using drag position as its always relative to the initial touch point
+    CGPoint velocity = [panRecognizer velocityInView:self.view];
+    handleGestures(Tangram::Gestures::Pan, glm::vec2(velocity.x, velocity.y));
+}
+
+- (void)respondToPinchGesture:(UIPinchGestureRecognizer *)pinchRecognizer {
+    CGPoint location = [pinchRecognizer locationInView:self.view];
+    CGFloat scale = pinchRecognizer.scale;
+    handleGestures(Tangram::Gestures::Pinch, glm::vec2(location.x, location.y), scale);
 }
 
 - (void)dealloc
