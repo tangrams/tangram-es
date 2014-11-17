d3efd0a078d7b589490faafdc74c6a11197ec219
Varun Talwar <tallytalwar@gmail.com>
2014-11-17 16:24:46 -0500

    renamed width to halfWidth

--- core/src/style/style.cpp
+++ core/src/style/style.cpp
@@ -104,9 +104,9 @@ void PolygonStyle::buildLine(Line& _line, std::string& _layer, Properties& _prop
     std::vector<glm::vec3> points;
     
     GLuint abgr = 0xff969696; // Default road color
-    float width = 0.005;
+    float halfWidth = 0.03;
     
-    GeometryHandler::buildPolyLine(_line, width, points, indices);
+    GeometryHandler::buildPolyLine(_line, halfWidth, points, indices);
     
     for (int i = 0; i < points.size(); i++) {
         glm::vec3 p = points[i];
