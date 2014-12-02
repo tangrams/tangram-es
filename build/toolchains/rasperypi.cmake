19262ef07b423c0c2b327946da01e3fac59a1972
Karim Naaji <karim.naaji@gmail.com>
2014-12-02 09:38:18 -0500

    raspery pi build cmake toolchain

---
+++ build/toolchains/rasperypi.cmake
@@ -0,0 +1,41 @@
+# options
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -stdlib=libc++ -std=c++11")
+set(CXX_FLAGS_DEBUG "-g -O0")
+set(EXECUTABLE_NAME "tangram")
+
+add_definitions(-DPLATFORM_RPI)
+
+# include headers for rpi-installed libraries
+include_directories(/opt/vc/include/)
+
+# load core library
+include_directories(${PROJECT_SOURCE_DIR}/core/include/)
+include_directories(${PROJECT_SOURCE_DIR}/core/include/jsoncpp/)
+add_subdirectory(${PROJECT_SOURCE_DIR}/core)
+include_recursive_dirs(${PROJECT_SOURCE_DIR}/core/src/*.h)
+
+# add sources and include headers
+set(OSX_EXTENSIONS_FILES *.mm *.cpp)
+foreach(_ext ${OSX_EXTENSIONS_FILES})
+    find_sources_and_include_directories(
+        ${PROJECT_SOURCE_DIR}/rpi/src/*.h 
+        ${PROJECT_SOURCE_DIR}/rpi/src/${_ext})
+endforeach()
+
+# link and build functions
+function(link_libraries)
+
+    target_link_libraries(${EXECUTABLE_NAME} -lEGL)
+    target_link_libraries(${EXECUTABLE_NAME} -lGLESv2)
+    target_link_libraries(${EXECUTABLE_NAME} -lbcm_host)
+    target_link_libraries(${EXECUTABLE_NAME} -lcurl) #use system libcurl
+    target_link_libraries(${EXECUTABLE_NAME} ${PROJECT_SOURCE_DIR}/rpi/precompiled/libtess2/libtess2.a)
+    target_link_libraries(${EXECUTABLE_NAME} core)
+
+endfunction()
+
+function(build) 
+
+    add_executable(${EXECUTABLE_NAME} MACOSX_BUNDLE ${SOURCES} ${RESOURCES})
+
+endfunction()
