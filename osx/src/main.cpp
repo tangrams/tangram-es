fa9671c3392a39a7cc8bc23f6e69df49edd3e17b
Matt Blair <blair1618@gmail.com>
2014-12-18 15:58:37 -0500

    Minor cleanup and one minor fix
    - Panning was ignoring the first frame of movement; this (very small) discrepancy is fixed
    - Add and re-word comments
    - Shed some whitespace

--- osx/src/main.cpp
+++ osx/src/main.cpp
@@ -10,39 +10,35 @@
 const double double_tap_time = 0.5; // seconds
 
 bool was_panning = false;
-double last_mouse_up = -double_tap_time;
-
-void window_size_callback(GLFWwindow* window, int width, int height) {
-    
-    Tangram::resize(width, height);
-
-}
+double last_mouse_up = -double_tap_time; // First click should never trigger a double tap
+double last_x_down = 0.0;
+double last_y_down = 0.0;
 
 void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
     
-    if (button != GLFW_MOUSE_BUTTON_1 || action != GLFW_RELEASE) {
-        return; // Stop here because this is a mouse event that we don't care about
+    if (button != GLFW_MOUSE_BUTTON_1) {
+        return; // This event is for a mouse button that we don't care about
     }
     
     if (was_panning) {
-        
         was_panning = false;
-        return; // Stop here so this isn't counted as the first tap in a double-tap
-        
+        return; // Clicks with movement don't count as taps
     }
     
     double x, y;
     glfwGetCursorPos(window, &x, &y);
     double time = glfwGetTime();
     
+    if (action == GLFW_PRESS) {
+        last_x_down = x;
+        last_y_down = y;
+        return;
+    }
+    
     if (time - last_mouse_up < double_tap_time) {
-        
         Tangram::handleDoubleTapGesture(x, y);
-        
     } else {
-        
         Tangram::handleTapGesture(x, y);
-        
     }
     
     last_mouse_up = time;
@@ -51,28 +47,17 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 
 void cursor_pos_callback(GLFWwindow* window, double x, double y) {
     
-    static double last_x = 0.0;
-    static double last_y = 0.0;
-    
     int action = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_1);
     
     if (action == GLFW_PRESS) {
         
-        if (!was_panning) {
-            
-            // TODO: This ignores the first frame of panning movement; to correct this we
-            // would need to track the cursor position whenever the mouse button is pressed
-            was_panning = true;
-            
-        } else {
-            
-            Tangram::handlePanGesture(x - last_x, y - last_y);
-            
+        if (was_panning) {
+            Tangram::handlePanGesture(x - last_x_down, y - last_y_down);
         }
         
-        last_x = x;
-        last_y = y;
-        
+        was_panning = true;
+        last_x_down = x;
+        last_y_down = y;
     }
     
 }
@@ -87,11 +72,19 @@ void scroll_callback(GLFWwindow* window, double scrollx, double scrolly) {
         
         double x, y;
         glfwGetCursorPos(window, &x, &y);
-        
         Tangram::handlePinchGesture(x, y, scrolled > 0 ? 1.5 : 0.5);
-        
         scrolled = 0.0;
+        
     }
+}
+
+// Window handling
+// ===============
+
+void window_size_callback(GLFWwindow* window, int width, int height) {
+    
+    Tangram::resize(width, height);
+    
 }
 
 // Main program
