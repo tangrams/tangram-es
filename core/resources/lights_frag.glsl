ca135a8fcabfd42043673613d432a00539ae972e
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2014-12-23 15:13:28 -0700

    FIXED- vert/frag lights working

---
+++ core/resources/lights_frag.glsl
@@ -0,0 +1,39 @@
+varying vec4 v_amb;
+varying vec4 v_diff;
+varying vec4 v_spec;
+
+vec4 calculateLighting(in vec3 _eyeToPoint, in vec3 _normal) {
+	vec3 eye = vec3(0.0, 0.0, 1.0);
+
+	vec4 amb = v_amb;
+	vec4 diff = v_diff;
+	vec4 spec = v_spec;
+
+//  COMPUTE ALL LIGHTS
+#pragma tangram: lights_to_compute
+
+	//  Final light intensity calculation
+	//
+	vec4 color = vec4(0.0);
+  
+	#ifdef MATERIAL_EMISSION
+	color = g_material.emission;
+	#endif
+
+	#ifdef MATERIAL_AMBIENT
+	color += amb * g_material.ambient;
+	#endif
+
+	#ifdef MATERIAL_DIFFUSE
+	color += diff * g_material.diffuse;
+	#endif
+
+	#ifdef MATERIAL_SPECULAR
+	color += spec * g_material.specular;
+	#endif
+
+	//  For the moment no alpha light (weird concept... right?)
+	color.a = 1.0;
+
+	return color;
+}
---
+++ core/resources/lights_vert.glsl
@@ -0,0 +1,31 @@
+varying vec4 v_amb;
+varying vec4 v_diff;
+varying vec4 v_spec;
+
+void calculateLighting(in vec3 _eyeToPoint, in vec3 _normal) {
+	vec3 eye = vec3(0.0, 0.0, 1.0);
+
+	// Light intensity accumulators
+	vec4 amb  = vec4(0.0);
+	vec4 diff = vec4(0.0);
+	vec4 spec = vec4(0.0);
+
+	v_amb	= vec4(0.0);
+	v_diff 	= vec4(0.0);
+	v_spec 	= vec4(0.0);
+
+//  COMPUTE ALL LIGHTS
+#pragma tangram: lights_to_compute
+
+	#ifdef MATERIAL_AMBIENT
+	v_amb = amb * g_material.ambient;
+	#endif
+
+	#ifdef MATERIAL_DIFFUSE
+	v_diff = diff * g_material.diffuse;
+	#endif
+
+	#ifdef MATERIAL_SPECULAR
+	v_spec = spec * g_material.specular;
+	#endif
+}
