b8c9d1d5a07eb316d7e1e81d26b28ffa8518dac8
Varun Talwar <tallytalwar@gmail.com>
2015-01-02 17:59:36 -0500

    Multiple updates to travis build system
    - Builds for osx and ios
    - Running of tests
    - Explicit shell scripts for builds and excecution of tests

---
+++ travis/script_build_osx.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+if [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
+    # Build osx project
+    echo "Building osx project"
+    make osx
+
+    # Build ios project
+    echo "Building ios project (simulator)"
+    make ios
+fi
+
--- travis/script_build_tests.sh
+++ travis/script_build_tests.sh
@@ -4,5 +4,7 @@ set -e
 set -o pipefail
 
 if [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
+    # Build unit tests
     make tests
 fi
+
