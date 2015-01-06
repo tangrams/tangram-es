f7042e86e760c389e9e564a9593d2d3a759e35c3
Varun Talwar <tallytalwar@gmail.com>
2015-01-06 13:35:22 -0500

    Travis Updates:
    - added android build on linux platform
        - updated before_install shell script to download, install android sdk and ndk and setup linux environment
        appropriately
    - Made all shell scripts executable

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -3,6 +3,11 @@
 set -e
 set -o pipefail
 
+if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
+    ANDROID_SDK_VERSION = "r24.0.2"
+    ANDROID_NDK_VERSION = "r10b"
+fi
+
 if [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
     brew update
     brew tap homebrew/versions
@@ -10,3 +15,27 @@ if [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
 fi
 
 
+if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
+    sudo apt-get update -y
+    sudo apt-get -y install gcc-4.8 g++-4.8
+    sudo apt-get -y install git build-essential automake gdb libtool xutils-dev make cmake pkg-config libcurl4-openssl-dev
+    # not installing mesa and opengl right now
+
+    # install jdk
+    sudo apt-get -qq -y install openjdk-8-jdk ant lib32z1-dev lib32stdc++6
+
+    # install android sdk
+    wget https://dl-ssl.google.com/android/android-sdk_${ANDROID_SDK_VERSION}-linux.tgz
+    tar -zxf android-sdk_${ANDROID_SDK_VERSION}-linux.tgz
+    export ANDROID_HOME=$PWD/android-sdk-linux
+    # Install required Android components.
+    android update sdk --filter platform-tools,android-19,extra-android-support --no-ui --force
+
+    # install android ndk
+    wget http://dl.google.com/android/ndk/android-ndk-${ANDROID_NDK_VERSION}-linux-x86_64.tar.bz2
+    tar -zxf android-ndk-${ANDROID_NDK_VERSION}-linux-x86_64.tar.bz2
+    export ANDROID_NDK=$PWD/android-ndk-${ANDROID_NDK_VERSION}
+
+    export PATH=${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools:${ANDROID_NDK}
+    echo $PATH
+fi
--- travis/script_build.sh
+++ travis/script_build.sh
@@ -13,3 +13,9 @@ if [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
     make ios
 fi
 
+if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
+    # Build android project
+    echo "Building android project"
+    make android
+fi
+
