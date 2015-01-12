39bb7e8eab415a2d4bb4e6b94f5dff125642e078
Matt Blair <blair1618@gmail.com>
2015-01-12 17:39:12 -0500

    GLM_FORCE_RADIANS no longer needed

--- core/src/util/geom.h
+++ core/src/util/geom.h
@@ -6,7 +6,6 @@
 
 #include "glm/glm.hpp"
 
-#define GLM_FORCE_RADIANS
 #include "glm/gtc/matrix_transform.hpp"
 
 #ifndef PI
--- core/src/view/view.h
+++ core/src/view/view.h
@@ -5,7 +5,6 @@
 #include <cmath>
 #include <memory>
 
-#define GLM_FORCE_RADIANS
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
