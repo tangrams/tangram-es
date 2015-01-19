0529f13500441fc554b35e56e54b12945e73df8e
Karim Naaji <karim.naaji@gmail.com>
2015-01-19 15:17:43 -0500

    test travis

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -10,11 +10,11 @@ if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
     ANDROID_PLATFORM_VERSION="19"
 fi
 
-#if [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
-    #brew update
-    #brew tap homebrew/versions
-    #brew install glfw3
-#fi
+if [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
+    brew update
+    brew tap homebrew/versions
+    brew install glfw3
+fi
 
 
 if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
