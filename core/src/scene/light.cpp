b886c7e668a4405ab1bd8de5b25eaed407cef998
Matt Blair <blair1618@gmail.com>
2015-01-22 13:42:12 -0500

    Add comments and simplify Scene::addLight

--- core/src/scene/light.cpp
+++ core/src/scene/light.cpp
@@ -5,7 +5,15 @@
 std::string Light::s_vertexLightingBlock;
 std::string Light::s_fragmentLightingBlock;
 
-Light::Light(const std::string& _name, bool _dynamic):m_name(_name),m_typeName("undefined_light"),m_ambient(0.0f),m_diffuse(1.0f),m_specular(0.0f),m_type(LightType::UNDEFINED),m_injType(VERTEX),m_dynamic(_dynamic) {
+Light::Light(const std::string& _name, bool _dynamic):
+    m_name(_name),
+    m_typeName("undefined_light"),
+    m_ambient(0.0f),
+    m_diffuse(1.0f),
+    m_specular(0.0f),
+    m_type(LightType::UNDEFINED),
+    m_injType(VERTEX),
+    m_dynamic(_dynamic) {
 
 }
 
@@ -35,9 +43,26 @@ void Light::injectOnProgram(std::shared_ptr<ShaderProgram> _shader, InjectionTyp
         m_injType = _injType;
     }
     
-    _shader->addSourceBlock("defines", "#define TANGRAM_LIGHTS", false);
+    // Inject all needed #defines for this light instance
+    
+    _shader->addSourceBlock("defines", "#define TANGRAM_LIGHTS\n", false);
     _shader->addSourceBlock("defines", getInstanceDefinesBlock(), false);
-
+    if (m_injType == FRAGMENT) {
+        _shader->addSourceBlock("defines", "#define TANGRAM_FRAGMENT_LIGHTS\n", false);
+    }
+    if (m_injType == VERTEX) {
+        _shader->addSourceBlock("defines", "#define TANGRAM_VERTEX_LIGHTS\n", false);
+    }
+    
+    // For the vertex and/or fragment shaders (based on specified injection type), inject:
+    // 1. The struct and function definitions for this light class ("ClassBlock")
+    // 2. The declaration of this light instance ("InstanceBlock")
+    // 3. The function call to compute lighting for this light instance ("InstanceComputeBlock")
+    //
+    // These source strings are added to tags which do not directly correspond to a tag in the
+    // actual shader source, instead these tags are referenced in the <assembleLights> function
+    // to build a single string with all the lighting source code in a usable order.
+    
     if (m_injType == FRAGMENT) {
         _shader->addSourceBlock("__fragment_lighting", getClassBlock(), false);
         _shader->addSourceBlock("__fragment_lighting", getInstanceBlock());
@@ -61,27 +86,37 @@ void Light::setupProgram(std::shared_ptr<ShaderProgram> _shader) {
 
 void Light::assembleLights(std::map<std::string, std::vector<std::string>>& _sourceBlocks) {
     
+    // If needed, load the strings containing the main vertex and fragment lighting functions
+    
     if (s_vertexLightingBlock.empty()) {
         s_vertexLightingBlock = stringFromResource("lightVertex.glsl");
     }
-    
     if (s_fragmentLightingBlock.empty()) {
         s_fragmentLightingBlock = stringFromResource("lightFragment.glsl");
     }
     
+    // Create strings to contain the assembled vertex and fragment lighting source code
+    
     std::string vertexLighting;
     std::string fragmentLighting;
+    
+    // Concatenate all strings at the "__vertex_lighting" and "__fragment_lighting" keys
+    // (struct definitions and function definitions)
     
     for (const auto& string : _sourceBlocks["__vertex_lighting"]) {
         vertexLighting += string;
     }
-    
     for (const auto& string : _sourceBlocks["__fragment_lighting"]) {
         fragmentLighting += string;
     }
     
+    // After lights definitions are all added, add the main lighting functions
+    
     vertexLighting += s_vertexLightingBlock;
     fragmentLighting += s_fragmentLightingBlock;
+    
+    // The main lighting functions each contain a tag where all light instances should be computed;
+    // Insert all of our "lights_to_compute" at this tag
     
     std::string tag = "#pragma tangram: vertex_lights_to_compute";
     int pos = vertexLighting.find(tag) + tag.length();
@@ -96,6 +131,10 @@ void Light::assembleLights(std::map<std::string, std::vector<std::string>>& _sou
         fragmentLighting.insert(pos, string);
         pos += string.length();
     }
+    
+    // Place our assembled lighting source code back into the map of "source blocks";
+    // The assembled strings will then be injected into a shader at the "vertex_lighting"
+    // and "fragment_lighting" tags
     
     _sourceBlocks["vertex_lighting"].push_back(vertexLighting);
     _sourceBlocks["fragment_lighting"].push_back(fragmentLighting);
--- core/src/scene/scene.cpp
+++ core/src/scene/scene.cpp
@@ -17,25 +17,15 @@ void Scene::addStyle(std::unique_ptr<Style> _style) {
 
 void Scene::addLight(std::shared_ptr<Light> _light, InjectionType _type) {
 
-    //  Avoid duplications
+    // Avoid duplications
     if (m_lights.find(_light->getName()) != m_lights.end()) {
         logMsg("ERROR: Can't add the same light twice. Try using another the name instead.\n");
         return;
     }
     
+    // Add light to shader programs for all styles
     for (auto& style : m_styles) {
         _light->injectOnProgram(style->getShaderProgram(), _type);
-        
-        if( _light->getInjectionType() == FRAGMENT){
-            style->getShaderProgram()->addSourceBlock("defines", "#define TANGRAM_FRAGMENT_LIGHTS\n", false);
-        }
-        
-
-        if( _light->getInjectionType() == VERTEX){
-            style->getShaderProgram()->addSourceBlock("defines", "#define TANGRAM_VERTEX_LIGHTS\n", false);
-        }
-        
-        
     }
     
     m_lights[_light->getName()] = _light;
