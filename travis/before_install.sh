9c529019009933ddf03d7ab262c74962982e35e8
Matt Blair <blair1618@gmail.com>
2015-02-06 16:23:38 -0500

    Clean up travis before_install

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -18,37 +18,28 @@ if [[ ${PLATFORM} == "android" ]]; then
     ANDROID_BUILD_TOOL_VERSION="21.1.2"
     ANDROID_PLATFORM_VERSION="19"
 
-    # gcc-4.8 should be default in travis ci now (https://github.com/travis-ci/travis-cookbooks/pull/215), but for safety
-    #sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-    #sudo apt-get update -qq
-    # No linux specific build right now, for android builds, gcc bundled with ndk is used
-    # if [ "$CXX" = "g++" ]; then sudo apt-get install -qq g++-4.8; fi
-    # if [ "$CXX" = "g++" ]; then export CXX="g++-4.8" CC="gcc-4.8"; fi
-
-    # install other required packages
-    #sudo apt-get -y install git build-essential automake gdb libtool make cmake pkg-config 
-
-    # not installing mesa and opengl right now, will need with linux builds though
-
-    # install jdk, ant and 32bit dependencies for android sdk
-    #sudo apt-get -qq -y install openjdk-7-jdk ant lib32z1-dev lib32stdc++6
+    # Install ant
     brew install ant
 
-    # install android sdk
+    # Install android sdk
     wget https://dl-ssl.google.com/android/android-sdk_${ANDROID_SDK_VERSION}-macosx.zip
     tar -zxf android-sdk_${ANDROID_SDK_VERSION}-macosx.zip
     export ANDROID_HOME=$PWD/android-sdk-macosx
-    
-    # install android ndk
+
+    # Install required Android components; automatically accept the license prompt
+    echo "Updating Android SDK..."
+    echo "y" | android update sdk --filter platform-tools,build-tools-${ANDROID_BUILD_TOOL_VERSION},android-${ANDROID_PLATFORM_VERSION},extra-android-support --no-ui --force >/dev/null
+    echo "Done."
+
+    # Install android ndk
+    echo "Cloning mindk..."
     git clone --quiet --depth 1 https://github.com/tangrams/mindk.git
     export ANDROID_NDK=$PWD/mindk/android-ndk-r10d
+    echo "Done."
 
     # Update PATH
+    echo "Updating PATH..."
     export PATH=${PATH}:${ANDROID_HOME}/tools:${ANDROID_NDK}
-
-    # Install required Android components.
-    # automatically accept the license prompt
-    echo "y" | android update sdk --filter platform-tools,build-tools-${ANDROID_BUILD_TOOL_VERSION},android-${ANDROID_PLATFORM_VERSION},extra-android-support --no-ui --force >/dev/null
-
     echo $PATH
+    echo "Done."
 fi
