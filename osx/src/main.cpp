4575a26dc61dfa47a62daf5ea7bcfeab5300f48b
Matt Blair <blair1618@gmail.com>
2015-02-19 19:12:04 -0500

    Forgot that I changed a function signature

--- osx/src/main.cpp
+++ osx/src/main.cpp
@@ -72,11 +72,11 @@ void scroll_callback(GLFWwindow* window, double scrollx, double scrolly) {
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
     if (key == GLFW_KEY_E && (action == GLFW_PRESS || action == GLFW_REPEAT)) {
-        Tangram::handleRotateGesture(0.0f, 0.0f, -0.1f);
+        Tangram::handleRotateGesture(-0.1f);
     }
     
     if (key == GLFW_KEY_Q && (action == GLFW_PRESS || action == GLFW_REPEAT)) {
-        Tangram::handleRotateGesture(0.0f, 0.0f, 0.1f);
+        Tangram::handleRotateGesture(0.1f);
     }
 }
 
