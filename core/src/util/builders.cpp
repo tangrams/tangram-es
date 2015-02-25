4d76655799c236b1606ae58ae0bdd72541d57485
Matt Blair <blair1618@gmail.com>
2015-02-25 18:43:46 -0500

    Overhaul polyline tesselation for simplicity
    Still todo, add outline tesselation again

--- core/src/util/builders.cpp
+++ core/src/util/builders.cpp
@@ -170,7 +170,7 @@ glm::vec2 perp2d(const glm::vec3& _v1, const glm::vec3& _v2 ){
 }
 
 // Get 2D vector rotated 
-glm::vec2 rotate (const glm::vec2& _v, float _radians) {
+glm::vec2 rotate(const glm::vec2& _v, float _radians) {
     float cos = std::cos(_radians);
     float sin = std::sin(_radians);
     return glm::vec2(_v.x * cos - _v.y * sin, _v.x * sin + _v.y * cos);
@@ -192,144 +192,95 @@ void addPolyLineVertex(const glm::vec3& _coord, const glm::vec2& _normal, const 
 }
 
 // Helper function for polyline tesselation; adds indices for pairs of vertices arranged like a line strip
-void indexPairs ( int _nPairs, int _vertexDataOffset, std::vector<int>& _indicesOut) {
+void indexPairs( int _nPairs, int _nVertices, std::vector<int>& _indicesOut) {
     for (int i = 0; i < _nPairs; i++) {
-        _indicesOut.push_back(_vertexDataOffset + 2*i+2);
-        _indicesOut.push_back(_vertexDataOffset + 2*i+1);
-        _indicesOut.push_back(_vertexDataOffset + 2*i);
+        _indicesOut.push_back(_nVertices - 2*i - 4);
+        _indicesOut.push_back(_nVertices - 2*i - 2);
+        _indicesOut.push_back(_nVertices - 2*i - 3);
         
-        _indicesOut.push_back(_vertexDataOffset + 2*i+2);
-        _indicesOut.push_back(_vertexDataOffset + 2*i+3);
-        _indicesOut.push_back(_vertexDataOffset + 2*i+1);
+        _indicesOut.push_back(_nVertices - 2*i - 3);
+        _indicesOut.push_back(_nVertices - 2*i - 2);
+        _indicesOut.push_back(_nVertices - 2*i - 1);
     }
 }
 
-//  Tessalate a FAN geometry between points A       B
+//  Tessalate a fan geometry between points A       B
 //  using their normals from a center        \ . . /
 //  and interpolating their UVs               \ p /
 //                                             \./
 //                                              C
-void addFan (const glm::vec3& _coord, 
-             const glm::vec2& _nA, const glm::vec2& _nC, const glm::vec2& _nB, 
-             const glm::vec2& _uA, const glm::vec2& _uC, const glm::vec2& _uB, 
-             bool _isSigned, int _numTriangles, float _halfWidth,
-             PolyLineOutput _out) {
-
-    int vertexDataOffset = _out.points.size();
-
-    if (_numTriangles < 1) {
-        return;
-    }
-
-    glm::vec2 normCurr = _nA;
-    glm::vec2 normPrev = glm::vec2(0.0,0.0);
-
-    float angle_delta = glm::dot(_nA, _nB);
-    if (angle_delta < -1.0) {
-        angle_delta = -1.0;
-    } else if (angle_delta > 1.0){
-        angle_delta = 1.0;
-    }
-    angle_delta = std::acos(angle_delta)/(float)_numTriangles;
-
-    if (!_isSigned) {
-        angle_delta *= -1;
-    }
-
-    glm::vec2 uvCurr = _uA;
-    glm::vec2 uv_delta = (_uB-_uA)/(float)_numTriangles;
-
-    //  Add the first and CENTER vertex 
-    //  The triangles will be composed on FAN style arround it
-    addPolyLineVertex(_coord, _nC, _uC, _halfWidth, _out);
-
-    //  Add first corner
-    addPolyLineVertex(_coord, normCurr, _uA, _halfWidth, _out);
-
-    // Iterate through the rest of the coorners
+void addFan(const glm::vec3& _C, const glm::vec2& _CA, const glm::vec2& _CB, const glm::vec2& _uv,
+             int _numTriangles, float _halfWidth, PolyLineOutput _out) {
+    
+    // Find angle difference
+    float cross = _CA.x * _CB.y - _CA.y * _CB.x; // z component of cross(_CA, _CB)
+    float angle = atan2f(cross, glm::dot(_CA, _CB));
+    float dAngle = angle / (float)_numTriangles;
+    
+    int startIndex = _out.points.size();
+    
+    // Add center vertex
+    addPolyLineVertex(_C, {0.0f, 0.0f}, _uv, _halfWidth, _out);
+    
+    // Add vertex for point A
+    addPolyLineVertex(_C, _CA, _uv, _halfWidth, _out);
+    
+    // Add radial vertices
+    glm::vec2 radial = _CA;
     for (int i = 0; i < _numTriangles; i++) {
-        normPrev = glm::normalize(normCurr);
-        normCurr = rotate( glm::normalize(normCurr), angle_delta);     //  Rotate the extrusion normal
-
-        if (_numTriangles == 4 && (i == 0 || i == _numTriangles - 2)) {
-            float scale = 2.0 / (1.0 + std::fabs(glm::dot(normPrev, normCurr)));
-            normCurr *= scale*scale;
-        }
-
-        uvCurr = uvCurr+uv_delta;
-
-        addPolyLineVertex(_coord, normCurr, uvCurr, _halfWidth, _out);      //  Add computed corner
+        radial = rotate(radial, dAngle);
+        addPolyLineVertex(_C, radial, _uv, _halfWidth, _out);
     }
-
+    
+    // Add indices
     for (int i = 0; i < _numTriangles; i++) {
-        if (_isSigned) {
-            _out.indices.push_back(vertexDataOffset + i+2);
-            _out.indices.push_back(vertexDataOffset );
-            _out.indices.push_back(vertexDataOffset + i+1);
+        _out.indices.push_back(startIndex); // center vertex
+        if (angle > 0) {
+            _out.indices.push_back(startIndex + i + 2);
+            _out.indices.push_back(startIndex + i + 1);
         } else {
-            _out.indices.push_back(vertexDataOffset + i+1);
-            _out.indices.push_back(vertexDataOffset );
-            _out.indices.push_back(vertexDataOffset + i+2);
+            _out.indices.push_back(startIndex + i + 1);
+            _out.indices.push_back(startIndex + i + 2);
         }
     }
+    
 }
 
-//  Function to add the vertex need for line caps,
-//  because re-use the buffers needs to be at the end
-void addCap (const glm::vec3& _coord, const glm::vec2& _normal, int _numCorners, bool _isBeginning, float _halfWidth, PolyLineOutput _out) {
+// Function to add the vertices for line caps
+void addCap(const glm::vec3& _coord, const glm::vec2& _normal, int _numCorners, bool _isBeginning, float _halfWidth, PolyLineOutput _out) {
 
     if (_numCorners < 1) {
         return;
     }
+    
+    glm::vec2 uv = glm::vec2(0.5, _isBeginning ? 0.0 : 1.0); // center point UVs
+    float sign = _isBeginning ? 1.0f : -1.0f; // caps at beginning and end fan in opposite directions
 
-    // UVs
-    glm::vec2 uvA = glm::vec2(0.0,0.0); // Begining angle UVs
-    glm::vec2 uvC = glm::vec2(0.5,0.0); // center point UVs
-    glm::vec2 uvB = glm::vec2(1.0,0.0); // Ending angle UVs
- 
-    if (!_isBeginning) {
-        uvA = glm::vec2(0.0,1.0);       // Begining angle UVs
-        uvC = glm::vec2(0.5,1.0);       // center point UVs
-        uvB = glm::vec2(1.0,1.0);       // Ending angle UVs
-    }
-
-    addFan( _coord, 
-            -_normal, glm::vec2(0,0), _normal, 
-            uvA, uvC, uvB, 
-            _isBeginning, _numCorners*2, 
-            _halfWidth, _out);
+    addFan(_coord, -sign * _normal, sign * _normal, uv, _numCorners * 2, _halfWidth, _out);
 }
 
-//  Cross product
-float signed_area (const glm::vec3& _v1, const glm::vec3& _v2, const glm::vec3& _v3) {
-    return (_v2.x-_v1.x)*(_v3.y-_v1.y) - (_v3.x-_v1.x)*(_v2.y-_v1.y);
-};
-
-float valuesWithinTolerance ( float _a, float _b, float _tolerance = 0.001) {
-    return std::abs(_a - _b) < _tolerance;
+float valuesWithinTolerance(float _a, float _b, float _tolerance = 0.001) {
+    return fabs(_a - _b) < _tolerance;
 }
 
 // Tests if a line segment (from point A to B) is nearly coincident with the edge of a tile
-bool isOnTileEdge (const glm::vec3& _pa, const glm::vec3& _pb) {
+bool isOnTileEdge(const glm::vec3& _pa, const glm::vec3& _pb) {
+    
     float tolerance = 0.0002; // tweak this adjust if catching too few/many line segments near tile edges
-                                            // TODO: make tolerance configurable by source if necessary
+    // TODO: make tolerance configurable by source if necessary
     glm::vec2 tile_min = glm::vec2(-1.0,-1.0);
     glm::vec2 tile_max = glm::vec2(1.0,1.0);
 
-    if (valuesWithinTolerance(_pa.x, tile_min.x, tolerance) && valuesWithinTolerance(_pb.x, tile_min.x, tolerance)) {
-        return true;
-    } else if (valuesWithinTolerance(_pa.x, tile_max.x, tolerance) && valuesWithinTolerance(_pb.x, tile_max.x, tolerance)) {
-        return true;
-    } else if (valuesWithinTolerance(_pa.y, tile_min.y, tolerance) && valuesWithinTolerance(_pb.y, tile_min.y, tolerance)) {
-        return true;
-    } else if (valuesWithinTolerance(_pa.y, tile_max.y, tolerance) && valuesWithinTolerance(_pb.y, tile_max.y, tolerance)) {
-        return true;
-    }
-    return false;
+    return (valuesWithinTolerance(_pa.x, tile_min.x, tolerance) && valuesWithinTolerance(_pb.x, tile_min.x, tolerance)) ||
+           (valuesWithinTolerance(_pa.x, tile_max.x, tolerance) && valuesWithinTolerance(_pb.x, tile_max.x, tolerance)) ||
+           (valuesWithinTolerance(_pa.y, tile_min.y, tolerance) && valuesWithinTolerance(_pb.y, tile_min.y, tolerance)) ||
+           (valuesWithinTolerance(_pa.y, tile_max.y, tolerance) && valuesWithinTolerance(_pb.y, tile_max.y, tolerance));
 }
 
 void Builders::buildPolyLine(const Line& _line, const PolyLineOptions& _options, PolyLineOutput& _out) {
- 
+    
+    // TODO: For outlines, we'll want another function which pre-processes the points into distinct line segments and then calls buildPolyLine as needed
+    
     int lineSize = (int)_line.size();
     
     if (lineSize < 2) {
@@ -339,187 +290,83 @@ void Builders::buildPolyLine(const Line& _line, const PolyLineOptions& _options,
     // TODO: pre-allocate output vectors; try estimating worst-case space usage
     
     glm::vec3 coordPrev, coordCurr, coordNext;
-    glm::vec2 normPrev, normCurr, normNext;
+    glm::vec2 normPrev, normNext, miterVec;
 
     int cornersOnCap = (int)_options.cap;
     int trianglesOnJoin = (int)_options.join;
-
-    int vertexDataOffset = (int)_out.points.size();
-    int nPairs = 0;
     
-    bool hasPrev = false;
-    bool hasNext = false;
+    // Process first point in line with an end cap
+    coordCurr = _line[0];
+    coordNext = _line[1];
+    normNext = glm::normalize(perp2d(coordCurr, coordNext));
+    addCap(coordCurr, normNext, cornersOnCap, true, _options.halfWidth, _out);
+    addPolyLineVertex(coordCurr, normNext, {1.0f, 0.0f}, _options.halfWidth, _out);
+    addPolyLineVertex(coordCurr, -normNext, {0.0f, 0.0f}, _options.halfWidth, _out);
     
-    for (int i = 0; i < lineSize; i++) {
+    // Process intermediate points
+    for (int i = 1; i < lineSize - 1; i++) {
 
-        // There is a next one?
-        hasNext = i+1 < lineSize;
+        coordPrev = coordCurr;
+        coordCurr = coordNext;
+        coordNext = _line[i + 1];
+        
+        normPrev = normNext;
+        normNext = glm::normalize(perp2d(coordCurr, coordNext));
 
-        if (hasPrev) {
-            // If there is a previous one, copy the current (previous) values on *Prev values
-            coordPrev = coordCurr;
-            normPrev = glm::normalize(perp2d(coordPrev, _line[i]) );
-        } else if (i == 0 && _options.closePolygon) {
-            // If is the first point and is a close polygon
-            // TODO   
-            bool needToClose = true;
-            if (_options.removeTileEdges) {
-                if(isOnTileEdge(_line[i], _line[lineSize-2])) {
-                    needToClose = false;
-                }
-            }
-
-            if (needToClose) {
-                coordPrev = _line[lineSize-2];
-                normPrev = glm::normalize(perp2d(coordPrev, _line[i]));
-                hasPrev = true;
-            }
-        }
-
-        // Assign current coordinate
-        coordCurr = _line[i];
-
-        if (hasNext) {
-            coordNext = _line[i+1];
-        } else if (_options.closePolygon) {
-            // If is the last point a close polygon
-            coordNext = _line[1];
-            hasNext = true;
-        }
-
-        if (hasNext) {
-            // If is not the last one get next coordinates and calculate the right normal
-
-            normNext = glm::normalize(perp2d(coordCurr, coordNext));
-            if (_options.removeTileEdges) {
-                if (isOnTileEdge(coordCurr, coordNext) ) {
-                    normCurr = glm::normalize(perp2d(coordPrev, coordCurr));
-                    
-                    if (hasPrev) {
-                        addPolyLineVertex(coordCurr, normCurr, {1.0, i/(float)lineSize}, _options.halfWidth, _out);
-                        addPolyLineVertex(coordCurr, normCurr, {0.0, i/(float)lineSize}, _options.halfWidth, _out);
-
-                        // Add vertices to buffer acording their index
-                        indexPairs(nPairs, vertexDataOffset, _out.indices);
-                        
-                        vertexDataOffset = (int)_out.points.size();
-                        nPairs = 0;
-                    }
-                    hasPrev = false;
-                    continue;
-                }
-            }
-        }
-
-        //  Compute current normal
-        if (hasPrev) {
-            //  If there is a PREVIUS ...
-            if (hasNext) {
-                // ... and a NEXT ONE, compute previus and next normals (scaled by the angle with the last prev)
-                normCurr = glm::normalize(normPrev + normNext);
-                float scale = 2.0f / (1.0f + std::fabs(glm::dot(normPrev, normCurr)));
-                normCurr *= scale*scale;
-            } else {
-                // ... and there is NOT a NEXT ONE, copy the previus next one (which is the current one)
-                normCurr = glm::normalize(perp2d(coordPrev, coordCurr));
-            }
+        // Compute "normal" for miter joint
+        miterVec = normPrev + normNext;
+        float scale = sqrtf(2.0f / (1.0f + glm::dot(normPrev, normNext)) / glm::dot(miterVec, miterVec) );
+        miterVec *= scale;
+        
+        float v = i / (float)lineSize;
+        
+        if (trianglesOnJoin == 0) {
+            // Join type is a simple miter
+            
+            addPolyLineVertex(coordCurr, miterVec, {1.0, v}, _options.halfWidth, _out);
+            addPolyLineVertex(coordCurr, -miterVec, {0.0, v}, _options.halfWidth, _out);
+            indexPairs(1, _out.points.size(), _out.indices);
+            
         } else {
-            // If is NOT a PREVIUS ...
-            if (hasNext) {
-                // ... and a NEXT ONE,
-                normNext = glm::normalize(perp2d(coordCurr, coordNext));
-                normCurr = normNext;
-            } else {
-                // ... and NOT a NEXT ONE, nothing to do (without prev or next one this is just a point)
-                continue;
-            }
-        }
-
-        if (hasPrev || hasNext) {
+            // Join type uses a fan of triangles
             
-            // If is the BEGINING of a LINE
-            if (i == 0 && !hasPrev && !_options.closePolygon) {
-                // Add previus vertices to buffer and reset the index pairs counter
-                // Because we are going to add more triangles.
-                indexPairs(nPairs, vertexDataOffset, _out.indices);
-                
-                addCap( coordCurr, normCurr,
-                        cornersOnCap, true, 
-                        _options.halfWidth, _out);
-
-                vertexDataOffset = (int)_out.points.size();
-                nPairs = 0;
-            }
-
-            // If is a JOIN
-            if(trianglesOnJoin != 0 ) {
-                // Add previus vertices to buffer and reset the index pairs counter
-                // Because we are going to add more triangles.
-
-                bool isSigned = signed_area(coordPrev, coordCurr, coordNext) > 0;
-
-                glm::vec2 nA = normPrev;     // normal to point A (aT)
-                glm::vec2 nC = -normCurr;    // normal to center (-vP)
-                glm::vec2 nB = normNext;     // normal to point B (bT)
-
-                float pct = (float)i/(float)lineSize;
-
-                glm::vec2 uA = glm::vec2(1.0,pct);
-                glm::vec2 uC = glm::vec2(0.0,pct);
-                glm::vec2 uB = glm::vec2(1.0,pct);
-                
-                if (isSigned) {
-                    addPolyLineVertex(coordCurr, nA, uA, _options.halfWidth, _out);
-                    addPolyLineVertex(coordCurr, nC, uC, _options.halfWidth, _out);
-                } else {
-                    nA = -normPrev;
-                    nC = normCurr;
-                    nB = -normNext;
-                    uA = glm::vec2(0.0,pct);
-                    uC = glm::vec2(1.0,pct);
-                    uB = glm::vec2(0.0,pct);
-                    addPolyLineVertex(coordCurr, nC, uC, _options.halfWidth, _out);
-                    addPolyLineVertex(coordCurr, nA, uA, _options.halfWidth, _out);
-                }
+            bool isRightTurn = (normNext.x * normPrev.y - normNext.y * normPrev.x) > 0; // z component of cross(normNext, normPrev)
             
-                indexPairs(nPairs, vertexDataOffset, _out.indices);
-
-                addFan( coordCurr, nA, nC, nB, uA, uC, uB, isSigned, trianglesOnJoin, 
-                       _options.halfWidth, _out);
-
-                vertexDataOffset = (int)_out.points.size();
-                nPairs = 0;
-
-                if (isSigned) {
-                    addPolyLineVertex(coordCurr, nB, uB, _options.halfWidth, _out);
-                    addPolyLineVertex(coordCurr, nC, uC, _options.halfWidth, _out);
-                } else {
-                    addPolyLineVertex(coordCurr, nC, uC, _options.halfWidth, _out);
-                    addPolyLineVertex(coordCurr, nB, uB, _options.halfWidth, _out);
-                }
+            if (isRightTurn) {
+                
+                addPolyLineVertex(coordCurr, miterVec, {1.0f, v}, _options.halfWidth, _out);
+                addPolyLineVertex(coordCurr, -normPrev, {0.0f, v}, _options.halfWidth, _out);
+                indexPairs(1, _out.points.size(), _out.indices);
+                
+                addFan(coordCurr, -normPrev, -normNext, {0.0f, v}, trianglesOnJoin, _options.halfWidth, _out);
+                
+                addPolyLineVertex(coordCurr, miterVec, {1.0f, v}, _options.halfWidth, _out);
+                addPolyLineVertex(coordCurr, -normNext, {0.0f, v}, _options.halfWidth, _out);
+                indexPairs(1, _out.points.size(), _out.indices);
                 
             } else {
-                addPolyLineVertex( coordCurr, normCurr, {1.0, i/((float)lineSize-1.0)}, _options.halfWidth, _out);
-                addPolyLineVertex( coordCurr, normCurr, {0.0, i/((float)lineSize-1.0)}, _options.halfWidth, _out);
+                
+                addPolyLineVertex(coordCurr, normPrev, {1.0f, v}, _options.halfWidth, _out);
+                addPolyLineVertex(coordCurr, -miterVec, {0.0f, v}, _options.halfWidth, _out);
+                indexPairs(1, _out.points.size(), _out.indices);
+                
+                addFan(coordCurr, normPrev, normNext, {0.0f, v}, trianglesOnJoin, _options.halfWidth, _out);
+                
+                addPolyLineVertex(coordCurr, normNext, {1.0f, v}, _options.halfWidth, _out);
+                addPolyLineVertex(coordCurr, -miterVec, {0.0f, v}, _options.halfWidth, _out);
+                indexPairs(1, _out.points.size(), _out.indices);
+                
             }
             
-            if (i+1 < lineSize) {
-               nPairs++;
-            }
-
-            hasPrev = true;
         }
     }
     
-    // Add vertices to buffer acording their index
-    indexPairs(nPairs, vertexDataOffset, _out.indices);
+    // Process last point in line with a cap
+    addPolyLineVertex(coordNext, normNext, {1.0f, 1.0f}, _options.halfWidth, _out);
+    addPolyLineVertex(coordNext, -normNext, {0.0f, 1.0f}, _options.halfWidth, _out);
+    indexPairs(1, _out.points.size(), _out.indices);
+    addCap(coordNext, normNext, cornersOnCap , false, _options.halfWidth, _out);
     
-
-    // If is the END OF a LINE
-    if(!_options.closePolygon) {
-        vertexDataOffset = (int)_out.points.size();
-        addCap(coordCurr, normCurr, cornersOnCap , false, _options.halfWidth, _out);
-    }
 }
 
 void Builders::buildQuadAtPoint(const Point& _point, const glm::vec3& _normal, float halfWidth, float height, PolygonOutput& _out) {
