49760fb7bac347a32a44edf0c88ae1f0b3e02b98
Varun Talwar <tallytalwar@gmail.com>
2015-03-30 12:53:41 -0400

    Handles binary protobuf tiles now
    - simplier code
    - uses okio.BufferedSource API

--- android/src/com/mapzen/tangram/OkHttpInterface.java
+++ android/src/com/mapzen/tangram/OkHttpInterface.java
@@ -8,6 +8,8 @@ import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
 import com.squareup.okhttp.Call;
 
+import okio.BufferedSource;
+
 import java.io.IOException;
 
 public class OkHttpInterface
@@ -31,10 +33,10 @@ public class OkHttpInterface
         Request request = new Request.Builder().url(url).build();
         Response response = client.newCall(request).execute();
         if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+        BufferedSource src = response.body().source();
+        rawDataBytes = src.readByteArray();
         
-        rawDataOut = response.body().string();
-        rawDataBytes = rawDataOut.getBytes(Charset.forName("UTF-8"));
-        System.out.println("Tangram Java: " + url + " fetched str Final length: " + rawDataOut.length());
         System.out.println("Tangram Java: " + url + " fetched bytes Final length: " + rawDataBytes.length);
         return true;
     }
