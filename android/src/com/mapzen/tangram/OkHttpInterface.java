229d9740118179ff34e2736c06b1f0d3a199db92
Varun Talwar <tallytalwar@gmail.com>
2015-03-31 14:19:05 -0400

    Network timeout set
    - Cleaned some code to have a constructor for OkHttpInterface
    - Response is in a try catch block, and throws an Exception when a timeout occurs (in establishing connection: 10
    seconds, in reading data: 30 seconds)

--- android/src/com/mapzen/tangram/OkHttpInterface.java
+++ android/src/com/mapzen/tangram/OkHttpInterface.java
@@ -11,6 +11,7 @@ import com.squareup.okhttp.Call;
 import okio.BufferedSource;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class OkHttpInterface
 {
@@ -23,22 +24,34 @@ public class OkHttpInterface
         System.loadLibrary("c++_shared");
         System.loadLibrary("tangram");
     }
+
+    public OkHttpInterface()
+    {
+        if(client == null) 
+        {
+            client = new OkHttpClient();
+        }
+        client.setConnectTimeout(10, TimeUnit.SECONDS);
+        client.setReadTimeout(30, TimeUnit.SECONDS);
+    }
     
     public boolean run(String url) throws Exception
     {
-        if(client == null) {
-            client = new OkHttpClient();
-        }
-
         Request request = new Request.Builder().url(url).build();
-        Response response = client.newCall(request).execute();
-        if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
 
-        BufferedSource src = response.body().source();
-        rawDataBytes = src.readByteArray();
+        try {
+            Response response = client.newCall(request).execute();
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+            BufferedSource src = response.body().source();
+            rawDataBytes = src.readByteArray();
         
-        System.out.println("Tangram Java: " + url + " fetched bytes Final length: " + rawDataBytes.length);
-        return true;
+            System.out.println("Tangram Java: " + url + " fetched bytes Final length: " + rawDataBytes.length);
+            return true;
+        } catch (Exception e) {
+            System.out.println("Tangram Timeout??");
+            return false;
+        }
     }
 }
 
