56a0ad983087285aca4f1cf9901f1b6322492537
Hannes Janetzek <hannes.janetzek@gmail.com>
2015-04-09 02:03:02 +0200

    Add TypedMesh template, used for Polygon and Polyline

---
+++ core/src/util/typedMesh.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "vboMesh.h"
+
+template<class T>
+class TypedMesh : public VboMesh {
+   public:
+    TypedMesh(std::shared_ptr<VertexLayout> _vertexLayout, GLenum _drawMode)
+        : VboMesh(_vertexLayout, _drawMode){};
+
+    void addVertices(std::vector<T>&& _vertices,
+                     std::vector<int>&& _indices) {
+        vertices.push_back(_vertices);
+        indices.push_back(_indices);
+
+        m_nVertices += _vertices.size();
+        m_nIndices += _indices.size();
+    }
+
+   protected:
+    std::vector<std::vector<T>> vertices;
+    std::vector<std::vector<int>> indices;
+
+    virtual ByteBuffers compileVertexBuffer() override {
+        return compile(vertices, indices);
+    }
+};
