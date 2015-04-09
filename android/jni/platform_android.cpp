a9f898eb02cec76f99a8747ffedbbb64bc8b8dbc
Varun Talwar <tallytalwar@gmail.com>
2015-04-09 13:16:51 -0400

    streamFromHttpASync and other android specific
    - implements cacheJniEnv method
    - implements cacheTangramInstance method -> caching a global reference of the tangram instance
    - modified setAssetManager to use cached jni environment.
    - removed implementation of streamFromHttpSync (older) and implemented streamFromHttpAsync
        - find the tangramClass to get the "networkRequest" methodID
        - get the methodID for "networkRequest" method
        - calls the "networkRequest" method with appropriate arguments. (networkRequest uses a OkHttp callback to perform network request)
        - returns the status of the networkRequest.

--- android/jni/platform_android.cpp
+++ android/jni/platform_android.cpp
@@ -16,45 +16,21 @@
  */
 
 static AAssetManager* assetManager;
-static JavaVM* jvm; //store the only jvm instance for android to be used to attach JNIEnv for specific threads
 
-/* We will also cache the class instance since JavaVM is not happy when we try to do a findClass from within a native thread.
- * So we will store an instance of the class (jobject) and use it to find the class itself (jclass).
- * NOTE: this will change when we have async OKHTTP, which will only require a single instance of the OkHttpInterface
- */
-const char* okHttpInterfacePath = "com/mapzen/tangram/OkHttpInterface";
-static jobject okHttpInterfaceObj;
+static JNIEnv* jniEnv;
+static jobject tangramInstance;
 
-void cacheJavaVM(JNIEnv* _jniEnv) {
-    _jniEnv->GetJavaVM(&jvm);
+void cacheJniEnv(JNIEnv* _jniEnv) {
+    jniEnv = _jniEnv;
 }
 
-void cacheClassInstance(JNIEnv* _jniEnv) {
-    jclass cls = _jniEnv->FindClass(okHttpInterfacePath);
-    if(!cls) {
-        logMsg("failed to get %s class reference.\n", okHttpInterfacePath);
-        return;
-    }
-    jmethodID constr = _jniEnv->GetMethodID(cls, "<init>", "()V");
-    if(!constr) {
-        logMsg("failed to get %s constructor.\n", okHttpInterfacePath);
-        return;
-    }
-    jobject obj = _jniEnv->NewObject(cls, constr);
-    if(!obj) {
-        logMsg("failed to create a %s object.\n", okHttpInterfacePath);
-        return;
-    }
-    okHttpInterfaceObj = _jniEnv->NewGlobalRef(obj); //Make sure this is a global reference, to avoid this being garbase collected during the life cycle of the app
+void cacheTangramInstance(jobject _tangramInstance) {
+    tangramInstance = jniEnv->NewGlobalRef(_tangramInstance);
 }
 
-void deleteClassInstance(JNIEnv* _jniEnv) {
-    _jniEnv->DeleteGlobalRef(okHttpInterfaceObj);
-}
+void setAssetManager(jobject _assetManager) {
 
-void setAssetManager(JNIEnv* _jniEnv, jobject _assetManager) {
-
-    assetManager = AAssetManager_fromJava(_jniEnv, _assetManager);
+    assetManager = AAssetManager_fromJava(jniEnv, _assetManager);
 
     if (assetManager == nullptr) {
         logMsg("ERROR: Could not obtain Asset Manager reference\n");
@@ -128,120 +104,25 @@ unsigned char* bytesFromResource(const char* _path, unsigned int* _size) {
     return data;
 }
 
-bool streamFromHttpSync(const std::string& _url, std::stringstream& _rawData) {
-    int status;
-    JNIEnv *jniEnv;
-    bool isAttached = false;
+bool streamFromHttpASync(const std::string& _url, const int _tileIDx, const int _tileIDy, const int _tileIDz, const int _dataSourceID) {
     jstring jUrl;
     jboolean methodResult;
-    jbyteArray jfetchedBytes;
-    std::string resultStr;
 
+    jclass tangramClass = jniEnv->FindClass("com/mapzen/tangram/Tangram");
 
-    /*
-     * Get JNI Environment, or link new jniEnv with this native thread
-     */
-    status = jvm->GetEnv(reinterpret_cast<void**>(&jniEnv), JNI_VERSION_1_6);
-    if(status == JNI_EVERSION) {
-        logMsg("JNI_VERSION_1_6 not supported. Can not get JNI Environment.\n");
-        return false;
-    }
-    if(status == JNI_EDETACHED) {
-        logMsg("Failed to get JNI environment, assuming native thread and calling AttachCurrentThread to get JNIEnv");
-        status = jvm->AttachCurrentThread(&jniEnv, NULL);
-        if(status < 0) {
-            logMsg("Failed to attach current thread.\n");
-            return false;
-        }
-        isAttached = true;
-    }
+    jmethodID method = jniEnv->GetMethodID(tangramClass, "networkRequest", "(Ljava/lang/String;IIII)Z");
 
-    /*
-     * Get the interface class from the cached global object
-     */
-    jclass interfaceClass = jniEnv->GetObjectClass(okHttpInterfaceObj);
-    if(!interfaceClass) {
-        logMsg("Failed to get %s class reference from object.", okHttpInterfacePath);
-        if(isAttached) {
-            jvm->DetachCurrentThread();
-        }
-        return false;
-    }
-
-    /*
-     * Get the main run methodID
-     */
-    jmethodID method = jniEnv->GetMethodID(interfaceClass, "run", "(Ljava/lang/String;)Z");
-    if(!method) {
-        logMsg("Failed to get \"run\" method ID.\n");
-        if(isAttached) {
-            jvm->DetachCurrentThread();
-        }
-        return false;
-    }
-
-    /*
-     * Get the method id for the constructor to create a local object for the interfaceclass.
-     */
-    jmethodID constr = jniEnv->GetMethodID(interfaceClass, "<init>", "()V");
-    if(!constr) {
-        logMsg("failed to get %s constructor.\n", okHttpInterfacePath);
-        return false;
-    }
-    jobject obj = jniEnv->NewObject(interfaceClass, constr);
-    if(!obj) {
-        logMsg("failed to create a %s object.\n", okHttpInterfacePath);
-        if(isAttached) {
-            jvm->DetachCurrentThread();
-        }
-        return false;
-    }
-
-    /* 
-     * construct jstring for url
-     */
     jUrl = jniEnv->NewStringUTF(_url.c_str());
 
-    methodResult = jniEnv->CallBooleanMethod(obj, method, jUrl);
+    methodResult = jniEnv->CallBooleanMethod(tangramInstance, method, jUrl, (jint)_tileIDx, (jint)_tileIDy, (jint)_tileIDz, (jint)_dataSourceID);
 
     if(!methodResult) {
-        logMsg("run method returns false\n");
-        if(isAttached) {
-            jvm->DetachCurrentThread();
-        }
-        return false;
+        logMsg("\"networkRequest\" returned false");
+        return methodResult;
     }
 
-    /* 
-     * Get the appropriate class object field id
-     */
-    jfieldID fieldID = jniEnv->GetFieldID(interfaceClass, "rawDataBytes" , "[B");
-
-    if(!fieldID) {
-        logMsg("Failed getting rawDataOut fieldID\n");
-        if(isAttached) {
-            jvm->DetachCurrentThread();
-        }
-    }
-
-    // grab byteArray field
-    jfetchedBytes = (jbyteArray) jniEnv->GetObjectField(obj, fieldID);
-    int length = jniEnv->GetArrayLength(jfetchedBytes);
-    jbyte* const byte_array_start = jniEnv->GetByteArrayElements(jfetchedBytes, 0);
-    const char* byteCVal = (const char*)byte_array_start;
-    resultStr = std::string(byteCVal, length);
-    jniEnv->ReleaseByteArrayElements(jfetchedBytes, byte_array_start, 0);
-    _rawData << resultStr;
-
-    if(isAttached) {
-        jvm->DetachCurrentThread();
-    }
-
-    logMsg("Final Length: %d\n", resultStr.length());
-
-    return true;
+    return methodResult;
 }
-
 
 
 #endif
