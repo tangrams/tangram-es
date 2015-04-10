1a6f8aee8c3ba4b0af3eac7552d6a29bdf703c8d
Matt Blair <blair1618@gmail.com>
2015-04-10 13:28:41 -0400

    Use clang++ to build linux on Travis (already installed)

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -22,10 +22,9 @@ if [[ ${PLATFORM} == "linux" ]]; then
     sudo add-apt-repository -y ppa:kalakris/cmake > /dev/null
     sudo apt-get update -qq
     
-    #Install a c++11 compatible compiler
-    sudo apt-get install -y -qq gcc-4.8 g++-4.8
-    export CXX=g++-4.8
-    export CC=gcc-4.8
+    #Use a c++11 compatible compiler
+    export CXX=clang++-3.4
+    export CC=clang-3.4
     
     #Install X11, OpenGL, Doxygen, and CMake for GLFW
     sudo apt-get install -y -qq xorg-dev libglu1-mesa-dev doxygen cmake
