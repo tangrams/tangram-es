7f21bc787fea0f8bf9d2a42e0a75f7fc52d06947
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2015-04-29 08:43:39 -0400

    adding method to change urlTemplate

--- core/src/data/dataSource.cpp
+++ core/src/data/dataSource.cpp
@@ -10,12 +10,12 @@
 //---- DataSource Implementation----
 
 bool DataSource::hasTileData(const TileID& _tileID) {
-    
+
     return m_tileStore.find(_tileID) != m_tileStore.end();
 }
 
 std::shared_ptr<TileData> DataSource::getTileData(const TileID& _tileID) {
-    
+
     if (hasTileData(_tileID)) {
         std::shared_ptr<TileData> tileData = m_tileStore[_tileID];
         return tileData;
@@ -54,36 +54,36 @@ std::unique_ptr<std::string> NetworkDataSource::constructURL(const TileID& _tile
 
     size_t xpos = urlPtr->find("[x]");
     urlPtr->replace(xpos, 3, std::to_string(_tileCoord.x));
-    
+
     size_t ypos = urlPtr->find("[y]");
     urlPtr->replace(ypos, 3, std::to_string(_tileCoord.y));
-    
+
     size_t zpos = urlPtr->find("[z]");
     urlPtr->replace(zpos, 3, std::to_string(_tileCoord.z));
-    
+
     if (xpos == std::string::npos || ypos == std::string::npos || zpos == std::string::npos) {
         logMsg("Bad URL template!!\n");
     }
-    
+
     return std::move(urlPtr);
 }
 
 bool NetworkDataSource::loadTileData(const MapTile& _tile) {
-    
+
     bool success = true; // Begin optimistically
-    
+
     if (hasTileData(_tile.getID())) {
         // Tile has been fetched already!
         return success;
     }
-    
+
     std::unique_ptr<std::string> url = constructURL(_tile.getID());
 
     CURL* curlHandle = curl_easy_init();
 
     // out will store the stringStream contents from curl
     std::stringstream out;
-    
+
     // set up curl to perform fetch
     curl_easy_setopt(curlHandle, CURLOPT_WRITEFUNCTION, write_data);
     curl_easy_setopt(curlHandle, CURLOPT_WRITEDATA, &out);
@@ -92,35 +92,36 @@ bool NetworkDataSource::loadTileData(const MapTile& _tile) {
     curl_easy_setopt(curlHandle, CURLOPT_VERBOSE, 0L);
     curl_easy_setopt(curlHandle, CURLOPT_ACCEPT_ENCODING, "gzip");
     curl_easy_setopt(curlHandle, CURLOPT_NOSIGNAL, 1L);
-    
+
     logMsg("Fetching URL with curl: %s\n", url->c_str());
 
     CURLcode result = curl_easy_perform(curlHandle);
-    
+
     if (result != CURLE_OK) {
-        
+
         logMsg("curl_easy_perform failed: %s\n", curl_easy_strerror(result));
         success = false;
-        
+
     } else {
-        
+
         // parse fetched data
         std::shared_ptr<TileData> tileData = parse(_tile, out);
-        
+
         // Lock our mutex so that we can safely write to the tile store
         {
             std::lock_guard<std::mutex> lock(m_mutex);
             m_tileStore[_tile.getID()] = tileData;
         }
-        
+
     }
-    
+
     curl_easy_cleanup(curlHandle);
-    
+
     return success;
 }
 
+void NetworkDataSource::setUrlTemplate(const std::string &_urlTemplate){
+  m_urlTemplate = _urlTemplate;
+}
+
 //---- MapzenVectorTileJson Implementation----
-
-
-
--- core/src/data/dataSource.h
+++ core/src/data/dataSource.h
@@ -13,22 +13,22 @@ class MapTile;
 class DataSource {
 
 protected:
-    
+
     /* Map of tileIDs to data for that tile */
     std::map< TileID, std::shared_ptr<TileData> > m_tileStore;
-    
+
     std::mutex m_mutex; // Used to ensure safe access from async loading threads
-    
+
     /* Parse an I/O response into a <TileData>, returning an empty TileData on failure */
     virtual std::shared_ptr<TileData> parse(const MapTile& _tile, std::stringstream& _in) = 0;
-    
+
 public:
-    
+
     /* Fetch data for a map tile
      *
      * LoadTile performs synchronous I/O to retrieve all needed data for a tile,
      * then stores it to be accessed via <GetTileData>. This method SHALL NOT be called
-     * from the main thread. 
+     * from the main thread.
      */
     virtual bool loadTileData(const MapTile& _tile) = 0;
 
@@ -37,7 +37,7 @@ public:
 
     /* Checks if data exists for a specific <TileID> */
     virtual bool hasTileData(const TileID& _tileID);
-    
+
     /* Clears all data associated with this dataSource */
     void clearData();
 
@@ -49,9 +49,9 @@ class NetworkDataSource : public DataSource {
 
 protected:
 
-    /* URL template for network data sources 
+    /* URL template for network data sources
      *
-     * Network data sources must define a URL template including exactly one 
+     * Network data sources must define a URL template including exactly one
      * occurrance each of '[x]', '[y]', and '[z]' which will be replaced by
      * the x index, y index, and zoom level of tiles to produce their URL
      */
@@ -67,6 +67,8 @@ public:
 
     virtual bool loadTileData(const MapTile& _tile) override;
 
+    virtual void setUrlTemplate(const std::string& _urlTemplate);
+
 };
 
 // TODO: Support TopoJSON tiles
@@ -76,4 +78,3 @@ class TopoJsonNetSrc : public NetworkDataSource {
 // TODO: Support local GeoJSON tiles
 class GeoJsonFileSrc : public DataSource {
 };
-
