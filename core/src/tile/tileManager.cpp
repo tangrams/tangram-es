124efe8f0b966b8417d26059da9f327b27385f8f
Hannes Janetzek <hannes.janetzek@gmail.com>
2015-06-21 13:08:32 +0200

    add check in updateTileSet whether tiles could be removed

--- core/src/tile/tileManager.cpp
+++ core/src/tile/tileManager.cpp
@@ -95,6 +95,7 @@ void TileManager::updateTileSet() {
     }
     
     const std::set<TileID>& visibleTiles = m_view->getVisibleTiles();
+    bool cleanupTiles = false;
 
     // Loop over visibleTiles and add any needed tiles to tileSet
     {
@@ -119,13 +120,14 @@ void TileManager::updateTileSet() {
 
             } else {
                 // visibleTiles is missing an element present in tileSet (handled below)
+                cleanupTiles = true;
                 ++setTilesIter;
             }
         }
     }
 
     // Loop over tileSet and remove any tiles that are neither visible nor proxies
-    {
+    if (cleanupTiles) {
         auto setTilesIter = m_tileSet.begin();
         auto visTilesIter = visibleTiles.begin();
 
