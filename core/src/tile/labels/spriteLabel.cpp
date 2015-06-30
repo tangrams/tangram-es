f0c0f7e60833f5ccd84517b50e2311d07f44d8cd
Karim Naaji <karim.naaji@gmail.com>
2015-06-30 17:59:43 -0400

    init size of sprites

--- core/src/tile/labels/spriteLabel.cpp
+++ core/src/tile/labels/spriteLabel.cpp
@@ -1,9 +1,10 @@
 #include "spriteLabel.h"
 
 SpriteLabel::SpriteLabel(Label::Transform _transform) : Label(_transform, Label::Type::POINT) {
-
+    // TODO : get size
+    m_dim = {32, 32};
 }
 
 void SpriteLabel::pushTransform() {
-    
+    // TODO : update vbo mesh
 }
