c1597d65461310c4f9ad6d304b538769915e1b81
Karim Naaji <karim.naaji@gmail.com>
2015-07-08 17:58:00 -0400

    comments

--- core/src/util/typedMesh.h
+++ core/src/util/typedMesh.h
@@ -6,7 +6,7 @@ template<class T>
 class TypedMesh : public VboMesh {
 
 public:
-    
+
     TypedMesh(std::shared_ptr<VertexLayout> _vertexLayout, GLenum _drawMode, GLenum _hint = GL_STATIC_DRAW)
         : VboMesh(_vertexLayout, _drawMode, _hint) {};
 
@@ -22,38 +22,47 @@ public:
     virtual void compileVertexBuffer() override {
         compile(vertices, indices);
     }
-    
+
+    /*
+     * Update _nVerts vertices in the mesh with the new T value _newVertexValue starting after
+     * _byteOffset in the mesh vertex data memory
+     */
     void updateVertices(GLintptr _byteOffset, unsigned int _nVerts, const T& _newVertexValue);
-    
+
+    /*
+     * Update _nVerts vertices in the mesh with the new attribute A _newAttributeValue starting
+     * after _byteOffset in the mesh vertex data memory
+     */
     template<class A>
     void updateAttribute(GLintptr _byteOffset, unsigned int _nVerts, const A& _newAttributeValue) {
         if (!m_isCompiled) {
             return;
         }
-        
+
         size_t aSize = sizeof(A);
         size_t tSize = sizeof(T);
-        
-        // update the vertices
+
+        // update the vertices attributes
         for (size_t i = 0; i < _nVerts; ++i) {
             std::memcpy(m_glVertexData + _byteOffset + i * tSize, &_newAttributeValue, aSize);
         }
-        
+
         setDirty(_byteOffset + aSize, _nVerts * tSize + aSize);
     }
 
 protected:
-    
+
     void setDirty(GLintptr _byteOffset, GLsizei _byteSize);
-    
+
     std::vector<std::vector<T>> vertices;
     std::vector<std::vector<int>> indices;
-    
+
 };
 
 template<class T>
 void TypedMesh<T>::setDirty(GLintptr _byteOffset, GLsizei _byteSize) {
-    // not dirty, init the dirtyness of the buffer
+
+    // not dirty at all, init the dirtyness of the buffer
     if(!m_dirty) {
         m_dirtySize = _byteSize;
         m_dirtyOffset = _byteOffset;
@@ -63,20 +72,21 @@ void TypedMesh<T>::setDirty(GLintptr _byteOffset, GLsizei _byteSize) {
         long dBytes = std::abs(_byteOffset - m_dirtyOffset);
         long newEnd = _byteOffset + _byteSize;
         long oldEnd = m_dirtySize + m_dirtyOffset;
-        
+
         if(_byteOffset < m_dirtyOffset) {
-            // update from left part of the buffer
+            // update before the old buffer offset (left part of the buffer)
             m_dirtyOffset = _byteOffset;
-            
+
             // merge sizes
             if(newEnd > oldEnd) {
                 m_dirtySize = _byteSize;
             } else {
                 m_dirtySize += dBytes;
             }
+
             m_dirty = true;
         } else if(newEnd > oldEnd) {
-            // update from right part of the buffer
+            // update starting after the old buffer offset (right part of the buffer)
             m_dirtySize = dBytes + _byteSize;
             m_dirty = true;
         }
@@ -88,13 +98,13 @@ void TypedMesh<T>::updateVertices(GLintptr _byteOffset, unsigned int _nVerts, co
     if (!m_isCompiled) {
         return;
     }
-    
+
     size_t tSize = sizeof(T);
-    
+
     // update the vertices
     for (int i = 0; i < _nVerts; ++i) {
         std::memcpy(m_glVertexData + _byteOffset + i * tSize, &_newVertexValue, tSize);
     }
-    
+
     setDirty(_byteOffset, _nVerts * tSize);
 }
