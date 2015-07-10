1438249fc6f1732bbba03566a9c8a875cd9bd6b5
Matt Blair <blair1618@gmail.com>
2015-07-10 17:03:58 -0400

    Use travis to configure android sdks

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -5,15 +5,6 @@ set -o pipefail
 
 if [[ ${PLATFORM} == "android" ]]; then
 
-    ANDROID_SDK_VERSION="r24.0.2"
-    ANDROID_BUILD_TOOL_VERSION="21.1.2"
-    ANDROID_PLATFORM_VERSION="22"
-
-    # Install android sdk
-    wget https://dl-ssl.google.com/android/android-sdk_${ANDROID_SDK_VERSION}-linux.tgz
-    tar -zxf android-sdk_${ANDROID_SDK_VERSION}-linux.tgz
-    export ANDROID_HOME=$PWD/android-sdk-linux
-
     # Install android ndk
     echo "Cloning mindk..."
     git clone --quiet --depth 1 --branch linux-x86_64-api15-armv7 https://github.com/tangrams/mindk.git
@@ -25,13 +16,5 @@ if [[ ${PLATFORM} == "android" ]]; then
     export PATH=${PATH}:${ANDROID_HOME}/tools:${ANDROID_NDK}
     echo $PATH
     echo "Done."
-
-    # Install required Android components; automatically accept the license prompt
-    echo "Updating Android SDK..."
-    echo "y" | android update sdk --all --filter platform-tools,build-tools-${ANDROID_BUILD_TOOL_VERSION},android-${ANDROID_PLATFORM_VERSION},extra-android-support --no-ui --force >/dev/null
-    echo "Done."
-
-    # Make sure gradlew is executable
-    chmod +x android/gradlew
 
 fi
