9cffe4f953aa0290838f5162eb6ae8bd1b496556
Karim Naaji <karim.naaji@gmail.com>
2015-07-15 17:55:25 -0400

    fix cache miss

--- core/src/tile/labels/spriteLabel.cpp
+++ core/src/tile/labels/spriteLabel.cpp
@@ -4,18 +4,30 @@ SpriteLabel::SpriteLabel(Label::Transform _transform, const glm::vec2& _size, co
     Label(_transform, Label::Type::POINT),
     m_offset(_offset),
     m_attribOffsets(_attribOffsets) {
-        
+
     m_dim = _size;
 }
 
 void SpriteLabel::pushTransform(VboMesh& _mesh) {
     if (m_dirty) {
         TypedMesh<BufferVert>& mesh = static_cast<TypedMesh<BufferVert>&>(_mesh);
-        int memOffset = m_attribOffsets.memOffset;
-        
-        mesh.updateAttribute(m_attribOffsets.m_position + memOffset, 4, glm::vec2(m_transform.m_screenPosition.x, m_transform.m_screenPosition.y) + m_offset);
-        mesh.updateAttribute(m_attribOffsets.m_alpha + memOffset, 4, m_transform.m_alpha);
-        mesh.updateAttribute(m_attribOffsets.m_rotation + memOffset, 4, m_transform.m_rotation);
+
+        // used to write all attributes memory at once, good for caching
+        struct VertexAttributes {
+            glm::vec2 screenPos;
+            float alpha;
+            float rot;
+        };
+
+        GLintptr stride = std::min<GLintptr>(m_attribOffsets.m_position, std::min<GLintptr>(m_attribOffsets.m_alpha, m_attribOffsets.m_rotation));
+
+        VertexAttributes newAttributes {
+            m_transform.m_screenPosition + m_offset,
+            m_transform.m_alpha,
+            m_transform.m_rotation,
+        };
+
+        mesh.updateAttribute(stride + m_attribOffsets.memOffset, 4, newAttributes);
     }
 }
 
