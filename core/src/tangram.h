635cf885c4d67ec0c7f3483670d705de4ff9d213
Matt Blair <blair1618@gmail.com>
2015-07-22 15:08:51 -0400

    Remove indentation within namespaces

--- core/src/tangram.h
+++ core/src/tangram.h
@@ -9,74 +9,74 @@
 
 namespace Tangram {
 
-    // Create resources and initialize the map view
-    void initialize();
+// Create resources and initialize the map view
+void initialize();
 
-    // Resize the map view to a new width and height (in pixels)
-    void resize(int _newWidth, int _newHeight);
+// Resize the map view to a new width and height (in pixels)
+void resize(int _newWidth, int _newHeight);
 
-    // Update the map state with the time interval since the last update
-    void update(float _dt);
+// Update the map state with the time interval since the last update
+void update(float _dt);
 
-    // Render a new frame of the map view (if needed)
-    void render();
+// Render a new frame of the map view (if needed)
+void render();
 
-    // Set the position of the map view in degrees longitude and latitude
-    void setPosition(double _lon, double _lat);
+// Set the position of the map view in degrees longitude and latitude
+void setPosition(double _lon, double _lat);
 
-    // Set the values of the arguments to the position of the map view in degrees longitude and latitude
-    void getPosition(double& _lon, double& _lat);
+// Set the values of the arguments to the position of the map view in degrees longitude and latitude
+void getPosition(double& _lon, double& _lat);
 
-    // Set the fractional zoom level of the view
-    void setZoom(float _z);
+// Set the fractional zoom level of the view
+void setZoom(float _z);
 
-    // Get the fractional zoom level of the view
-    float getZoom();
+// Get the fractional zoom level of the view
+float getZoom();
 
-    // Set the counter-clockwise rotation of the view in radians; 0 corresponds to North pointing up
-    void setRotation(float _radians);
+// Set the counter-clockwise rotation of the view in radians; 0 corresponds to North pointing up
+void setRotation(float _radians);
 
-    // Get the counter-clockwise rotation of the view in radians; 0 corresponds to North pointing up
-    float getRotation();
+// Get the counter-clockwise rotation of the view in radians; 0 corresponds to North pointing up
+float getRotation();
 
-    // Set the tilt angle of the view in radians; 0 corresponds to straight down
-    void setTilt(float _radians);
+// Set the tilt angle of the view in radians; 0 corresponds to straight down
+void setTilt(float _radians);
 
-    // Get the tilt angle of the view in radians; 0 corresponds to straight down
-    float getTilt();
+// Get the tilt angle of the view in radians; 0 corresponds to straight down
+float getTilt();
 
-    // Transform coordinates in screen space (x right, y down) into their longitude and latitude in the map view
-    void screenToWorldCoordinates(double& _x, double& _y);
+// Transform coordinates in screen space (x right, y down) into their longitude and latitude in the map view
+void screenToWorldCoordinates(double& _x, double& _y);
 
-    // Invalidate and re-create all OpenGL resources
-    void onContextDestroyed();
+// Invalidate and re-create all OpenGL resources
+void onContextDestroyed();
 
-    // Set the ratio of hardware pixels to logical pixels (defaults to 1.0)
-    void setPixelScale(float _pixelsPerPoint);
+// Set the ratio of hardware pixels to logical pixels (defaults to 1.0)
+void setPixelScale(float _pixelsPerPoint);
 
-    // Respond to a tap at the given screen coordinates (x right, y down)
-    void handleTapGesture(float _posX, float _posY);
+// Respond to a tap at the given screen coordinates (x right, y down)
+void handleTapGesture(float _posX, float _posY);
 
-    // Respond to a double tap at the given screen coordinates (x right, y down)
-    void handleDoubleTapGesture(float _posX, float _posY);
+// Respond to a double tap at the given screen coordinates (x right, y down)
+void handleDoubleTapGesture(float _posX, float _posY);
 
-    // Respond to a drag with the given displacement in screen coordinates (x right, y down)
-    void handlePanGesture(float _startX, float _startY, float _endX, float _endY);
+// Respond to a drag with the given displacement in screen coordinates (x right, y down)
+void handlePanGesture(float _startX, float _startY, float _endX, float _endY);
 
-    // Respond to a pinch at the given position in screen coordinates with the given incremental scale
-    void handlePinchGesture(float _posX, float _posY, float _scale);
+// Respond to a pinch at the given position in screen coordinates with the given incremental scale
+void handlePinchGesture(float _posX, float _posY, float _scale);
 
-    // Respond to a rotation gesture with the given incremental rotation in radians
-    void handleRotateGesture(float _posX, float _posY, float _rotation);
+// Respond to a rotation gesture with the given incremental rotation in radians
+void handleRotateGesture(float _posX, float _posY, float _rotation);
 
-    // Respond to a two-finger shove with the given distance
-    void handleShoveGesture(float _distance);
-    
-    // Set debug features on or off using a boolean (see debug.h)
-    void setDebugFlag(DebugFlags _flag, bool _on);
-    
-    // Get the boolean state of a debug feature (see debug.h)
-    bool getDebugFlag(DebugFlags _flag);
+// Respond to a two-finger shove with the given distance
+void handleShoveGesture(float _distance);
+
+// Set debug features on or off using a boolean (see debug.h)
+void setDebugFlag(DebugFlags _flag, bool _on);
+
+// Get the boolean state of a debug feature (see debug.h)
+bool getDebugFlag(DebugFlags _flag);
 
 }
 
--- core/src/util/geoJson.h
+++ core/src/util/geoJson.h
@@ -11,15 +11,15 @@ class Tile;
 
 namespace GeoJson {
 
-    void extractPoint(const rapidjson::Value& _in, Point& _out, const Tile& _tile);
+void extractPoint(const rapidjson::Value& _in, Point& _out, const Tile& _tile);
 
-    void extractLine(const rapidjson::Value& _in, Line& _out, const Tile& _tile);
+void extractLine(const rapidjson::Value& _in, Line& _out, const Tile& _tile);
 
-    void extractPoly(const rapidjson::Value& _in, Polygon& _out, const Tile& _tile);
+void extractPoly(const rapidjson::Value& _in, Polygon& _out, const Tile& _tile);
 
-    void extractFeature(const rapidjson::Value& _in, Feature& _out, const Tile& _tile);
+void extractFeature(const rapidjson::Value& _in, Feature& _out, const Tile& _tile);
 
-    void extractLayer(const rapidjson::Value& _in, Layer& _out, const Tile& _tile);
+void extractLayer(const rapidjson::Value& _in, Layer& _out, const Tile& _tile);
 
 }
 
