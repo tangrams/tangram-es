d544033c827e7ca101c7e6d8bef26bafac9dbb2f
Karim Naaji <karim.naaji@gmail.com>
2015-07-23 14:28:17 -0400

    test some benchmarks

--- bench/loading.cpp
+++ bench/loading.cpp
@@ -4,18 +4,19 @@
 #include "text/fontContext.h"
 
 #include "benchmark/benchmark_api.h"
+#include "benchmark/benchmark.h"
 
 using namespace Tangram;
 
 static void BM_Tangram_LoadFont(benchmark::State& state) {
     while(state.KeepRunning()) {
+        state.PauseTiming();
         std::shared_ptr<FontContext> m_ftContext;
-
         m_ftContext = std::make_shared<FontContext>();
+        state.ResumeTiming();
         m_ftContext->addFont("FiraSans-Medium.ttf", "FiraSans");
     }
 }
-
 BENCHMARK(BM_Tangram_LoadFont);
 
 BENCHMARK_MAIN();
