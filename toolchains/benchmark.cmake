7170e3a7648e32f174705f3e42bfd6a8fedc4f45
Karim Naaji <karim.naaji@gmail.com>
2015-07-23 12:16:06 -0400

    add benchmark library as submodule

---
+++ toolchains/benchmark.cmake
@@ -0,0 +1,37 @@
+# options
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -stdlib=libc++ -std=c++11")
+
+add_definitions(-DPLATFORM_OSX)
+
+# load glfw
+include(${PROJECT_SOURCE_DIR}/toolchains/add_glfw.cmake)
+
+# load core library
+add_subdirectory(${PROJECT_SOURCE_DIR}/core)
+include_directories(${CORE_INCLUDE_DIRS})
+include_directories(${CORE_LIBRARIES_INCLUDE_DIRS})
+
+add_subdirectory(${PROJECT_SOURCE_DIR}/core/dependencies/benchmark)
+
+set(OSX_PLATFORM_SRC ${PROJECT_SOURCE_DIR}/osx/src/platform_osx.mm)
+
+file(GLOB BENCH_SOURCES bench/*.cpp)
+
+# create an executable per test
+#foreach(_src_file_path ${BENCH_SOURCES})
+#    string(REPLACE ".cpp" "" test_case ${_src_file_path})
+#    string(REGEX MATCH "([^/]*)$" test_name ${test_case})
+#
+#    set(EXECUTABLE_NAME "${test_name}.out")
+#
+#    add_executable(${EXECUTABLE_NAME} ${_src_file_path} ${OSX_PLATFORM_SRC})
+#
+#    target_link_libraries(${EXECUTABLE_NAME} core glfw ${GLFW_LIBRARIES})
+#
+#endforeach(_src_file_path ${TEST_SOURCES})
+
+# copy resources in order to make tests with resources dependency
+file(GLOB_RECURSE RESOURCES ${PROJECT_SOURCE_DIR}/core/resources/*)
+foreach(_resource ${RESOURCES})
+    file(COPY ${_resource} DESTINATION ${PROJECT_SOURCE_DIR}/build/bench/bin)
+endforeach()
