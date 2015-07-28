91110c56b8a7267d59be55854fa0327cd5629922
Hannes Janetzek <hannes.janetzek@gmail.com>
2015-07-29 01:46:35 +0200

    add linux cmake, compile benchmarks in Release mode

--- toolchains/linux.benchmark.cmake
+++ toolchains/linux.benchmark.cmake
@@ -1,7 +1,7 @@
 # options
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -stdlib=libc++ -std=c++11")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -std=c++11")
 
-add_definitions(-DPLATFORM_OSX)
+add_definitions(-DPLATFORM_LINUX)
 
 # load glfw
 include(${PROJECT_SOURCE_DIR}/toolchains/add_glfw.cmake)
@@ -13,24 +13,26 @@ include_directories(${CORE_LIBRARIES_INCLUDE_DIRS})
 
 add_subdirectory(${PROJECT_SOURCE_DIR}/core/dependencies/benchmark)
 
-set(OSX_PLATFORM_SRC ${PROJECT_SOURCE_DIR}/osx/src/platform_osx.mm)
+set(LINUX_PLATFORM_SRC
+  ${PROJECT_SOURCE_DIR}/linux/src/platform_linux.cpp
+  ${PROJECT_SOURCE_DIR}/linux/src/urlWorker.cpp
+)
 
 file(GLOB BENCH_SOURCES bench/*.cpp)
 
-# create an executable per benchmark
+# create an executable per test
 foreach(_src_file_path ${BENCH_SOURCES})
     string(REPLACE ".cpp" "" bench ${_src_file_path})
     string(REGEX MATCH "([^/]*)$" bench_name ${bench})
 
-    set(EXECUTABLE_NAME "${bench_name}_bench.out")
+    set(EXECUTABLE_NAME "${bench_name}.out")
 
-    add_executable(${EXECUTABLE_NAME} ${_src_file_path} ${OSX_PLATFORM_SRC})
+    add_executable(${EXECUTABLE_NAME} ${_src_file_path} ${LINUX_PLATFORM_SRC})
 
-    target_link_libraries(${EXECUTABLE_NAME} benchmark core glfw ${GLFW_LIBRARIES})
-
+    target_link_libraries(${EXECUTABLE_NAME} benchmark core -lcurl glfw ${GLFW_LIBRARIES})
 endforeach(_src_file_path ${BENCH_SOURCES})
 
-# copy resources in order to make tests benchmark resources dependency
+# copy resources in order to make tests with resources dependency
 file(GLOB_RECURSE RESOURCES ${PROJECT_SOURCE_DIR}/core/resources/*)
 foreach(_resource ${RESOURCES})
     file(COPY ${_resource} DESTINATION ${PROJECT_SOURCE_DIR}/build/bench/bin)
