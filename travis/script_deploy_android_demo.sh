416764e104331b29ec36862f4c51b2049ddc122d
Chuck Greb <charles.greb@gmail.com>
2015-07-31 16:20:30 -0400

    Install s3cmd via addon package in travis.yml

--- travis/script_deploy_android_demo.sh
+++ travis/script_deploy_android_demo.sh
@@ -4,8 +4,6 @@ set -e
 set -o pipefail
 
 if [ "${PLATFORM}" = "android" ]; then
-    apt-get update
-    apt-get install s3cmd
     printf "[default]\naccess_key = $S3_ACCESS_KEY\n secret_key = $S3_SECRET_KEY" > ~/.s3cfg
 
     if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
