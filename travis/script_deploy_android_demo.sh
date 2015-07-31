ec051a7d8323ac8cbf7dd5ad3976466f4a4bd8be
Chuck Greb <charles.greb@gmail.com>
2015-07-31 15:56:37 -0400

    Update script_deploy_android_demo.sh

--- travis/script_deploy_android_demo.sh
+++ travis/script_deploy_android_demo.sh
@@ -3,12 +3,11 @@
 set -e
 set -o pipefail
 
-# Install s3cmd
-sudo apt-get update
-sudo apt-get install s3cmd
-printf "[default]\naccess_key = $S3_ACCESS_KEY\n secret_key = $S3_SECRET_KEY" > ~/.s3cfg
-
 if [ "${PLATFORM}" = "android" ]; then
+    apt-get update
+    apt-get install s3cmd
+    printf "[default]\naccess_key = $S3_ACCESS_KEY\n secret_key = $S3_SECRET_KEY" > ~/.s3cfg
+
     if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
         s3cmd put android/demo/build/outputs/apk/*.apk s3://android.mapzen.com/tangram/latest.apk
         s3cmd put android/demo/build/outputs/apk/*.apk s3://android.mapzen.com/tangram-snapshots/master-$TRAVIS_BUILD_NUMBER.apk
