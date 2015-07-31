a81ea52847793e7efe5bb41b9e4f3a1a6e87cb87
Chuck Greb <charles.greb@gmail.com>
2015-07-31 14:38:46 -0400

    Deploy Android demo app to s3

---
+++ travis/script_deploy_android_demo.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+# Install s3cmd
+sudo apt-get update
+sudo apt-get install s3cmd
+printf "[default]\naccess_key = $S3_ACCESS_KEY\n secret_key = $S3_SECRET_KEY" > ~/.s3cfg
+
+if [ "${PLATFORM}" = "android" ]; then
+    if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
+        s3cmd put android/demo/build/outputs/apk/*.apk s3://android.mapzen.com/tangram/latest.apk
+        s3cmd put android/demo/build/outputs/apk/*.apk s3://android.mapzen.com/tangram-snapshots/master-$TRAVIS_BUILD_NUMBER.apk
+    else
+        s3cmd put android/demo/build/outputs/apk/*.apk s3://android.mapzen.com/tangram-development/$TRAVIS_BRANCH-$TRAVIS_BUILD_NUMBER.apk
+    fi
+fi
