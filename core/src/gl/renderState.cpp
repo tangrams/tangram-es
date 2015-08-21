4ef6f80be24044c29f6240aae39253f7606b7102
Karim Naaji <karim.naaji@gmail.com>
2015-08-21 18:08:40 -0400

    force invalidation of state wrap the index buffer/vertex buffer was not invalidated on context loss

--- core/src/gl/renderState.cpp
+++ core/src/gl/renderState.cpp
@@ -36,6 +36,9 @@ namespace RenderState {
         glClearDepthf(1.0);
         glDepthRangef(0.0, 1.0);
         glClearColor(0.3, 0.3, 0.3, 1.0);
+
+        vertexBuffer(-1);
+        indexBuffer(-1);
     }
 
 }
