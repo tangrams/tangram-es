087ca2a5cff7f91a3308c8d77c989ec0f10cb01c
Varun Talwar <tallytalwar@gmail.com>
2015-08-21 13:46:42 -0400

    addLabel requires font config
    - instead of doing font configuration on text buffer init, font are configured based on styling rules for the text style
            - no use of TextBuffer::init
            - TextBuffer::addLabel takes font parameters to use apt styled fonts

--- core/src/text/textBuffer.h
+++ core/src/text/textBuffer.h
@@ -22,17 +22,11 @@ public:
     TextBuffer(std::shared_ptr<VertexLayout> _vertexLayout);
     ~TextBuffer();
 
-    /* Set <TextBuffer> options for subsequent added labels */
-    void init(FontID _fontID, float _size, float _blurSpread);
-
     /* Create and add TextLabel */
-    bool addLabel(const std::string& _text, Label::Transform _transform, Label::Type _type, Label::Options _options);
+    bool addLabel(const std::string& _text, Label::Transform _transform, Label::Type _type, const std::string& _fontName, const float _fontSize, const float _blurSpread, Label::Options _options);
 
 private:
 
-    FontID m_fontID;
-    float m_fontSize;
-    float m_fontBlurSpread;
 
     bool m_dirtyTransform;
 };
