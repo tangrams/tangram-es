53a6ef937b653c1ba324d222f0dfc9f50adac394
Karim Naaji <karim.naaji@gmail.com>
2015-08-27 16:55:23 -0400

    improve sdf shader - automatic blur factor induced from font size - automatic smooth factor in shader

--- core/resources/shaders/sdf.fs
+++ core/resources/shaders/sdf.fs
@@ -13,37 +13,40 @@ precision mediump float;
 #define LOWP
 #endif
 
+// TODO: use this as attribute
+const float textSize = 15.0;
+const float emSize = textSize / 16.0;
+
 uniform sampler2D u_tex;
 
 varying vec2 v_uv;
 varying float v_alpha;
 varying vec4 v_color;
 
-const float tint = 1.8;
-const float sdf = 0.8;
-
-float contour(in float d, in float w, in float off) {
-    return smoothstep(off - w, off + w, d);
+float contour(in float d, in float w) {
+    return smoothstep(0.5 - w, 0.5 + w, d);
 }
 
-float sample(in vec2 uv, float w, in float off) {
-    return contour(texture2D(u_tex, uv).a, w, off);
+float sample(in vec2 uv, float w) {
+    return contour(texture2D(u_tex, uv).a, w);
 }
 
-float sampleAlpha(in vec2 uv, float distance, in float off) {
-    float alpha = contour(distance, distance, off);
+float sampleAlpha(in vec2 uv, float distance) {
+    const float smooth = 0.0625 * emSize; // 0.0625 = 1.0/1em ratio
+    float alpha = contour(distance, smooth);
 
 #ifdef TANGRAM_SDF_MULTISAMPLING
+    const float aaSmooth = smooth / 2.0;
     float dscale = 0.354; // 1 / sqrt(2)
     vec2 duv = dscale * (dFdx(uv) + dFdy(uv));
     vec4 box = vec4(uv - duv, uv + duv);
 
-    float asum = sample(box.xy, distance, off)
-        + sample(box.zw, distance, off)
-        + sample(box.xw, distance, off)
-        + sample(box.zy, distance, off);
+    float asum = sample(box.xy, aaSmooth)
+        + sample(box.zw, aaSmooth)
+        + sample(box.xw, aaSmooth)
+        + sample(box.zy, aaSmooth);
 
-    alpha = (alpha + 0.5 * asum) / 2.0;
+    alpha = mix(alpha, asum, 0.25);
 #endif
 
     return alpha;
@@ -54,7 +57,8 @@ void main(void) {
         discard;
     } else {
         float distance = texture2D(u_tex, v_uv).a;
-        float alpha = sampleAlpha(v_uv, distance, sdf) * tint;
+
+        float alpha = sampleAlpha(v_uv, distance);
         alpha = pow(alpha, 0.4545);
 
         gl_FragColor = vec4(v_color.rgb, v_alpha * alpha * v_color.a);
