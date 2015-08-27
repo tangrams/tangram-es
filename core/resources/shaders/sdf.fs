6ad1ece9794683ac847f825f316775fbef9de1eb
Karim Naaji <karim.naaji@gmail.com>
2015-08-27 12:11:49 -0400

    Misc fixes:
    - fix: add missing break
    - fix: add missing unlock
    - trim spaces

--- core/resources/shaders/sdf.fs
+++ core/resources/shaders/sdf.fs
@@ -1,9 +1,9 @@
 #ifdef TANGRAM_SDF_MULTISAMPLING
-    #ifdef GL_OES_standard_derivatives
-        #extension GL_OES_standard_derivatives : enable
-    #else
-        #undef TANGRAM_SDF_MULTISAMPLING
-    #endif
+#ifdef GL_OES_standard_derivatives
+#extension GL_OES_standard_derivatives : enable
+#else
+#undef TANGRAM_SDF_MULTISAMPLING
+#endif
 #endif
 
 #ifdef GL_ES
@@ -33,31 +33,31 @@ float sample(in vec2 uv, float w, in float off) {
 float sampleAlpha(in vec2 uv, float distance, in float off) {
     float alpha = contour(distance, distance, off);
 
-    #ifdef TANGRAM_SDF_MULTISAMPLING
-        float dscale = 0.354; // 1 / sqrt(2)
-        vec2 duv = dscale * (dFdx(uv) + dFdy(uv));
-        vec4 box = vec4(uv - duv, uv + duv);
+#ifdef TANGRAM_SDF_MULTISAMPLING
+    float dscale = 0.354; // 1 / sqrt(2)
+    vec2 duv = dscale * (dFdx(uv) + dFdy(uv));
+    vec4 box = vec4(uv - duv, uv + duv);
 
-        float asum = sample(box.xy, distance, off)
-                   + sample(box.zw, distance, off)
-                   + sample(box.xw, distance, off)
-                   + sample(box.zy, distance, off);
+    float asum = sample(box.xy, distance, off)
+        + sample(box.zw, distance, off)
+        + sample(box.xw, distance, off)
+        + sample(box.zy, distance, off);
 
-        alpha = (alpha + 0.5 * asum) / 2.0;
-    #endif
+    alpha = (alpha + 0.5 * asum) / 2.0;
+#endif
 
     return alpha;
 }
 
 void main(void) {
-	if (v_alpha < TANGRAM_EPSILON) {
+    if (v_alpha < TANGRAM_EPSILON) {
         discard;
-	} else {
-	    float distance = texture2D(u_tex, v_uv).a;
-    	float alpha = sampleAlpha(v_uv, distance, sdf) * tint;
-    	alpha = pow(alpha, 0.4545);
+    } else {
+        float distance = texture2D(u_tex, v_uv).a;
+        float alpha = sampleAlpha(v_uv, distance, sdf) * tint;
+        alpha = pow(alpha, 0.4545);
 
-    	gl_FragColor = vec4(v_color.rgb, v_alpha * alpha * v_color.a);
-	}
+        gl_FragColor = vec4(v_color.rgb, v_alpha * alpha * v_color.a);
+    }
 }
 
