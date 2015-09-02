40294440b752d81f32374d8420a5a50fc54c35ac
Varun Talwar <tallytalwar@gmail.com>
2015-09-02 18:16:58 -0400

    More java fixes

--- android/tangram/src/com/mapzen/tangram/FontFileParser.java
+++ android/tangram/src/com/mapzen/tangram/FontFileParser.java
@@ -97,7 +97,7 @@ class FontFileParser {
             fileXml = systemFontFile.getAbsolutePath();
         }
 
-        if(fileXml == "") {
+        if("".equals(fileXml)) {
             return;
         }
 
