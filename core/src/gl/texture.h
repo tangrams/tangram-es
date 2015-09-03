6dfbd49e6b72f1d151457a9e28e900c1917a7e11
Hannes Janetzek <hannes.janetzek@gmail.com>
2015-09-03 10:02:25 +0200

    use deque for texture updates

--- core/src/gl/texture.h
+++ core/src/gl/texture.h
@@ -3,7 +3,7 @@
 #include "gl.h"
 
 #include <vector>
-#include <queue>
+#include <deque>
 #include <memory>
 #include <string>
 
@@ -63,7 +63,8 @@ public:
     void setData(const GLuint* _data, unsigned int _dataSize);
 
     /* Update a region of the texture */
-    void setSubData(const GLuint* _subData, unsigned int _xoff, unsigned int _yoff, unsigned int _width, unsigned int _height);
+    void setSubData(const GLuint* _subData, unsigned int _xoff, unsigned int _yoff,
+                    unsigned int _width, unsigned int _height);
 
     typedef std::pair<GLuint, GLuint> TextureSlot;
 
@@ -102,7 +103,7 @@ private:
     bool m_generateMipmaps;
 
     // used to queue the subdata updates, each call of setSubData would be treated in the order that they arrived
-    std::queue<TextureSubData> m_subData;
+    std::deque<TextureSubData> m_subData;
 };
 
 }
