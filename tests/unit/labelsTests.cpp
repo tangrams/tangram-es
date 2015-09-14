6e45e02c0ee34b04557876d4eb0808c67548716d
Hannes Janetzek <hannes.janetzek@gmail.com>
2015-09-14 13:00:23 +0200

    more tests

--- tests/unit/labelsTests.cpp
+++ tests/unit/labelsTests.cpp
@@ -16,16 +16,17 @@ namespace Tangram {
 glm::vec2 screenSize(256.f, 256.f);
 TextBuffer dummy(nullptr);
 
-std::unique_ptr<TextLabel> makeLabel(Label::Transform _transform, Label::Type _type) {
+std::unique_ptr<TextLabel> makeLabel(Label::Transform _transform, Label::Type _type, std::string id) {
     Label::Options options;
     options.color = 0xff;
     options.offset = {0.0f, 0.0f};
+    options.id = id;
     return std::unique_ptr<TextLabel>(new TextLabel("label",
                                                     _transform, _type, {0, 0},
                                                     dummy, {0, 0}, options));
 }
 
-TEST_CASE("", "") {
+TEST_CASE("Test getFeaturesAtPoint", "[Labels][FeaturePicking]") {
     std::unique_ptr<Labels> labels(new Labels());
 
     View view(256, 256);
@@ -36,7 +37,9 @@ TEST_CASE("", "") {
     auto labelMesh = std::unique_ptr<LabelMesh>(new LabelMesh(nullptr, 0));
     auto textStyle = std::unique_ptr<TextStyle>(new TextStyle("test"));
 
-    labelMesh->addLabel(makeLabel(glm::vec2{0,0}, Label::Type::point));
+    labelMesh->addLabel(makeLabel(glm::vec2{0,0}, Label::Type::point, "0"));
+    labelMesh->addLabel(makeLabel(glm::vec2{1,-1}, Label::Type::point, "1"));
+    labelMesh->addLabel(makeLabel(glm::vec2{1,1}, Label::Type::point, "2"));
 
     std::shared_ptr<Tile> tile(new Tile({0,0,0}, view.getMapProjection()));
     tile->getMesh(*textStyle.get()) = std::move(labelMesh);
@@ -48,11 +51,22 @@ TEST_CASE("", "") {
     std::vector<std::shared_ptr<Tile>> tiles;
 
     tiles.push_back(tile);
+    {
+        auto& items = labels->getFeaturesAtPoint(view, 0, styles, tiles, 128, 128);
+        REQUIRE(items.size() == 1);
+        REQUIRE(items[0].id == "0");
+    }
+    {
+        auto& items = labels->getFeaturesAtPoint(view, 0, styles, tiles, 256, 256);
+        REQUIRE(items.size() == 1);
+        REQUIRE(items[0].id == "1");
+    }
 
-    auto& items = labels->getFeaturesAtPoint(view, 0, styles, tiles, 128, 128);
-
-    REQUIRE(items.size() == 1);
-
+    {
+        auto& items = labels->getFeaturesAtPoint(view, 0, styles, tiles, 256, 0);
+        REQUIRE(items.size() == 1);
+        REQUIRE(items[0].id == "2");
+    }
 }
 
 }
