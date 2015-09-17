b19efab1b200efd0ba15215978a9560d7882c9bb
Matt Blair <blair1618@gmail.com>
2015-09-17 19:54:29 -0400

    Fix stops tests

--- tests/unit/stopsTests.cpp
+++ tests/unit/stopsTests.cpp
@@ -6,47 +6,45 @@
 using namespace Tangram;
 
 Stops instance_float() {
-    Stops stops;
-    stops.frames = { Frame(0, 0.f), Frame(1, 10.f), Frame(5, 50.f), Frame(7, 0.f) };
-    return stops;
+    return Stops({
+        Frame(0, 0.f),
+        Frame(1, 10.f),
+        Frame(5, 50.f),
+        Frame(7, 0.f)
+    });
 }
 
 Stops instance_color() {
-    Stops stops;
-    stops.frames = { Frame(0, 0xffffffff), Frame(1, 0xeeeeeeee), Frame(5, 0xaaaaaaaa) };
-    return stops;
+    return Stops({
+        Frame(0, Color(0xff, 0xff, 0xff, 1.)),
+        Frame(1, Color(0xee, 0xee, 0xee, 1.)),
+        Frame(5, Color(0xaa, 0xaa, 0xaa, 1.))
+    });
 }
 
-TEST_CASE( "Stops evaluate float values correctly at key frames", "[Stops]" ) {
-
-    auto stops = instance_float();
-
-    REQUIRE(stops.evalFloat(0) == 0.f);
-    REQUIRE(stops.evalFloat(1) == 10.f);
-    REQUIRE(stops.evalFloat(5) == 50.f);
-    REQUIRE(stops.evalFloat(7) == 0.f);
-
-}
-
-TEST_CASE( "Stops evaluate float values correctly between key frames", "[Stops]" ) {
+TEST_CASE( "Stops evaluate float values correctly at and between key frames", "[Stops]" ) {
 
     auto stops = instance_float();
 
     REQUIRE(stops.evalFloat(-3) == 0.f);
+    REQUIRE(stops.evalFloat(0) == 0.f);
     REQUIRE(stops.evalFloat(0.3) == 3.f);
+    REQUIRE(stops.evalFloat(1) == 10.f);
     REQUIRE(stops.evalFloat(3) == 30.f);
+    REQUIRE(stops.evalFloat(5) == 50.f);
     REQUIRE(stops.evalFloat(6) == 25.f);
+    REQUIRE(stops.evalFloat(7) == 0.f);
     REQUIRE(stops.evalFloat(8) == 0.f);
 
 }
 
-TEST_CASE( "Stops evaluates color values correctly at key frames", "[Stops]" ) {
+TEST_CASE( "Stops evaluate color values correctly at and between key frames", "[Stops]" ) {
 
     auto stops = instance_color();
 
     REQUIRE(stops.evalColor(-1) == 0xffffffff);
-    REQUIRE(stops.evalColor(1) == 0xeeeeeeee);
-    REQUIRE(stops.evalColor(2) == 0xaaaaaaaa);
-    REQUIRE(stops.evalColor(7) == 0xaaaaaaaa);
+    REQUIRE(stops.evalColor(1) == 0xffeeeeee);
+    REQUIRE(stops.evalColor(2) == 0xffdddddd);
+    REQUIRE(stops.evalColor(7) == 0xffaaaaaa);
 
 }
