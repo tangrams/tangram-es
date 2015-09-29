1e9e732ab99380ef6cf406905e26e7771b857f43
Varun Talwar <tallytalwar@gmail.com>
2015-09-29 17:11:12 -0400

    white space/tabs alignment

--- core/src/scene/sceneLoader.h
+++ core/src/scene/sceneLoader.h
@@ -33,7 +33,7 @@ using StyleUniforms = std::pair<std::string, std::vector<UniformValue>>;
 class SceneLoader {
 
     static void loadBackground(YAML::Node background, Scene& scene);
-	static void loadSources(YAML::Node sources, Scene& scene);
+    static void loadSources(YAML::Node sources, Scene& scene);
     static void loadFont(YAML::Node fontProps);
     static void loadLights(YAML::Node lights, Scene& scene);
     static void loadCameras(YAML::Node cameras, Scene& scene);
