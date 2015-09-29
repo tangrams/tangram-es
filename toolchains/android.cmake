1c9d6e7374db882ebff08ffb44fd0a69f3bc8541
Matt Blair <blair1618@gmail.com>
2015-09-29 19:19:21 -0400

    Silence warnings for gnu-anonymous-struct and nested-anon-types

--- toolchains/android.cmake
+++ toolchains/android.cmake
@@ -13,6 +13,12 @@ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -std=c++1y -pedantic -llog -lz -la
 set(CXX_FLAGS_DEBUG "${CXX_FLAGS_DEBUG} -g -O0")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -fPIC")
 
+# For CMake 3.0
+if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+    # using regular Clang or AppleClang
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-gnu-anonymous-struct -Wno-nested-anon-types")
+endif()
+
 # adding manually the two jni c++ files
 set(ADDITIONAL_TARGET_DEPENDENT_SRC_FILES
      ${CMAKE_SOURCE_DIR}/android/tangram/jni/jniExports.cpp
