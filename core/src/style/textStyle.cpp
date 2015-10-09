7c268e1fb99c91d5dfe16d727c5ddef95b7bd884
Karim Naaji <karim.naaji@gmail.com>
2015-10-09 16:25:23 -0400

    Don't scale stroke width by pixel ratio

--- core/src/style/textStyle.cpp
+++ core/src/style/textStyle.cpp
@@ -105,7 +105,6 @@ Parameters TextStyle::applyRule(const DrawRule& _rule, const Properties& _props)
     /* Global operations done for fontsize and sdfblur */
     float emSize = p.fontSize / 16.f;
     p.fontSize *= m_pixelScale;
-    p.strokeWidth *= m_pixelScale;
     p.blurSpread = m_sdf ? emSize * 5.0f : 0.0f;
 
     return p;
