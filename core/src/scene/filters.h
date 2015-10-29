068ce97ff41410f7043cfeba613271bb823edab1
Hannes Janetzek <hannes.janetzek@gmail.com>
2015-10-29 05:29:23 +0100

    use uin8_t for enums and sort members for less padding

--- core/src/scene/filters.h
+++ core/src/scene/filters.h
@@ -9,13 +9,13 @@ namespace Tangram {
 class StyleContext;
 struct Feature;
 
-enum class Operators : int {
+enum class Operators : uint8_t {
     any = 0,
     all,
     none
 };
 
-enum class FilterType : int {
+enum class FilterType : uint8_t {
     any = 0,
     all,
     none,
@@ -26,7 +26,7 @@ enum class FilterType : int {
     undefined
 };
 
-enum class FilterGlobal : int {
+enum class FilterGlobal : uint8_t {
     undefined,
     zoom,
     geometry,
@@ -37,15 +37,15 @@ struct Filter {
         std::vector<Filter> operands;
     };
     struct Equality {
-        FilterGlobal global;
         std::string key;
         std::vector<Value> values;
+        FilterGlobal global;
     };
     struct Range {
-        FilterGlobal global;
         std::string key;
         float min;
         float max;
+        FilterGlobal global;
     };
     struct Existence {
         std::string key;
@@ -54,13 +54,12 @@ struct Filter {
     struct Function {
         uint32_t id;
     };
-
-    FilterType type;
     using Data = variant<none_type, Operator, Equality, Range, Existence, Function>;
     Data data;
+    FilterType type;
 
-    Filter() : type(FilterType::undefined), data(none_type{}) {}
-    Filter(FilterType _type, Data _data) : type(_type), data(std::move(_data)) {}
+    Filter() : data(none_type{}), type(FilterType::undefined) {}
+    Filter(FilterType _type, Data _data) : data(std::move(_data)), type(_type) {}
 
     // Create an 'any', 'all', or 'none' filter
     inline static Filter MatchAny(const std::vector<Filter>& filters) {
@@ -74,11 +73,11 @@ struct Filter {
     }
     // Create an 'equality' filter
     inline static Filter MatchEquality(const std::string& k, const std::vector<Value>& vals) {
-        return { FilterType::equality, Equality{ globalType(k), k, vals }};
+        return { FilterType::equality, Equality{ k, vals, globalType(k) }};
     }
     // Create a 'range' filter
     inline static Filter MatchRange(const std::string& k, float min, float max) {
-        return { FilterType::range, Range{ globalType(k), k, min, max }};
+        return { FilterType::range, Range{ k, min, max, globalType(k) }};
     }
     // Create an 'existence' filter
     inline static Filter MatchExistence(const std::string& k, bool ex) {
