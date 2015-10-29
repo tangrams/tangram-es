b012df48dca8bd1757dab105e4097fa825306ed2
Hannes Janetzek <hannes.janetzek@gmail.com>
2015-10-29 06:04:44 +0100

    fontstash needs FONS_NORMALIZE_TEX_COORDS until atlas-resize is merged

--- core/src/text/fontContext.cpp
+++ core/src/text/fontContext.cpp
@@ -194,7 +194,7 @@ void FontContext::initFontContext(int _atlasSize) {
     FONSparams params;
     params.width = _atlasSize;
     params.height = _atlasSize;
-    params.flags = (unsigned char)FONS_ZERO_TOPLEFT;
+    params.flags = (unsigned char)FONS_ZERO_TOPLEFT | FONS_NORMALIZE_TEX_COORDS;
 
     params.renderCreate = renderCreate;
     params.renderResize = nullptr;
