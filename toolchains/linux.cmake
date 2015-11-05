38a10837d0f3a84c5c7a082f45d1ba68e4c2d3e1
Hannes Janetzek <hannes.janetzek@gmail.com>
2015-11-05 15:17:17 +0100

    linux: use libc++ with clang, and CXX11_ABI with gcc

--- toolchains/linux.cmake
+++ toolchains/linux.cmake
@@ -4,19 +4,35 @@ set(PLATFORM_LINUX ON)
 # global compile options
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -std=c++1y")
 
+if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-gnu-zero-variadic-macro-arguments")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
+  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS}  -lc++ -lc++abi")
+endif()
+
+if (CMAKE_COMPILER_IS_GNUCC)
+  execute_process(COMMAND ${CMAKE_C_COMPILER} -dumpversion
+    OUTPUT_VARIABLE GCC_VERSION)
+  string(REGEX MATCHALL "[0-9]+" GCC_VERSION_COMPONENTS ${GCC_VERSION})
+  list(GET GCC_VERSION_COMPONENTS 0 GCC_MAJOR)
+  list(GET GCC_VERSION_COMPONENTS 1 GCC_MINOR)
+
+  message(STATUS "Using gcc ${GCC_VERSION}")
+  if (GCC_VERSION VERSION_GREATER 5.1)
+    message(STATUS "USE CXX11_ABI")
+    add_definitions("-D_GLIBCXX_USE_CXX11_ABI=1")
+  endif()
+endif()
+
+# compile definitions (adds -DPLATFORM_LINUX)
+set(CORE_COMPILE_DEFS PLATFORM_LINUX)
+
 if (USE_EXTERNAL_LIBS)
 include(${EXTERNAL_LIBS_DIR}/core-dependencies.cmake)
 include(${EXTERNAL_LIBS_DIR}/glfw.cmake)
 else()
 add_subdirectory(${PROJECT_SOURCE_DIR}/external)
 endif()
-
-# if(CMAKE_COMPILER_IS_GNUCXX)
-#   list(APPEND CORE_CXX_FLAGS -ffast-math)
-# endif()
-
-# compile definitions (adds -DPLATFORM_LINUX)
-set(CORE_COMPILE_DEFS PLATFORM_LINUX)
 
 # load core library
 add_subdirectory(${PROJECT_SOURCE_DIR}/core)
