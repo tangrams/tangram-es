29b8c2216b13e4760f23d8121e977bd5d56d86d8
Karim Naaji <karim.naaji@gmail.com>
2015-12-03 18:58:59 -0500

    Adjust bounding boxes

--- core/src/labels/textLabel.cpp
+++ core/src/labels/textLabel.cpp
@@ -23,25 +23,27 @@ TextLabel::TextLabel(Label::Transform _transform, Type _type, glm::vec2 _dim, Te
             case Anchor::center: break;
         }
     }
+
+    m_perpAxis = glm::vec2(0.0, 1.0);
 }
 
 void TextLabel::updateBBoxes(float _zoomFract) {
     glm::vec2 t(1.0, 0.0);
-    glm::vec2 tperp(0.0, 1.0);
     glm::vec2 obbCenter;
 
     if (m_type == Type::line) {
         t = glm::vec2(cos(m_transform.state.rotation), sin(m_transform.state.rotation));
-        tperp = glm::vec2(-t.y, t.x);
+        m_perpAxis = glm::vec2(-t.y, t.x);
     }
 
     obbCenter = m_transform.state.screenPos;
     // move forward on line by half the text length
     obbCenter += t * m_dim.x * 0.5f;
     // move down on the perpendicular to estimated font baseline
-    obbCenter -= tperp * m_dim.y * 0.5f;
+    obbCenter -= m_perpAxis * m_dim.y * 0.5f;
     // ajdust with baseline
-    obbCenter += tperp * m_metrics.lineHeight * (float) m_nLines * 0.5f;
+    obbCenter += m_perpAxis * m_metrics.lineHeight * (float) (m_nLines - 1);
+    obbCenter -= m_perpAxis * m_metrics.descender;
 
     m_obb = OBB(obbCenter.x, obbCenter.y, m_transform.state.rotation, m_dim.x, m_dim.y);
     m_aabb = m_obb.getExtent();
@@ -63,6 +65,8 @@ void TextLabel::align(glm::vec2& _screenPosition, const glm::vec2& _ap1, const g
             // move back by half the length (so that text will be drawn centered)
             glm::vec2 direction = glm::normalize(_ap1 - _ap2);
             _screenPosition += direction * m_dim.x * 0.5f;
+
+            _screenPosition += m_perpAxis * (m_dim.y * 0.5f  + m_metrics.descender);
             break;
         }
     }
--- core/src/labels/textLabel.h
+++ core/src/labels/textLabel.h
@@ -24,6 +24,7 @@ protected:
 private:
 
     glm::vec2 m_anchor;
+    glm::vec2 m_perpAxis;
 
 };
 
