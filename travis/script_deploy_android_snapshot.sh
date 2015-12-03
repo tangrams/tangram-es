35f3f61fd521ac3afb7059b00378db14d510af53
Varun Talwar <tallytalwar@gmail.com>
2015-12-03 15:41:33 -0500

    Ask travis to build all android arch before deploy

--- travis/script_deploy_android_snapshot.sh
+++ travis/script_deploy_android_snapshot.sh
@@ -4,6 +4,14 @@ set -e
 set -o pipefail
 
 if [ "${PLATFORM}" = "android" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
+    
+    # Build all android architectures (armeabi-v7a already build)
+    make android ANDROID_ARCH=x86 
+    make android ANDROID_ARCH=x86_64
+    #make android ANDROID_ARCH=armeabi 
+    make android ANDROID_ARCH=arm64-v8a
+    #make android ANDROID_ARCH=mips
+    make android ANDROID_ARCH=mips64
     cd "$TRAVIS_BUILD_DIR"/android; ./gradlew uploadArchives -PsonatypeUsername="$SONATYPE_USERNAME" -PsonatypePassword="$SONATYPE_PASSWORD"
     cd "$TRAVIS_BUILD_DIR"
 fi
