e2912a9d77f4c36e1afa741af71f9c6cd5db962a
Hannes Janetzek <hannes.janetzek@gmail.com>
2015-12-07 19:47:51 +0100

    stress testing

--- linux/src/main.cpp
+++ linux/src/main.cpp
@@ -4,6 +4,9 @@
 #include "tangram.h"
 #include "data/clientGeoJsonSource.h"
 #include "platform_linux.h"
+#include "util/mapProjection.h"
+
+#include "glm/glm.hpp"
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -15,6 +18,10 @@ using namespace Tangram;
 void init_main_window();
 
 std::string sceneFile = "scene.yaml";
+
+GLFWwindow* main_window = nullptr;
+int width = 800;
+int height = 600;
 
 // Input handling
 // ==============
@@ -137,6 +144,44 @@ void scroll_callback(GLFWwindow* window, double scrollx, double scrolly) {
 
 }
 
+LngLat project(glm::dvec2 _m) {
+    MercatorProjection proj;
+    auto p = proj.MetersToLonLat(_m);
+    return { p.x, p.y };
+}
+
+void test(double time) {
+
+    LngLat t = {width/2.0, height/2.0};
+
+    Tangram::screenToWorldCoordinates(t.longitude, t.latitude);
+
+    MercatorProjection proj;
+    glm::dvec2 c = proj.LonLatToMeters({t.longitude, t.latitude});
+
+    std::string color = "#eeaaee";
+
+    Tangram::clearDataSource(*data_source, true, false);
+
+    for (int x = -5; x <= 5; x++) {
+        for (int y = -5; y <= 5; y++) {
+            Properties prop;
+            prop.add("type", "poly");
+            prop.add("color", color);
+            prop.add("height", (float)abs((int)((x * y) * 100 + 1000.0*sin(time))%1000));
+            data_source->addPoly(prop, {{
+                            project(c + glm::dvec2{x*220 - 100., y*220 - 100.}),
+                            project(c + glm::dvec2{x*220 - 100., y*220 + 100.}),
+                            project(c + glm::dvec2{x*220 + 100., y*220 + 100.}),
+                            project(c + glm::dvec2{x*220 + 100., y*220 - 100.}),
+                            project(c + glm::dvec2{x*220 - 100., y*220 - 100.})
+                            }});
+        }
+    }
+
+    requestRender();
+}
+
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
     if (action == GLFW_PRESS) {
         switch (key) {
@@ -176,6 +221,9 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
             case GLFW_KEY_N:
                 Tangram::setRotation(0.f, 1.f);
                 break;
+            case GLFW_KEY_X:
+                test(100);
+                break;
             default:
                 break;
         }
@@ -191,10 +239,6 @@ void drop_callback(GLFWwindow* window, int count, const char** paths) {
 
 // Window handling
 // ===============
-
-GLFWwindow* main_window = nullptr;
-int width = 800;
-int height = 600;
 
 void window_size_callback(GLFWwindow* window, int width, int height) {
 
@@ -271,7 +315,8 @@ int main(int argc, char* argv[]) {
 
     double lastTime = glfwGetTime();
 
-    setContinuousRendering(false);
+    //setContinuousRendering(false);
+    setContinuousRendering(true);
     glfwSwapInterval(0);
 
     // Loop until the user closes the window
@@ -282,6 +327,8 @@ int main(int argc, char* argv[]) {
         lastTime = currentTime;
 
         processNetworkQueue();
+
+        // test(currentTime);
 
         // Render
         Tangram::update(delta);
