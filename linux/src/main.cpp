57e547e132d14a1517635dd7c1353ab757331964
Hannes Janetzek <hannes.janetzek@gmail.com>
2016-01-16 22:59:26 +0100

    linux: wait for downloads threads to finish
    - otherwise the callback to TileManager can be called
      after TileManager or TileWorker haven been disposed

--- linux/src/main.cpp
+++ linux/src/main.cpp
@@ -332,6 +332,8 @@ int main(int argc, char* argv[]) {
         }
     }
 
+    finishUrlRequests();
+
     curl_global_cleanup();
     glfwTerminate();
     return 0;
--- linux/src/platform_linux.h
+++ linux/src/platform_linux.h
@@ -7,3 +7,5 @@
 
 void processNetworkQueue();
 
+void finishUrlRequests();
+
