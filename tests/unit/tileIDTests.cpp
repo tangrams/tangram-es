96a1558ae2098bfe87711e1742688c21ed6da751
Matt Blair <blair1618@gmail.com>
2016-01-15 21:46:59 -0500

    Add tests for TileID behavior with source zoom limits

--- tests/unit/tileIDTests.cpp
+++ tests/unit/tileIDTests.cpp
@@ -76,3 +76,20 @@ TEST_CASE( "Ensure TileIDs correctly evaluate their validity", "[Core][TileID]")
     REQUIRE(!NOT_A_TILE.isValid());
 
 }
+
+TEST_CASE("TileID correctly applies source zoom limits", "[Core][TileID]") {
+
+    auto a = TileID(1, 2, 4);
+    REQUIRE(a.withMaxSourceZoom(4) == a);
+    REQUIRE(a.withMaxSourceZoom(3) == TileID(0, 1, 3, 4, 0));
+    REQUIRE(a.withMaxSourceZoom(2) == TileID(0, 0, 2, 4, 0));
+
+    auto b = TileID(2, 1, 4, 6, 0); // Over-zoomed to zoom 6 with a limit of 4
+    auto c = b.getParent();
+    auto d = c.getParent();
+    auto e = d.getParent();
+    REQUIRE(c == TileID(2, 1, 4, 5, 0));
+    REQUIRE(d == TileID(2, 1, 4, 4, 0));
+    REQUIRE(e == TileID(1, 0, 3, 3, 0));
+
+}
