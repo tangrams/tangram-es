35ba8e428f31783ecb89e1d1323a67e596df3789
Hannes Janetzek <hannes.janetzek@gmail.com>
2016-01-25 22:33:18 +0100

    revert: line-param-refactor PolyLine changes temporarily
    Revert "Merge pull request #492 from tangrams/line-param-refactor"
    
    This reverts commit b0f4586008d50349ec976c6078c11c362e990fa1, reversing
    changes made to 8c2df5ebffb7049d98d206aac93e917f5cf80f3a.

--- core/src/style/polylineStyle.h
+++ core/src/style/polylineStyle.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "style.h"
+#include "glm/vec2.hpp"
 // TODO move cap/join types to types.h
 #include "util/builders.h"
 
@@ -11,17 +12,16 @@ class PolylineStyle : public Style {
 protected:
 
     struct Parameters {
-        struct {
-            uint32_t order = 0;
-            uint32_t color = 0xff00ffff;
-            float width = 0.f;
-            float slope = 0.f;
-            CapTypes cap = CapTypes::butt;
-            JoinTypes join = JoinTypes::miter;
-        } fill, outline;
-        float height = 0.f;
+        uint32_t order = 0;
+        uint32_t outlineOrder = 0;
+        uint32_t color = 0xff00ffff;
+        uint32_t outlineColor = 0xff00ffff;
+        CapTypes cap = CapTypes::butt;
+        CapTypes outlineCap = CapTypes::butt;
+        JoinTypes join = JoinTypes::miter;
+        JoinTypes outlineJoin = JoinTypes::miter;
         bool outlineOn = false;
-        bool keepTileEdges = false;
+        glm::vec2 extrude;
     };
 
     virtual void constructVertexLayout() override;
@@ -29,8 +29,7 @@ protected:
     virtual void buildLine(const Line& _line, const DrawRule& _rule, const Properties& _props, VboMesh& _mesh, Tile& _tile) const override;
     virtual void buildPolygon(const Polygon& _poly, const DrawRule& _rule, const Properties& _props, VboMesh& _mesh, Tile& _tile) const override;
 
-    Parameters parseRule(const DrawRule& _rule, const Properties& _props, const Tile& _tile) const;
-    void buildMesh(const Line& _line, Parameters& _parameters, VboMesh& _mesh) const;
+    Parameters parseRule(const DrawRule& _rule) const;
 
     virtual VboMesh* newMesh() const override;
 
@@ -38,8 +37,8 @@ public:
 
     PolylineStyle(std::string _name, Blending _blendMode = Blending::none, GLenum _drawMode = GL_TRIANGLES);
 
-    virtual ~PolylineStyle() {}
-
+    virtual ~PolylineStyle() {
+    }
 };
 
 }
