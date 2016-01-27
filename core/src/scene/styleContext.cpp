025a5025639f56100c25398b3b9caeeebc87514c
Hannes Janetzek <hannes.janetzek@gmail.com>
2016-01-27 14:22:07 +0100

    --dbg noise

--- core/src/scene/styleContext.cpp
+++ core/src/scene/styleContext.cpp
@@ -295,7 +295,7 @@ bool StyleContext::parseStyleResult(StyleParamKey _key, StyleParam::Value& _val)
 
     } else if (duk_is_nan(m_ctx, -1)) {
         // Ignore setting value
-        LOGD("duk evaluates JS method to NAN.\n");
+        //LOGD("duk evaluates JS method to NAN.");
     } else if (duk_is_number(m_ctx, -1)) {
 
         switch (_key) {
@@ -329,7 +329,7 @@ bool StyleContext::parseStyleResult(StyleParamKey _key, StyleParam::Value& _val)
         }
     } else if (duk_is_null_or_undefined(m_ctx, -1)) {
         // Ignore setting value
-        LOGD("duk evaluates JS method to null or undefined.\n");
+        //LOGD("duk evaluates JS method to null or undefined.");
     } else {
         LOGW("Unhandled return type from Javascript style function for %d.", _key);
     }
