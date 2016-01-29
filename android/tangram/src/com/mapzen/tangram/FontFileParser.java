74e3cf44be3fcab3cba58f1a62ad7efbb2da2255
Karim Naaji <karim.naaji@gmail.com>
2016-01-29 13:27:57 -0500

    squashed: Load font fallbacks from fonts.xml
    Add mapController interface for font fallbacks
    
    wip: Add systemFontFallbackPath
    
    Fix jni binding
    
    Some renaming
    
    Other platforms placeholder
    
    Don't parse from constructor

--- android/tangram/src/com/mapzen/tangram/FontFileParser.java
+++ android/tangram/src/com/mapzen/tangram/FontFileParser.java
@@ -6,18 +6,20 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.ArrayList;
+import java.util.Vector;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
 import android.util.Xml;
-import android.util.Log;
 
 class FontFileParser {
 
     private Map<String, String> fontDict = new HashMap<String, String>();
+    private Map<Integer, Vector<String>> fallbackFontDict = new HashMap<Integer, Vector<String>>();
 
     private void processDocument(XmlPullParser parser) throws XmlPullParserException, IOException {
 
@@ -34,7 +36,36 @@ class FontFileParser {
                 familyWeights.clear();
                 // Parse this family:
                 String name = parser.getAttributeValue(null, "name");
-                if (name == null) { continue; }
+
+                // fallback fonts
+                if (name == null) {
+                    while (parser.next() != XmlPullParser.END_TAG) {
+                        if (parser.getEventType() != XmlPullParser.START_TAG) {
+                            continue;
+                        }
+                        String tag = parser.getName();
+                        if ("font".equals(tag)) {
+                            String weightStr = parser.getAttributeValue(null, "weight");
+                            if (weightStr != null) { familyWeights.add(weightStr); }
+                            weightStr = (weightStr == null) ? "400" : weightStr;
+
+                            String styleStr = parser.getAttributeValue(null, "style");
+                            styleStr = (styleStr == null) ? "normal" : styleStr;
+
+                            String filename = parser.nextText();
+
+                            Integer weight = new Integer(weightStr);
+                            if (!fallbackFontDict.containsKey(weight)) {
+                                fallbackFontDict.put(weight, new Vector<String>());
+                            }
+
+                            fallbackFontDict.get(weight).add(filename);
+                        } else {
+                            skip(parser);
+                        }
+                    }
+                    continue;
+                }
                 while (parser.next() != XmlPullParser.END_TAG) {
                     if (parser.getEventType() != XmlPullParser.START_TAG) {
                         continue;
@@ -90,20 +121,19 @@ class FontFileParser {
         int depth = 1;
         while (depth > 0) {
             switch (parser.next()) {
-            case XmlPullParser.START_TAG:
-                depth++;
-                break;
-            case XmlPullParser.END_TAG:
-                depth--;
-                break;
+                case XmlPullParser.START_TAG:
+                    depth++;
+                    break;
+                case XmlPullParser.END_TAG:
+                    depth--;
+                    break;
             }
         }
     }
 
-    public FontFileParser() {
-
+    public void parse(String fontXMLPath) {
         InputStream in = null;
-        final File fontFile = new File("/system/etc/fonts.xml");
+        final File fontFile = new File(fontXMLPath);
 
         // TODO: Handle system_fonts parsing which has a different xml layout as compared to fonts.xml
         //       system_fonts.xml also does not seem to have a good css style font parameter mapping as is the case with
@@ -150,6 +180,34 @@ class FontFileParser {
         } else {
             return "";
         }
+    }
+
+    /*
+     * Returns the next available font fallback, or empty string if not found
+     * The integer value determines the fallback priority (lower is higher)
+     * The weightHint value determines the closest fallback hint for boldness
+     * See /etc/fonts/font_fallback for documentation
+     */
+    public String getFontFallback(int importance, int weightHint) {
+        Iterator it = fallbackFontDict.entrySet().iterator();
+        Integer diffWeight = Integer.MAX_VALUE;
+        String fallback = "";
+
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            Integer diff = Math.abs((Integer) pair.getKey() - weightHint);
+
+            if (diff < diffWeight) {
+                Vector<String> fallbacks = (Vector<String>) pair.getValue();
+
+                if (importance < fallbacks.size()) {
+                    fallback = fallbacks.elementAt(importance);
+                    diffWeight = diff;
+                }
+            }
+        }
+
+        return fallback;
     }
 
 }
