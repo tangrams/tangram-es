3f2735b6fbc2e36d2b12126930cb3bcad4d9648f
Hannes Janetzek <hannes.janetzek@gmail.com>
2016-01-30 07:03:46 +0100

    alfons: use new Mesh and LabelSet interfaces

--- core/src/style/alfonsStyle.cpp
+++ core/src/style/alfonsStyle.cpp
@@ -148,9 +148,8 @@ struct AlfonsMesh : public LabelMesh {
 struct GlyphBatch {
 
     GlyphBatch(std::shared_ptr<VertexLayout> _vertexLayout)
-        : texture(TEXTURE_SIZE, TEXTURE_SIZE)
-        { //,
-          //mesh(_vertexLayout, GL_TRIANGLES) {
+        : texture(TEXTURE_SIZE, TEXTURE_SIZE) {
+
         texData.resize(TEXTURE_SIZE * TEXTURE_SIZE);
         mesh = std::make_unique<AlfonsMesh>(_vertexLayout, GL_TRIANGLES);
     }
@@ -233,18 +232,18 @@ struct AlfonsContext : public alf::TextureCallback {
 // Not actually used as VboMesh!
 // TODO make LabelMesh an interface
 // - just keeps labels and vertices for all Labels of a tile
-struct LabelContainer : public LabelMesh {
+struct LabelContainer : public LabelSet, public Mesh {
 
     LabelContainer(AlfonsContext& _ctx)
         // just adding vertexlayout to not crash in getMemoryusage!
-        : LabelMesh(std::make_shared<VertexLayout>(std::vector<VertexLayout::VertexAttrib>{}), 0),
-          context(_ctx) {}
+        : context(_ctx) {}
 
     AlfonsContext& context;
     std::vector<GlyphQuad> quads;
     std::bitset<128> batchRefs;
 
     void draw(ShaderProgram& _shader) override {}
+    size_t bufferSize() override { return 0; }
 
     void setLabels(std::vector<std::unique_ptr<Label>>& _labels,
                    std::vector<GlyphQuad>& _quads) {
@@ -261,30 +260,10 @@ struct LabelContainer : public LabelMesh {
                      _quads.begin(),
                      _quads.end());
 
-        m_isCompiled = true;
+        //m_isCompiled = true;
     }
 };
 
-struct AlfonsLabel : public TextLabel {
-    using TextLabel::TextLabel;
-
-    void pushTransform() override {
-        // if (!m_dirty) { return; }
-        // m_dirty = false;
-
-        if (!visibleState()) { return; }
-
-        auto& mesh = *static_cast<LabelContainer*>(&m_mesh);
-        auto state = m_transform.state.vertex();
-
-        auto it = mesh.quads.begin() + m_vertexRange.start;
-        auto end = it + m_vertexRange.length;
-
-        for (; it != end; ++it) {
-            mesh.context.m_batches[it->atlas].mesh->pushQuad(*it, state);
-        }
-    }
-};
 
 AlfonsStyle::AlfonsStyle(std::string _name, bool _sdf, Blending _blendMode, GLenum _drawMode) :
     Style(_name, _blendMode, _drawMode), m_sdf(_sdf),
@@ -457,7 +436,7 @@ struct Builder : public StyleBuilder {
         m_mesh = std::make_unique<LabelContainer>(*m_style.context());
     }
 
-    virtual std::unique_ptr<VboMesh> build() override {
+    virtual std::unique_ptr<Mesh> build() override {
         if (!m_labels.empty()) {
             m_mesh->setLabels(m_labels, m_scratch.quads);
         }
@@ -696,6 +675,100 @@ AlfonsStyle::Parameters Builder::applyRule(const DrawRule& _rule,
 
 std::unique_ptr<StyleBuilder> AlfonsStyle::createBuilder() const {
     return std::make_unique<Builder>(*this);
+}
+
+
+using namespace LabelProperty;
+
+AlfonsLabel::AlfonsLabel(Label::Transform _transform, Type _type, glm::vec2 _dim,
+                     LabelContainer& _mesh, Range _vertexRange,
+                     Label::Options _options, FontContext::FontMetrics _metrics,
+                     int _nLines, Anchor _anchor, glm::vec2 _quadsLocalOrigin)
+    : Label(_transform, _dim, _type, _vertexRange, _options),
+      m_metrics(_metrics),
+      m_nLines(_nLines),
+      m_mesh(_mesh),
+      m_quadLocalOrigin(_quadsLocalOrigin) {
+
+    if (m_type == Type::point) {
+        glm::vec2 halfDim = m_dim * 0.5f;
+        switch(_anchor) {
+            case Anchor::left: m_anchor.x -= halfDim.x; break;
+            case Anchor::right: m_anchor.x += halfDim.x; break;
+            case Anchor::top: m_anchor.y -= halfDim.y; break;
+            case Anchor::bottom: m_anchor.y += halfDim.y; break;
+            case Anchor::bottom_left: m_anchor += glm::vec2(-halfDim.x, halfDim.y); break;
+            case Anchor::bottom_right: m_anchor += halfDim; break;
+            case Anchor::top_left: m_anchor -= halfDim; break;
+            case Anchor::top_right: m_anchor += glm::vec2(halfDim.x, -halfDim.y); break;
+            case Anchor::center: break;
+        }
+    }
+}
+
+void AlfonsLabel::updateBBoxes(float _zoomFract) {
+    glm::vec2 obbCenter;
+
+    if (m_type == Type::line) {
+        m_xAxis = glm::vec2(cos(m_transform.state.rotation), sin(m_transform.state.rotation));
+        m_yAxis = glm::vec2(-m_xAxis.y, m_xAxis.x);
+    }
+
+    obbCenter = m_transform.state.screenPos;
+    // move forward on line by half the text length
+    obbCenter += m_xAxis * m_dim.x * 0.5f;
+    // move down on the perpendicular to estimated font baseline
+    obbCenter += m_yAxis * m_dim.y * 0.5f;
+    // ajdust with local origin of the quads
+    obbCenter += m_yAxis * m_quadLocalOrigin.y;
+    obbCenter += m_xAxis * m_quadLocalOrigin.x;
+
+    m_obb = OBB(obbCenter.x, obbCenter.y, m_transform.state.rotation,
+                m_dim.x + m_options.buffer, m_dim.y + m_options.buffer);
+    m_aabb = m_obb.getExtent();
+}
+
+void AlfonsLabel::align(glm::vec2& _screenPosition, const glm::vec2& _ap1, const glm::vec2& _ap2) {
+
+    switch (m_type) {
+        case Type::debug:
+        case Type::point:
+            // modify position set by updateScreenTransform()
+            _screenPosition.x -= m_dim.x * 0.5f + m_quadLocalOrigin.x;
+            _screenPosition.y -= m_metrics.descender;
+
+            if (m_nLines > 1) {
+                _screenPosition.y -= m_dim.y * 0.5f;
+                _screenPosition.y += (m_metrics.lineHeight + m_metrics.descender);
+            }
+            _screenPosition += m_anchor;
+            break;
+        case Type::line: {
+            // anchor at line center
+            _screenPosition = (_ap1 + _ap2) * 0.5f;
+
+            // move back by half the length (so that text will be drawn centered)
+            glm::vec2 direction = glm::normalize(_ap1 - _ap2);
+            _screenPosition += direction * m_dim.x * 0.5f;
+            _screenPosition += m_yAxis * (m_dim.y * 0.5f + m_metrics.descender);
+            break;
+        }
+    }
+}
+
+void AlfonsLabel::pushTransform() {
+    // if (!m_dirty) { return; }
+    // m_dirty = false;
+    if (!visibleState()) { return; }
+
+    auto state = m_transform.state.vertex();
+
+    auto it = m_mesh.quads.begin() + m_vertexRange.start;
+    auto end = it + m_vertexRange.length;
+
+    for (; it != end; ++it) {
+        m_mesh.context.m_batches[it->atlas].mesh->pushQuad(*it, state);
+    }
 }
 
 }
--- core/src/style/alfonsStyle.h
+++ core/src/style/alfonsStyle.h
@@ -14,6 +14,35 @@ struct Properties;
 typedef int FontID;
 
 struct AlfonsContext;
+struct LabelContainer;
+
+struct AlfonsLabel : public Label {
+  public:
+    AlfonsLabel(Label::Transform _transform, Type _type,
+                glm::vec2 _dim, LabelContainer& _mesh,
+                Range _vertexRange,
+                Label::Options _options,
+                FontContext::FontMetrics _metrics,
+                int _nLines, LabelProperty::Anchor _anchor,
+                glm::vec2 _quadsLocalOrigin);
+
+    void updateBBoxes(float _zoomFract) override;
+
+protected:
+
+    void align(glm::vec2& _screenPosition, const glm::vec2& _ap1, const glm::vec2& _ap2) override;
+    FontContext::FontMetrics m_metrics;
+    int m_nLines;
+
+    void pushTransform() override;
+
+private:
+    // Back-pointer to owning container
+    LabelContainer& m_mesh;
+
+    glm::vec2 m_anchor;
+    glm::vec2 m_quadLocalOrigin;
+};
 
 class AlfonsStyle : public Style {
 
