d663a42f12fe08c1e9bf9160ffb52edad94919c7
Karim Naaji <karim.naaji@gmail.com>
2016-02-23 18:06:57 -0500

    wip: Line wrap tests

--- tests/unit/lineWrapTests.cpp
+++ tests/unit/lineWrapTests.cpp
@@ -1,5 +1,6 @@
 #include "catch.hpp"
 #include "tangram.h"
+#include "platform.h"
 #include "style/textStyle.h"
 
 #include "alfons/textBatch.h"
@@ -14,6 +15,9 @@
 
 namespace Tangram {
 
+#define TEST_FONT_SIZE  24
+#define TEST_FONT       "fonts/NotoSans-Regular.ttf"
+
 struct ScratchBuffer : public alfons::MeshCallback {
     void drawGlyph(const alfons::Quad& q, const alfons::AtlasGlyph& atlasGlyph) override {}
     void drawGlyph(const alfons::Rect& q, const alfons::AtlasGlyph& atlasGlyph) override {}
@@ -25,21 +29,49 @@ struct AtlasCallback : public alfons::TextureCallback {
             const unsigned char* src, uint16_t padding) override {}
 };
 
+AtlasCallback atlasCb;
+ScratchBuffer buffer;
+LineWrap wrap;
+alfons::TextShaper shaper;
+alfons::GlyphAtlas atlas(atlasCb);
+alfons::TextBatch batch(atlas, buffer);
+alfons::FontManager fontManager;
+std::shared_ptr<alfons::Font> font;
+
+void initFont() {
+    font = fontManager.addFont("default", alf::InputSource(TEST_FONT), TEST_FONT_SIZE);
+
+    unsigned int dataSize = 0;
+    unsigned char* data = bytesFromFile(TEST_FONT, PathType::internal, &dataSize);
+
+    auto face = fontManager.addFontFace(alf::InputSource(reinterpret_cast<char*>(data), dataSize), TEST_FONT_SIZE);
+
+    font->addFace(face);
+}
+
 TEST_CASE("Ensure empty line is given when giving empty shape to alfons", "[Core][Alfons]") {
-    AtlasCallback atlasCb;
-    ScratchBuffer buffer;
-    alfons::LineMetrics metrics;
-    alfons::TextShaper shaper;
-    alfons::GlyphAtlas atlas(atlasCb);
-    alfons::TextBatch batch(atlas, buffer);
-    alfons::FontManager fontManager;
-
-    auto font = fontManager.addFont("default", alf::InputSource("fonts/NotoSans-Regular.ttf"), 24);
-
+    initFont();
     auto line = shaper.shape(font, "");
-    metrics = drawWithLineWrapping(line, batch, 15, TextLabelProperty::Align::center, 1.0);
-
     REQUIRE(line.shapes().size() == 0);
+}
+
+TEST_CASE() {
+    initFont();
+
+    auto line = shaper.shape(font, "The quick brown fox");
+    wrap = drawWithLineWrapping(line, batch, 10, TextLabelProperty::Align::center, 1.0);
+
+    REQUIRE(line.shapes().size() == 19);
+    REQUIRE(wrap.nbLines == 2);
+}
+
+TEST_CASE() {
+    initFont();
+
+    auto line = shaper.shape(font, "The quick brown fox");
+    wrap = drawWithLineWrapping(line, batch, 4, TextLabelProperty::Align::center, 1.0);
+
+    REQUIRE(wrap.nbLines == 4);
 }
 
 }
