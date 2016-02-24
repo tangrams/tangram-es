cd286f1a2af674a1222359a7270b118f9c8790dd
Matt Blair <blair1618@gmail.com>
2016-02-23 23:19:23 -0500

    Allow GeoJsonSource to parse a root object FeatureCollection

--- core/src/util/geoJson.h
+++ core/src/util/geoJson.h
@@ -11,6 +11,8 @@ namespace GeoJson {
 
 using Transform = std::function<Point(glm::dvec2 _lonLat)>;
 
+bool isFeatureCollection(const JsonValue& _in);
+
 Point getPoint(const JsonValue& _in, const Transform& _proj);
 
 Line getLine(const JsonValue& _in, const Transform& _proj);
@@ -21,7 +23,7 @@ Properties getProperties(const JsonValue& _in, int32_t _sourceId);
 
 Feature getFeature(const JsonValue& _in, const Transform& _proj, int32_t _sourceId);
 
-Layer getLayer(const JsonDocument::MemberIterator& _in, const Transform& _proj, int32_t _sourceId);
+Layer getLayer(const JsonValue& _in, const Transform& _proj, int32_t _sourceId);
 
 }
 
