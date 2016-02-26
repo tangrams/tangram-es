54fe7082ddb750c04444c683e7b87abc78e8200f
Karim Naaji <karim.naaji@gmail.com>
2016-02-26 13:08:15 -0500

    Cleanup SDF shader

--- core/resources/shaders/sdf.fs
+++ core/resources/shaders/sdf.fs
@@ -1,13 +1,5 @@
 #pragma tangram: extensions
 
-#ifdef TANGRAM_SDF_MULTISAMPLING
-    #ifdef GL_OES_standard_derivatives
-        #extension GL_OES_standard_derivatives : enable
-    #else
-        #undef TANGRAM_SDF_MULTISAMPLING
-    #endif
-#endif
-
 #ifdef GL_ES
     precision mediump float;
     #define LOWP lowp
@@ -17,8 +9,6 @@
 
 #pragma tangram: defines
 
-const float emSize = 15.0 / 16.0;
-
 uniform sampler2D u_tex;
 uniform vec3 u_map_position;
 uniform vec3 u_tile_origin;
@@ -26,8 +16,8 @@ uniform vec2 u_resolution;
 uniform float u_time;
 uniform float u_meters_per_pixel;
 uniform float u_device_pixel_ratio;
-uniform int u_pass;
 uniform float u_max_stroke_width;
+uniform int u_pass;
 
 #pragma tangram: uniforms
 
@@ -39,40 +29,11 @@ varying float v_sdf_scale;
 
 #pragma tangram: global
 
-float contour(in float d, in float w, float t) {
-    return smoothstep(t - w, t + w, d);
-}
-
-float sample(in vec2 uv, float w, float t) {
-    return contour(texture2D(u_tex, uv).a, w, t);
-}
-
-float sampleAlpha(in vec2 uv, float distance, float threshold) {
-    const float smoothing = 0.0625 * emSize; // 0.0625 = 1.0/1em ratio
-    float alpha = contour(distance, smoothing, threshold);
-
-#ifdef TANGRAM_SDF_MULTISAMPLING
-    const float aaSmooth = smoothing / 2.0;
-    float dscale = 0.354; // 1 / sqrt(2)
-    vec2 duv = dscale * (dFdx(uv) + dFdy(uv));
-    vec4 box = vec4(uv - duv, uv + duv);
-
-    float asum = sample(box.xy, aaSmooth, threshold)
-               + sample(box.zw, aaSmooth, threshold)
-               + sample(box.xw, aaSmooth, threshold)
-               + sample(box.zy, aaSmooth, threshold);
-
-    alpha = mix(alpha, asum, 0.25);
-#endif
-
-    return alpha;
-}
-
 void main(void) {
 
     vec4 color = v_color;
 
-    float dist = texture2D(u_tex, v_texcoords).a;
+    float signed_distance = texture2D(u_tex, v_texcoords).a;
 
     // - At the glyph outline alpha is 0.5
     //
@@ -89,21 +50,17 @@ void main(void) {
     // - sdf_scale == fontScale / glyphScale:
     //   When the glyph is scaled down, 's' must be increased
     //   (used to interpolate 1px of the scaled glyph around v_sdf_threshold)
-    //float filter_width = 0.1245 / v_sdf_scale;
 
-    float sdf_pixel = (0.5/u_max_stroke_width) / v_sdf_scale;
+    float sdf_pixel = 0.5 / (u_max_stroke_width * v_sdf_scale);
     float add_smooth = 0.25;
     float filter_width = (sdf_pixel * (0.5 + add_smooth));
 
-    float alpha = smoothstep(max(v_sdf_threshold - filter_width, 0.0),
-                             v_sdf_threshold + filter_width,
-                             dist);
+    float start = max(v_sdf_threshold - filter_width, 0.0);
+    float end = v_sdf_threshold + filter_width;
+
+    float alpha = smoothstep(start, end, signed_distance);
 
     color.a *= v_alpha * alpha;
-
-    //color = mix(vec4(1., 0., 0., 1.), vec4(0., 0., 1., 1.), dist);
-    // color = mix(vec4(1., 0., 0., 1.), vec4(0., 0., 1., 1.), step(dist,0.5));
-    // color.a *= max(alpha, 0.2);
 
     #pragma tangram: color
     #pragma tangram: filter
