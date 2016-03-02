b76915c1a48ded5e71008e647a8599d6945ce6b0
Hannes Janetzek <hannes.janetzek@gmail.com>
2016-03-02 04:11:53 +0100

    premultiply textures load from files

--- core/src/gl/texture.cpp
+++ core/src/gl/texture.cpp
@@ -38,6 +38,21 @@ Texture::Texture(const std::string& _file, TextureOptions _options, bool _genera
 
     pixels = stbi_load_from_memory(data, size, &width, &height, &comp, STBI_rgb_alpha);
 
+    if (comp == STBI_rgb_alpha) {
+        // Premultiply
+        auto *p = pixels;
+        for (size_t i = 0, end = width*height; i < end; i++, p+=4) {
+            int32_t alpha = p[3];
+            if (alpha) {
+                p[0] = p[0] * alpha / 255;
+                p[1] = p[1] * alpha / 255;
+                p[2] = p[2] * alpha / 255;
+            } else {
+                p[0] = p[1] = p[2] = 0;
+            }
+        }
+    }
+
     resize(width, height);
     setData(reinterpret_cast<GLuint*>(pixels), width * height);
 
