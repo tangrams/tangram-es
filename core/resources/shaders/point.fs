6530eb26e91a12f206015061e540d31874760095
Hannes Janetzek <hannes.janetzek@gmail.com>
2016-03-03 18:53:23 +0100

    update point.fs

--- core/resources/shaders/point.fs
+++ core/resources/shaders/point.fs
@@ -48,16 +48,14 @@ void main(void) {
             float c2 = circle(uv, vec2(0.0), circleRadiusOut);
             color = vec4(vec3(c1) * v_color.rgb, c2 * v_alpha * v_color.a);
         #else
-            // premultiplied texture color
-            vec4 texColor = texture2D(u_tex, v_texcoords);
-            //color = vec4(texColor.rgb * v_color.rgb, texColor.a * v_color.a);
-
-            // Correct? v_color as multipler - to say how much of each texture channel is used?
-            color = texColor * v_color;
-            color *= v_alpha;
+            // assume premultiplied texture color
+            color = texture2D(u_tex, v_texcoords);
 
             // unpremultiply for blending(src_alpha,one_minus_src_alpha)
             color.rgb /= color.a;
+
+            color *= v_color;
+            color.a *= v_alpha;
         #endif
 
         #pragma tangram: color
