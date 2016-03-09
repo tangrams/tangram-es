29a8f200b1f412074b8b7314cf05ee618c09b7f2
Varun Talwar <tallytalwar@gmail.com>
2016-03-09 18:12:51 -0500

    Parse layer:visible layer paramater
    - adds a visible member to sceneLayer

--- core/src/scene/sceneLayer.cpp
+++ core/src/scene/sceneLayer.cpp
@@ -6,11 +6,13 @@ namespace Tangram {
 
 SceneLayer::SceneLayer(std::string _name, Filter _filter,
                        std::vector<DrawRuleData> _rules,
-                       std::vector<SceneLayer> _sublayers) :
+                       std::vector<SceneLayer> _sublayers,
+                       bool _visible) :
     m_filter(std::move(_filter)),
     m_name(_name),
     m_rules(_rules),
-    m_sublayers(std::move(_sublayers)) {
+    m_sublayers(std::move(_sublayers)),
+    m_visible(_visible) {
 
     setDepth(1);
 
