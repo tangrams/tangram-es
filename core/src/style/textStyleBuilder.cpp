8d3b7f7f69c0c15a61add9dac10b87b44caf2cd5
Hannes Janetzek <hannes.janetzek@gmail.com>
2016-03-15 08:46:03 +0100

    cleanups: reorder functions

--- core/src/style/textStyleBuilder.cpp
+++ core/src/style/textStyleBuilder.cpp
@@ -79,205 +79,6 @@ void TextStyleBuilder::addPolygon(const Polygon& _polygon, const Properties& _pr
     addPoint(p, _props, _rule);
 }
 
-// TODO use icu transforms
-// http://source.icu-project.org/repos/icu/icu/trunk/source/samples/ustring/ustring.cpp
-
-std::string TextStyleBuilder::applyTextTransform(const TextStyle::Parameters& _params,
-                                                 const std::string& _string) {
-    std::locale loc;
-    std::string text = _string;
-
-    switch (_params.transform) {
-        case TextLabelProperty::Transform::capitalize:
-            text[0] = toupper(text[0], loc);
-            if (text.size() > 1) {
-                for (size_t i = 1; i < text.length(); ++i) {
-                    if (text[i - 1] == ' ') {
-                        text[i] = std::toupper(text[i], loc);
-                    }
-                }
-            }
-            break;
-        case TextLabelProperty::Transform::lowercase:
-            for (size_t i = 0; i < text.length(); ++i) {
-                text[i] = std::tolower(text[i], loc);
-            }
-            break;
-        case TextLabelProperty::Transform::uppercase:
-            // TODO : use to wupper when any wide character is detected
-            for (size_t i = 0; i < text.length(); ++i) {
-                text[i] = std::toupper(text[i], loc);
-            }
-            break;
-        default:
-            break;
-    }
-
-    return text;
-}
-
-std::string TextStyleBuilder::resolveTextSource(const std::string& textSource,
-                                                const Properties& props) const {
-
-    std::string tmp, item;
-
-    // Meaning we have a yaml sequence defining fallbacks
-    if (textSource.find(',') != std::string::npos) {
-        std::stringstream ss(textSource);
-
-        // Parse fallbacks
-        while (std::getline(ss, tmp, ',')) {
-            if (props.getAsString(tmp, item)) {
-                return item;
-            }
-        }
-    }
-
-    // Fallback to default text source
-    if (props.getAsString(textSource, item)) {
-        return item;
-    }
-
-    // Default to 'name'
-    return props.getString(key_name);
-}
-
-bool TextStyleBuilder::prepareLabel(TextStyle::Parameters& _params, Label::Type _type) {
-
-    if (_params.text.empty() || _params.fontSize <= 0.f) {
-        LOGD("invalid params: '%s' %f", _params.text.c_str(), _params.fontSize);
-        return false;
-    }
-
-    // Apply text transforms
-    const std::string* renderText;
-    std::string text;
-
-    if (_params.transform == TextLabelProperty::Transform::none) {
-        renderText = &_params.text;
-    } else {
-        text = applyTextTransform(_params, _params.text);
-        renderText = &text;
-    }
-
-    // Scale factor by which the texture glyphs are scaled to match fontSize
-    float fontScale = (_params.fontSize * m_style.pixelScale()) / _params.font->size();
-
-    // Stroke width is normalized by the distance of the SDF spread, then
-    // scaled to a char, then packed into the "alpha" channel of stroke.
-    // Maximal strokeWidth is 3px, attribute is normalized to 0-1 range.
-    float strokeWidth = _params.strokeWidth * m_style.pixelScale();
-
-#if 0
-    // HACK - need to use a smaller font in this case
-    // to have enough sdf-radius for the stroke!
-    // if (strokeWidth > 1.5 * fontScale) {
-    //     strokeWidth = 1.5 * fontScale;
-    // }
-
-    //// see point.vs and sdf.fs
-    float sdf_radius = 3.0;
-
-    // (rate of change within one pixel)
-    float sdf_pixel = 0.5 / sdf_radius;
-
-    // scale strokeWidth to sdf_pixel
-    float stroke_width = strokeWidth * sdf_pixel;
-
-    // scale sdf (texture is scaled depeding on font size)
-    stroke_width /= fontScale;
-
-    float v_sdf_threshold = 0.5 - stroke_width;
-
-    // 0.1245 antialiasing filter width of unscaled glyph
-    float filter_width = sdf_pixel / fontScale * (0.5 + 0.25);
-
-    if (v_sdf_threshold - filter_width < 0) {
-        // modify stroke width to be within sdf_radius
-        LOG("size:%f scale:%f stroke:%f att:%f thres:%f filter:%f ==> %f",
-            _params.fontSize, fontScale, _params.strokeWidth,
-            stroke_width, v_sdf_threshold, filter_width,
-            v_sdf_threshold - filter_width);
-    }
-#endif
-    auto ctx = m_style.context();
-
-    uint32_t strokeAttrib = std::max(std::min(strokeWidth / ctx->maxStrokeWidth() * 255.f, 255.f), 0.f);
-
-    m_scratch.stroke = (_params.strokeColor & 0x00ffffff) + (strokeAttrib << 24);
-    m_scratch.fill = _params.fill;
-    m_scratch.fontScale = std::min(fontScale * 64.f, 255.f);
-
-    LineWrap wrap;
-
-    alf::LineLayout line;
-    alf::LineMetrics metrics;
-
-    {
-        std::lock_guard<std::mutex> lock(ctx->mutex());
-
-        // Shape text
-        line = m_shaper.shape(_params.font, *renderText);
-
-        if (line.shapes().size() == 0) {
-            LOGD("Empty text line");
-            return false;
-        }
-
-        line.setScale(fontScale);
-
-        // m_batch.draw() calls FontContext's TextureCallback for new glyphs
-        // and ScratchBuffer's MeshCallback (drawGlyph) for vertex quads of each
-        // glyph in LineLayout.
-
-        // reset count of added quads in drawGlyph
-        m_scratch.numQuads = 0;
-
-        if (_type == Label::Type::point && _params.wordWrap) {
-            auto wrap = drawWithLineWrapping(line, m_batch, MIN_LINE_WIDTH,
-                                             _params.maxLineWidth, _params.align,
-                                             m_style.pixelScale());
-            metrics = wrap.metrics;
-        } else {
-            glm::vec2 position(0);
-            m_batch.drawShapeRange(line, 0, line.shapes().size(), position, metrics);
-        }
-    }
-
-    // Bounding box dimension
-    float width = metrics.aabb.z - metrics.aabb.x;
-    float height = metrics.aabb.w - metrics.aabb.y;
-
-    // TextLabel parameter: Dimension
-    m_scratch.labelDimension = glm::vec2(width, height);
-
-    // Offset to center all glyphs around 0/0
-    glm::vec2 offset((metrics.aabb.x + width * 0.5) * position_scale,
-                     (metrics.aabb.y + height * 0.5) * position_scale);
-
-    auto it = m_scratch.quads.end() - m_scratch.numQuads;
-    while (it != m_scratch.quads.end()) {
-        it->quad[0].pos -= offset;
-        it->quad[1].pos -= offset;
-        it->quad[2].pos -= offset;
-        it->quad[3].pos -= offset;
-        ++it;
-    }
-
-    return true;
-}
-
-void TextStyleBuilder::addLabel(const TextStyle::Parameters& _params, Label::Type _type,
-                                Label::Transform _transform) {
-
-    int numQuads = m_scratch.numQuads;
-    int quadOffset = m_scratch.quads.size() - numQuads;
-
-    m_scratch.labels.emplace_back(new TextLabel(_transform, _type, _params.labelOptions, _params.anchor,
-                                                {m_scratch.fill, m_scratch.stroke, m_scratch.fontScale},
-                                                m_scratch.labelDimension, *m_textLabels, {quadOffset, numQuads}));
-}
-
 TextStyle::Parameters TextStyleBuilder::applyRule(const DrawRule& _rule,
                                                   const Properties& _props) const {
 
@@ -389,6 +190,173 @@ TextStyle::Parameters TextStyleBuilder::applyRule(const DrawRule& _rule,
     p.labelOptions.paramHash = hash(p);
 
     return p;
+}
+
+// TODO use icu transforms
+// http://source.icu-project.org/repos/icu/icu/trunk/source/samples/ustring/ustring.cpp
+
+std::string TextStyleBuilder::applyTextTransform(const TextStyle::Parameters& _params,
+                                                 const std::string& _string) {
+    std::locale loc;
+    std::string text = _string;
+
+    switch (_params.transform) {
+        case TextLabelProperty::Transform::capitalize:
+            text[0] = toupper(text[0], loc);
+            if (text.size() > 1) {
+                for (size_t i = 1; i < text.length(); ++i) {
+                    if (text[i - 1] == ' ') {
+                        text[i] = std::toupper(text[i], loc);
+                    }
+                }
+            }
+            break;
+        case TextLabelProperty::Transform::lowercase:
+            for (size_t i = 0; i < text.length(); ++i) {
+                text[i] = std::tolower(text[i], loc);
+            }
+            break;
+        case TextLabelProperty::Transform::uppercase:
+            // TODO : use to wupper when any wide character is detected
+            for (size_t i = 0; i < text.length(); ++i) {
+                text[i] = std::toupper(text[i], loc);
+            }
+            break;
+        default:
+            break;
+    }
+
+    return text;
+}
+
+std::string TextStyleBuilder::resolveTextSource(const std::string& textSource,
+                                                const Properties& props) const {
+
+    std::string tmp, item;
+
+    // Meaning we have a yaml sequence defining fallbacks
+    if (textSource.find(',') != std::string::npos) {
+        std::stringstream ss(textSource);
+
+        // Parse fallbacks
+        while (std::getline(ss, tmp, ',')) {
+            if (props.getAsString(tmp, item)) {
+                return item;
+            }
+        }
+    }
+
+    // Fallback to default text source
+    if (props.getAsString(textSource, item)) {
+        return item;
+    }
+
+    // Default to 'name'
+    return props.getString(key_name);
+}
+
+bool TextStyleBuilder::prepareLabel(TextStyle::Parameters& _params, Label::Type _type) {
+
+    if (_params.text.empty() || _params.fontSize <= 0.f) {
+        LOGD("invalid params: '%s' %f", _params.text.c_str(), _params.fontSize);
+        return false;
+    }
+
+    // Apply text transforms
+    const std::string* renderText;
+    std::string text;
+
+    if (_params.transform == TextLabelProperty::Transform::none) {
+        renderText = &_params.text;
+    } else {
+        text = applyTextTransform(_params, _params.text);
+        renderText = &text;
+    }
+
+    // Scale factor by which the texture glyphs are scaled to match fontSize
+    float fontScale = (_params.fontSize * m_style.pixelScale()) / _params.font->size();
+
+    // Stroke width is normalized by the distance of the SDF spread, then
+    // scaled to a char, then packed into the "alpha" channel of stroke.
+    // Maximal strokeWidth is 3px, attribute is normalized to 0-1 range.
+    float strokeWidth = _params.strokeWidth * m_style.pixelScale();
+
+    auto ctx = m_style.context();
+
+    uint32_t strokeAttrib = std::max(std::min(strokeWidth / ctx->maxStrokeWidth() * 255.f, 255.f), 0.f);
+
+    m_scratch.stroke = (_params.strokeColor & 0x00ffffff) + (strokeAttrib << 24);
+    m_scratch.fill = _params.fill;
+    m_scratch.fontScale = std::min(fontScale * 64.f, 255.f);
+
+    LineWrap wrap;
+
+    alf::LineLayout line;
+    alf::LineMetrics metrics;
+
+    {
+        std::lock_guard<std::mutex> lock(ctx->mutex());
+
+        // Shape text
+        line = m_shaper.shape(_params.font, *renderText);
+
+        if (line.shapes().size() == 0) {
+            LOGD("Empty text line");
+            return false;
+        }
+
+        line.setScale(fontScale);
+
+        // m_batch.draw() calls FontContext's TextureCallback for new glyphs
+        // and ScratchBuffer's MeshCallback (drawGlyph) for vertex quads of each
+        // glyph in LineLayout.
+
+        // reset count of added quads in drawGlyph
+        m_scratch.numQuads = 0;
+
+        if (_type == Label::Type::point && _params.wordWrap) {
+            auto wrap = drawWithLineWrapping(line, m_batch, MIN_LINE_WIDTH,
+                                             _params.maxLineWidth, _params.align,
+                                             m_style.pixelScale());
+            metrics = wrap.metrics;
+        } else {
+            glm::vec2 position(0);
+            m_batch.drawShapeRange(line, 0, line.shapes().size(), position, metrics);
+        }
+    }
+
+    // Bounding box dimension
+    float width = metrics.aabb.z - metrics.aabb.x;
+    float height = metrics.aabb.w - metrics.aabb.y;
+
+    // TextLabel parameter: Dimension
+    m_scratch.labelDimension = glm::vec2(width, height);
+
+    // Offset to center all glyphs around 0/0
+    glm::vec2 offset((metrics.aabb.x + width * 0.5) * position_scale,
+                     (metrics.aabb.y + height * 0.5) * position_scale);
+
+    auto it = m_scratch.quads.end() - m_scratch.numQuads;
+    while (it != m_scratch.quads.end()) {
+        it->quad[0].pos -= offset;
+        it->quad[1].pos -= offset;
+        it->quad[2].pos -= offset;
+        it->quad[3].pos -= offset;
+        ++it;
+    }
+
+    return true;
+}
+
+void TextStyleBuilder::addLabel(const TextStyle::Parameters& _params, Label::Type _type,
+                                Label::Transform _transform) {
+
+    int numQuads = m_scratch.numQuads;
+    int quadOffset = m_scratch.quads.size() - numQuads;
+
+    m_scratch.labels.emplace_back(new TextLabel(_transform, _type, _params.labelOptions, _params.anchor,
+                                                {m_scratch.fill, m_scratch.stroke, m_scratch.fontScale},
+                                                m_scratch.labelDimension, *m_textLabels, {quadOffset, numQuads}));
 }
 
 void TextStyleBuilder::ScratchBuffer::drawGlyph(const alf::Rect& q, const alf::AtlasGlyph& atlasGlyph) {
