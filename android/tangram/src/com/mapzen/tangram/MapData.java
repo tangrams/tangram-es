12dbe21db93b86ea8eed448c62bde8e6db733c89
Matt Blair <blair1618@gmail.com>
2016-03-22 21:05:08 -0700

    Add documentation for MapData

--- android/tangram/src/com/mapzen/tangram/MapData.java
+++ android/tangram/src/com/mapzen/tangram/MapData.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * MapData is a named, dynamic collection of drawable map features.
+ * {@code MapData} is a named collection of drawable map features.
  */
 public class MapData {
 
@@ -20,14 +20,31 @@ public class MapData {
     List<String> geojson = new ArrayList<>();
     List<Geometry> features = new ArrayList<>();
 
+    /**
+     * Construct a collection of drawable map features.
+     * @param name The name of the data collection. Once added to a map, features from this
+     * {@code MapData} will be available from a data source with this name, just like a data source
+     * specified in a scene file.
+     */
     public MapData(String name) {
         this.name = name;
     }
 
+    /**
+     * Get the name of this {@code MapData}.
+     * @return The name.
+     */
     public String name() {
         return name;
     }
 
+    /**
+     * Add the features from this {@code MapData} to a map.
+     * <p>
+     * This {@code MapData} will be associated with the given map until {@link #removeFromMap()}
+     * is called or until this method is called again.
+     * @param map The {@code MapController} managing the destination map.
+     */
     public void addToMap(MapController map) {
         if (this.map != null) {
             removeFromMap();
@@ -37,12 +54,23 @@ public class MapData {
         syncWithMap();
     }
 
+    /**
+     * Remove this {@code MapData} from the map it is currently associated with. This must not be
+     * called when this {@code MapData} is not associated with a map.
+     */
     public void removeFromMap() {
         map.nativeRemoveDataSource(pointer);
         pointer = 0;
         map = null;
     }
 
+    /**
+     * Synchronize the features in this {@code MapData} with the map.
+     * <p>
+     * This method must be called after features are added or removed to update their
+     * appearance in the map. This method must not be called when this {@code MapData} is not
+     * associated with a map.
+     */
     public void syncWithMap() {
         map.nativeClearDataSource(pointer);
         for (String data : geojson) {
@@ -56,26 +84,58 @@ public class MapData {
         }
     }
 
+    /**
+     * Add a point feature to this collection.
+     * @param point The coordinates of the feature.
+     * @param properties The properties of the feature, used for filtering and styling according to
+     * the scene file used by the map.
+     * @return This object, for chaining.
+     */
     public MapData addPoint(LngLat point, Map<String, String> properties) {
         features.add(new Point(point, properties));
         return this;
     }
 
+    /**
+     * Add a polyline feature to this collection.
+     * @param polyline A list of coordinates that define the line segments of the feature.
+     * @param properties The properties of the feature, used for filtering and styling according to
+     * the scene file used by the map.
+     * @return This object, for chaining.
+     */
     public MapData addPolyline(List<LngLat> polyline, Map<String, String> properties) {
         features.add(new Polyline(polyline, properties));
         return this;
     }
 
+    /**
+     * Add a polygon feature to this collection.
+     * @param polygon A list of rings describing the shape of the feature. Each
+     * ring is a list of coordinates. The first ring is taken as the "exterior" of the polygon and
+     * rings with opposite winding are considered "holes".
+     * @param properties The properties of the feature, used for filtering and styling according to
+     * the scene file used by the map.
+     * @return This object, for chaining.
+     */
     public MapData addPolygon(List<List<LngLat>> polygon, Map<String, String> properties) {
         features.add(new Polygon(polygon, properties));
         return this;
     }
 
+    /**
+     * Add features described in a GeoJSON string to this collection.
+     * @param data A string containing a <a href="http://geojson.org/">GeoJSON</a> FeatureCollection
+     * @return This object, for chaining.
+     */
     public MapData addGeoJson(String data) {
         geojson.add(data);
         return this;
     }
 
+    /**
+     * Remove all features from this collection.
+     * @return This object, for chaining.
+     */
     public MapData clear() {
         geojson.clear();
         features.clear();
