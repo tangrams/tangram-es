7a395014c58fcc0f2fc76ec71deeb64b1d0a5426
Matt Blair <blair1618@gmail.com>
2016-03-30 16:22:18 -0400

    Add getMapAsync as primary initialization process

--- android/tangram/src/com/mapzen/tangram/MapView.java
+++ android/tangram/src/com/mapzen/tangram/MapView.java
@@ -2,9 +2,13 @@ package com.mapzen.tangram;
 
 import android.content.Context;
 import android.opengl.GLSurfaceView;
+import android.os.AsyncTask;
 import android.util.AttributeSet;
+import android.widget.FrameLayout;
 
-public class MapView extends GLSurfaceView {
+public class MapView extends FrameLayout {
+
+    protected GLSurfaceView glSurfaceView;
 
     public MapView(Context context) {
 
@@ -22,11 +26,46 @@ public class MapView extends GLSurfaceView {
 
     }
 
-    private void configureGLSurfaceView() {
+    public interface OnMapReadyCallback {
 
-        setEGLContextClientVersion(2);
-        setPreserveEGLContextOnPause(true);
-        setEGLConfigChooser(new ConfigChooser(8, 8, 8, 0, 16, 0));
+        void onMapReady(MapController mapController);
+
+    }
+
+    /**
+     * Construct a MapController asynchronously; may only be called from the UI thread
+     * @param listener The object to receive the resulting MapController in a callback;
+     * the callback will be made on the UI thread
+     * @param sceneFilePath Location of the YAML scene file within the asset bundle
+     */
+    public void getMapAsync(final OnMapReadyCallback listener, final String sceneFilePath) {
+
+        final Context context = getContext();
+
+        new AsyncTask<Void, Void, MapController>() {
+
+            @Override
+            protected MapController doInBackground(Void... params) {
+                return new MapController(context, sceneFilePath);
+            }
+
+            @Override
+            protected void onPostExecute(MapController map) {
+                map.setView(glSurfaceView);
+                addView(glSurfaceView);
+                listener.onMapReady(map);
+            }
+
+        }.execute();
+
+    }
+
+    protected void configureGLSurfaceView() {
+
+        glSurfaceView = new GLSurfaceView(getContext());
+        glSurfaceView.setEGLContextClientVersion(2);
+        glSurfaceView.setPreserveEGLContextOnPause(true);
+        glSurfaceView.setEGLConfigChooser(new ConfigChooser(8, 8, 8, 0, 16, 0));
 
     }
 
