51bbe646a563f5a2daf2681d167d79bece81aa5e
Karim Naaji <karim.naaji@gmail.com>
2016-04-06 18:26:23 -0400

    Unit tests

--- tests/unit/sceneUpdateTests.cpp
+++ tests/unit/sceneUpdateTests.cpp
@@ -12,7 +12,7 @@ using namespace Tangram;
 
 const char* path = "scene.yaml";
 
-TEST_CASE("") {
+TEST_CASE("Scene update for cameras scene block") {
     Scene scene("scene.yaml");
 
     auto sceneString = stringFromFile(setResourceRoot(path).c_str(), PathType::resource);
@@ -25,11 +25,37 @@ TEST_CASE("") {
 
     // Update
     scene.setComponent("cameras.iso-camera.active", "true");
+    scene.setComponent("cameras.iso-camera.type", "perspective");
 
     // Tangram apply scene updates, reload the scene
     REQUIRE(SceneLoader::loadScene(sceneString, scene, root));
     scene.clearUserDefines();
 
     REQUIRE(root["cameras"]["iso-camera"]["active"].Scalar() == "true");
+    REQUIRE(root["cameras"]["iso-camera"]["type"].Scalar() == "perspective");
+}
+
+TEST_CASE("Scene update for light scene block") {
+    Scene scene("scene.yaml");
+
+    auto sceneString = stringFromFile(setResourceRoot(path).c_str(), PathType::resource);
+
+    REQUIRE(!sceneString.empty());
+
+    Node root;
+    REQUIRE(SceneLoader::loadScene(sceneString, scene, root));
+
+    // Update
+    scene.setComponent("lights.light1.ambient", "0.9");
+    scene.setComponent("lights.light1.type", "spotlight");
+    scene.setComponent("lights.light1.origin", "ground");
+
+    // Tangram apply scene updates, reload the scene
+    REQUIRE(SceneLoader::loadScene(sceneString, scene, root));
+    scene.clearUserDefines();
+
+    REQUIRE(root["lights"]["light1"]["ambient"].Scalar() == "0.9");
+    REQUIRE(root["lights"]["light1"]["type"].Scalar() == "spotlight");
+    REQUIRE(root["lights"]["light1"]["origin"].Scalar() == "ground");
 }
 
