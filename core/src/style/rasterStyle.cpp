70f76239ab5392ef8798638d41ce3c77617cb9f5
Karim Naaji <karim.naaji@gmail.com>
2016-04-13 11:25:47 -0400

    Place holder for RasterStyle

---
+++ core/src/style/rasterStyle.cpp
@@ -0,0 +1,17 @@
+#include "rasterStyle.h"
+
+#include "gl/shaderProgram.h"
+#include "gl/mesh.h"
+
+namespace Tangram {
+
+RasterStyle::RasterStyle(std::string _name, Blending _blendMode, GLenum _drawMode)
+    : PolygonStyle(_name, _blendMode, _drawMode)
+{
+}
+
+void RasterStyle::constructShaderProgram() {
+    PolygonStyle::constructShaderProgram();
+}
+
+}
