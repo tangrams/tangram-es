0f2617ac8c69e948cf531569f432d6c28e4bf2f4
Karim Naaji <karim.naaji@gmail.com>
2016-04-15 16:30:31 -0400

    Add pixel sampler/raster size shader accesors

--- core/resources/shaders/rasters.glsl
+++ core/resources/shaders/rasters.glsl
@@ -4,10 +4,20 @@ uniform sampler2D u_rasters[TANGRAM_NUM_RASTER_SOURCES];
 uniform vec2 u_raster_sizes[TANGRAM_NUM_RASTER_SOURCES];
 uniform vec3 u_raster_offsets[TANGRAM_NUM_RASTER_SOURCES];
 
+// TODO
+#define adjustRasterUV(raster_index, uv) \
+    (uv)
+
 #define currentRasterUV(raster_index) \
     (v_modelpos_base_zoom.xy)
 
+#define sampleRasterAtPixel(raster_index, pixel) \
+    (texture2D(u_rasters[raster_index], adjustRasterUV(raster_index, (pixel) / rasterPixelSize(raster_index))))
+
 #define sampleRaster(raster_index) \
     (texture2D(u_rasters[raster_index], currentRasterUV(raster_index)))
+
+#define rasterPixelSize(raster_index) \
+    (u_raster_sizes[raster_index])
 
 #endif
