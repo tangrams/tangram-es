e50d9bc5c1f387e0be3cfbf6eed5cd1f761fc51a
Varun Talwar <tallytalwar@gmail.com>
2016-04-18 14:32:25 -0400

    Fix scene redraw/reload texture issues & more
    - Make loadRasterTextures recursive. Datasource can have raster references which then can have
    further raster references
    - cancel raster tasks when uber tasks get cancelled
    - make sure to have raster tasks created even when raster data has been fetched (this fixes the
    scene reload texture issue which was seen before)

--- core/src/tile/tileManager.h
+++ core/src/tile/tileManager.h
@@ -13,6 +13,7 @@
 #include <mutex>
 #include <tuple>
 #include <set>
+#include <data/dataSource.h>
 
 namespace Tangram {
 
@@ -105,6 +106,10 @@ private:
 
         void cancelTask() {
             if (task) {
+                for (auto& raster : task->rasterTasks()) {
+                    raster->cancel();
+                }
+                task->rasterTasks().clear();
                 task->cancel();
                 task.reset();
             }
@@ -159,7 +164,8 @@ private:
     void enqueueTask(TileSet& _tileSet, const TileID& _tileID, const ViewState& _view);
 
     void loadTiles();
-    void loadRasterTasks(TileSet& tileSet, std::shared_ptr<TileTask>& tileTask, const TileID& tileID);
+    void loadRasterTasks(std::vector<std::shared_ptr<DataSource>>& rasters, std::shared_ptr<TileTask>& tileTask,
+                         const TileID& tileID);
 
     /*
      * Constructs a future (async) to load data of a new visible tile this is
