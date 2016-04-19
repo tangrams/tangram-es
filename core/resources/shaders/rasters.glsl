64f762a79996c1cff8d32820b0c8113e07820443
Karim Naaji <karim.naaji@gmail.com>
2016-04-19 17:22:51 -0400

    Add shader define for current raster pixel

--- core/resources/shaders/rasters.glsl
+++ core/resources/shaders/rasters.glsl
@@ -10,6 +10,9 @@ uniform vec3 u_raster_offsets[TANGRAM_NUM_RASTER_SOURCES];
 #define currentRasterUV(raster_index) \
     (adjustRasterUV(raster_index, v_modelpos_base_zoom.xy))
 
+#define currentRasterPixel(raster_index) \
+    (currentRasterUV(raster_index) * rasterPixelSize(raster_index))
+
 #define sampleRasterAtPixel(raster_index, pixel) \
     (texture2D(u_rasters[raster_index], adjustRasterUV(raster_index, (pixel) / rasterPixelSize(raster_index))))
 
