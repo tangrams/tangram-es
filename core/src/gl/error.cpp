90b9ae14f2e9639fd319e0dc5143ad7c58d83b52
Karim Naaji <karim.naaji@gmail.com>
2016-05-02 10:45:02 -0400

    Check GL error in loop, Add invalid framebuffer op define

--- core/src/gl/error.cpp
+++ core/src/gl/error.cpp
@@ -7,7 +7,8 @@ std::unordered_map<GLenum, std::string> Error::s_GlErrorCodesToStrings = {
         {GL_INVALID_ENUM, "GL_INVALID_ENUM"},
         {GL_INVALID_VALUE, "GL_INVALID_VALUE"},
         {GL_INVALID_OPERATION, "GL_INVALID_OPERATION"},
-        {GL_OUT_OF_MEMORY, "GL_OUT_OF_MEMORY"}
+        {GL_OUT_OF_MEMORY, "GL_OUT_OF_MEMORY"},
+        {GL_INVALID_FRAMEBUFFER_OPERATION, "GL_INVALID_FRAMEBUFFER_OPERATION"}
     };
 
 bool Error::hadGlError(const std::string& _locationTag) {
@@ -29,10 +30,14 @@ bool Error::hadGlError(const std::string& _locationTag) {
 void Error::glError(const char* stmt, const char* fname, int line) {
     GLenum err = glGetError();
 
-    auto it = s_GlErrorCodesToStrings.find(err);
+    while (err != GL_NO_ERROR) {
+        auto it = s_GlErrorCodesToStrings.find(err);
 
-    if (it != s_GlErrorCodesToStrings.end() && err != GL_NO_ERROR) {
-        LOGE("OpenGL error %s, at %s:%i - for %s\n", it->second.c_str(), fname, line, stmt);
+        if (it != s_GlErrorCodesToStrings.end()) {
+            LOGE("OpenGL error %s, at %s:%i - for %s\n", it->second.c_str(), fname, line, stmt);
+        }
+
+        err = glGetError();
     }
 }
 
