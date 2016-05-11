5382da4fd066df2d7b2b8d7c4963a2e99acc7146
Varun Talwar <tallytalwar@gmail.com>
2016-05-11 11:11:17 -0400

    Importing resolves all resource and import paths
    - Make sure to append base parent url to resources/imports before actually merging/loading the
    scene.
    - Prepends parent base url to texture resources (scene.textures, style.textures, style's material
    textures and styles' shaders' uniform textures).

--- core/src/scene/importer.cpp
+++ core/src/scene/importer.cpp
@@ -1,5 +1,6 @@
 #include "importer.h"
 #include "platform.h"
+#include "scene/sceneLoader.h"
 #include "util/topologicalSort.h"
 #include "yaml-cpp/yaml.h"
 
@@ -19,24 +20,137 @@ Node Importer::applySceneImports(const std::string& scenePath) {
     return Node();
 }
 
-bool Importer::loadScene(const std::string& scenePath) {
+std::string Importer::getFilename(const std::string& filePath) {
+    std::string sceneName = filePath;
+    std::regex r("[^//]+$");
+    std::smatch match;
+    if (std::regex_search(sceneName, match, r)) { sceneName = match[0]; }
+    return sceneName;
+}
 
+std::string Importer::normalizePath(const std::string &path,
+                                         const std::string &parentPath) {
+
+    //TODO: Handle network urls
+
+    if (path[0] == '/') {
+        //absolute path, return as it is
+        return path;
+    }
+
+    std::regex r("[^//]+$");
+    return std::regex_replace(parentPath, r, path);
+}
+
+void Importer::normalizeSceneImports(Node& root, const std::string& parentPath) {
+    if (Node import = root["import"]) {
+        if (import.IsScalar()) {
+            import = normalizePath(import.Scalar(), parentPath);
+        } else if (import.IsSequence()) {
+            for (Node imp : import) {
+                if (imp.IsScalar()) { imp = normalizePath(imp.Scalar(), parentPath); }
+            }
+        }
+    }
+}
+
+void Importer::setNormalizedTexture(Node& texture, const std::vector<std::string>& names,
+        const std::string& parentPath) {
+
+    for (auto& name : names) {
+        if (m_textureNames.find(name) == m_textureNames.end()) {
+            m_textureNames.insert(name);
+            if (names.size() > 1) {
+                texture.push_back(normalizePath(name, parentPath));
+            } else {
+                texture = normalizePath(name, parentPath);
+            }
+        }
+    }
+}
+
+void Importer::normalizeSceneTextures(Node& root, const std::string& parentPath) {
+
+    if (Node textures = root["textures"]) {
+        for (auto texture : textures) {
+            if (Node textureUrl = texture.second["url"]) {
+                setNormalizedTexture(textureUrl, {textureUrl.Scalar()}, parentPath);
+            }
+        }
+    }
+
+    if (Node styles = root["styles"]) {
+
+        for (auto styleNode : styles) {
+
+            auto style = styleNode.second;
+            //style->texture
+            if (Node texture = style["texture"]) {
+                if (texture.IsScalar()) {
+                    setNormalizedTexture(texture, {texture.Scalar()}, parentPath);
+                }
+            }
+
+            //style->material->texture
+            if (Node material = style["material"]) {
+                for (auto& prop : {"emission", "ambient", "diffuse", "specular", "normal"}) {
+                    if (auto propNode = material[prop]) {
+                        if (auto matTexture = material[propNode]["texture"]) {
+                            if (matTexture.IsScalar()) {
+                                setNormalizedTexture(matTexture, {matTexture.Scalar()}, parentPath);
+                            }
+                        }
+                    }
+                }
+            }
+
+            //style->shader->uniforms->texture
+            if (Node shaders = style["shaders"]) {
+                if (Node uniforms = shaders["uniforms"]) {
+                    for (auto uniformNode : uniforms) {
+                        StyleUniform styleUniform;
+                        auto uniformValue = uniformNode.second;
+                        if (SceneLoader::parseStyleUniforms(uniformValue, nullptr, styleUniform) &&
+                                styleUniform.type == "sampler2D") {
+                            if (styleUniform.value.is<UniformTextureArray>()) {
+                                setNormalizedTexture(uniformValue,
+                                             styleUniform.value.get<UniformTextureArray>().names,
+                                             parentPath);
+                            } else {
+                                auto name = styleUniform.value.get<std::string>();
+                                setNormalizedTexture(uniformValue, {name}, parentPath);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+bool Importer::loadScene(const std::string& path) {
+
+    auto scenePath = path;
     auto sceneString = stringFromFile(setResourceRoot(scenePath.c_str()).c_str(), PathType::resource);
-    auto sceneName = scenePath;
+    auto sceneName = getFilename(scenePath);
+
+    if (m_scenes.find(sceneName) != m_scenes.end()) { return true; }
+
+    // Make sure all references from uber scene file are relative to itself, instead of being
+    // absolute paths (Example: when loading a file using command line args).
+    // TODO: Could be made better later.
+    if (m_scenes.size() == 0 && scenePath[0] == '/') { scenePath = getFilename(scenePath); }
 
     try {
         auto root = YAML::Load(sceneString);
+        normalizeSceneImports(root, scenePath);
+        normalizeSceneTextures(root, scenePath);
         auto imports = getScenesToImport(root);
-        std::regex r("[^//]+$");
-        std::smatch match;
-        if (std::regex_search(sceneName, match, r)) { sceneName = match[0]; }
+        m_scenes[sceneName] = root;
         for (const auto& import : imports) {
             // TODO: What happens when parsing fails for an import
-            // Relative path of imports with respect to the original
-            auto importPath = std::regex_replace(scenePath, r, import);
-            loadScene(importPath);
+            loadScene(import);
         }
-        m_scenes[sceneName] = root;
     }
     catch (YAML::ParserException e) {
         LOGE("Parsing scene config '%s'", e.what());
@@ -71,8 +185,15 @@ std::vector<std::string> Importer::getImportOrder() {
         const auto& name = scene.first.k;
         const auto& sceneRoot = scene.second;
         for (const auto& import : getScenesToImport(sceneRoot)) {
-            dependencies.push_back( {import, name} );
+            auto importName = getFilename(import);
+            // TODO: fixme do not allow cycles
+            dependencies.push_back( {importName, name} );
         }
+    }
+
+    if (dependencies.empty()) {
+        // only possible when only 1 scene to load with no imports
+        return {m_scenes.map[0].first.k};
     }
 
     return topologicalSort(dependencies);
@@ -99,6 +220,9 @@ void Importer::mergeMapFields(Node& target, const Node& import) {
     for (const auto& entry : import) {
 
         const auto &key = entry.first.Scalar();
+
+        // do not merge ignore property
+        if (key == "import") { continue; }
 
         if (!target[key]) {
             target[key] = entry.second;
