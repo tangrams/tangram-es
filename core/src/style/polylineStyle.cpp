991f9a82be4187ca1b2334cbfd7fabb193242dcc
Matt Blair <blair1618@gmail.com>
2016-05-17 18:29:32 -0400

    Re-scale overzoomed line widths in vertex shader

--- core/src/style/polylineStyle.cpp
+++ core/src/style/polylineStyle.cpp
@@ -163,8 +163,8 @@ void PolylineStyleBuilder<V>::setup(const Tile& tile) {
 
     // When a tile is overzoomed, we are actually styling the area of its
     // 'source' tile, which will have a larger effective pixel size at the
-    // 'style' zoom level.
-    m_tileSizePixels *= std::exp2(id.s - id.z);
+    // 'style' zoom level. This scaling is performed in the vertex shader to
+    // prevent loss of precision for small dimensions in packed attributes.
 }
 
 template <class V>
