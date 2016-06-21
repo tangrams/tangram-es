be922547e2051b77a5af623777f0bafd89dc5873
Matt Blair <blair1618@gmail.com>
2016-06-21 15:52:39 -0400

    Update RPi demo to use Map class

--- rpi/src/main.cpp
+++ rpi/src/main.cpp
@@ -30,6 +30,8 @@
 struct timeval tv;
 unsigned long long timePrev, timeStart;
 
+Tangram::Map* map = nullptr;
+
 static bool bUpdate = true;
 
 //==============================================================================
@@ -98,21 +100,21 @@ void setup(int argc, char **argv) {
         }
     }
 
-    Tangram::initialize(scene.c_str());
-    Tangram::loadSceneAsync(scene.c_str());
-    Tangram::setupGL();
-    Tangram::resize(width, height);
+    map = new Tangram::Map();
+    map->loadSceneAsync(scene.c_str());
+    map->setupGL();
+    map->resize(width, height);
     if (lon != 0.0f && lat != 0.0f) {
-        Tangram::setPosition(lon,lat);
+        map->setPosition(lon,lat);
     }
     if (zoom != 0.0f) {
-        Tangram::setZoom(zoom);
+        map->setZoom(zoom);
     }
     if (tilt != 0.0f) {
-        Tangram::setTilt(glm::radians(tilt));
+        map->setTilt(glm::radians(tilt));
     }
     if (rot != 0.0f) {
-        Tangram::setRotation(glm::radians(rot));
+        map->setRotation(glm::radians(rot));
     }
 }
 
@@ -125,11 +127,11 @@ void newFrame() {
 
     //logMsg("New frame (delta %d msec)\n",delta);
 
-    Tangram::update(delta);
+    map->update(delta);
     timePrev = timeNow;
 
     // Render
-    Tangram::render();
+    map->render();
 
     renderGL();
 }
@@ -139,22 +141,22 @@ void newFrame() {
 void onKeyPress(int _key) {
     switch (_key) {
         case KEY_ZOOM_IN:
-            Tangram::handlePinchGesture(0.0,0.0,0.5,0.0);
+            map->handlePinchGesture(0.0,0.0,0.5,0.0);
             break;
         case KEY_ZOOM_OUT:
-            Tangram::handlePinchGesture(0.0,0.0,2.0,0.0);
+            map->handlePinchGesture(0.0,0.0,2.0,0.0);
             break;
         case KEY_UP:
-            Tangram::handlePanGesture(0.0,0.0,0.0,100.0);
+            map->handlePanGesture(0.0,0.0,0.0,100.0);
             break;
         case KEY_DOWN:
-            Tangram::handlePanGesture(0.0,0.0,0.0,-100.0);
+            map->handlePanGesture(0.0,0.0,0.0,-100.0);
             break;
         case KEY_LEFT:
-            Tangram::handlePanGesture(0.0,0.0,100.0,0.0);
+            map->handlePanGesture(0.0,0.0,100.0,0.0);
             break;
         case KEY_RIGHT:
-            Tangram::handlePanGesture(0.0,0.0,-100.0,0.0);
+            map->handlePanGesture(0.0,0.0,-100.0,0.0);
             break;
         case KEY_ESC:
             bUpdate = false;
@@ -176,10 +178,7 @@ void onMouseClick(float _x, float _y, int _button) {
 void onMouseDrag(float _x, float _y, int _button) {
     if( _button == 1 ){
 
-        Tangram::handlePanGesture(  _x-getMouseVelX()*1.0,
-                                    _y+getMouseVelY()*1.0,
-                                    _x,
-                                    _y);
+        map->handlePanGesture(_x - getMouseVelX(), _y + getMouseVelY(), _x, _y);
 
     } else if( _button == 2 ){
         if ( getKeyPressed() == 'r') {
@@ -188,11 +187,11 @@ void onMouseDrag(float _x, float _y, int _button) {
             if( _x < getWindowWidth()/2.0 ) {
                 scale *= -1.0;
             }
-            Tangram::handleRotateGesture(getWindowWidth()/2.0, getWindowHeight()/2.0, rot*scale);
+            map->handleRotateGesture(getWindowWidth()/2.0, getWindowHeight()/2.0, rot*scale);
         } else if ( getKeyPressed() == 't') {
-            Tangram::handleShoveGesture(getMouseVelY()*0.005);
+            map->handleShoveGesture(getMouseVelY()*0.005);
         } else {
-            Tangram::handlePinchGesture(getWindowWidth()/2.0, getWindowHeight()/2.0, 1.0 + getMouseVelY()*0.001, 0.f);
+            map->handlePinchGesture(getWindowWidth()/2.0, getWindowHeight()/2.0, 1.0 + getMouseVelY()*0.001, 0.f);
         }
 
     }
