a59633a3c5371c81af28d43ec1a24ed9615f221a
Matt Blair <blair1618@gmail.com>
2016-06-29 16:29:57 -0400

    Add Tizen platform function to set a custom re-render callback

--- tizen/inc/platform_tizen.h
+++ tizen/inc/platform_tizen.h
@@ -1,8 +1,11 @@
 #pragma once
 
 #include "platform.h"
+#include <functional>
 
 bool shouldRender();
+
+void setRenderCallbackFunction(std::function<void()> callback);
 
 void finishUrlRequests();
 void initUrlRequests();
