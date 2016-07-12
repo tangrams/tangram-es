4fed1165caf1b34c38838b9df74b21590053a513
Matt Smollinger <matt.smollinger@mapzen.com>
2016-07-12 17:15:46 -0400

    Adding in missing plist and header file for ios framework compilation

---
+++ ios/tangram_framework/tangram_framework.h
@@ -0,0 +1,19 @@
+//
+//  tangram_framework.h
+//  tangram_framework
+//
+//  Created by Matt Smollinger on 7/8/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for tangram_framework.
+FOUNDATION_EXPORT double tangram_frameworkVersionNumber;
+
+//! Project version string for tangram_framework.
+FOUNDATION_EXPORT const unsigned char tangram_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <tangram_framework/PublicHeader.h>
+
+#import <tangram_framework/ViewController.h>
