682a8dc4e39d09c3d128e6547b6773b3c4f618d4
Tim Engler <engler@gmail.com>
2016-07-26 16:50:49 +0800

    added another native type armaebi-7a or something

--- android_aar_build.sh
+++ android_aar_build.sh
@@ -4,11 +4,8 @@ cd `dirname $0`
 CMAKE_OPTIONS="-DCMAKE_INSTALL_PREFIX=build" RELEASE=1 make android-native-lib ANDROID_ARCH=x86 && \
 CMAKE_OPTIONS="-DCMAKE_INSTALL_PREFIX=build" RELEASE=1 make android-native-lib ANDROID_ARCH=armeabi && \
 CMAKE_OPTIONS="-DCMAKE_INSTALL_PREFIX=build" RELEASE=1 make android-native-lib ANDROID_ARCH=arm64-v8a && \
+CMAKE_OPTIONS="-DCMAKE_INSTALL_PREFIX=build" RELEASE=1 make android-native-lib ANDROID_ARCH=armeabi-v7a && \
 RELEASE=1 make android-tangram-apk && \
 echo "Output is `dirname $0`/android/tangram/build/outputs/aar/tangram-release.aar"
 
-#### Currently Not building the following architectures
-#make android-native-lib ANDROID_ARCH=x86_64
-#make android-native-lib ANDROID_ARCH=mips
-#make android-native-lib ANDROID_ARCH=mips64
 
