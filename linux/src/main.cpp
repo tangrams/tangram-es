baba7d03c0bc1b6d3c6de9f5560b5841c49ff7a5
Matt Blair <blair1618@gmail.com>
2016-08-02 10:40:35 -0400

    Desktop demo applications use starting position from scene file

--- linux/src/main.cpp
+++ linux/src/main.cpp
@@ -263,7 +263,7 @@ void init_main_window(bool recreate) {
     // Setup tangram
     if (!map) {
         map = new Tangram::Map();
-        map->loadSceneAsync(sceneFile.c_str());
+        map->loadSceneAsync(sceneFile.c_str(), true);
     }
 
     if (!recreate) {
