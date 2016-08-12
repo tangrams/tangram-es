725f730d1590309e0af119a03e774cdf47a56399
Hannes Janetzek <hannes.janetzek@gmail.com>
2016-08-12 12:37:46 +0200

    use single instance for duplicate JS scene functions

--- core/src/scene/scene.cpp
+++ core/src/scene/scene.cpp
@@ -105,6 +105,14 @@ int Scene::getIdForName(const std::string& _name) const {
     return it - m_names.begin();
 }
 
+int Scene::addJsFunction(const std::string& _function) {
+    for (size_t i = 0; i < m_jsFunctions.size(); i++) {
+        if (m_jsFunctions[i] == _function) { return i; }
+    }
+    m_jsFunctions.push_back(_function);
+    return m_jsFunctions.size()-1;
+}
+
 const Light* Scene::findLight(const std::string &_name) const {
     for (auto& light : m_lights) {
         if (light->getInstanceName() == _name) { return light.get(); }
