dfadfd8aed1da8a9e28f455e0ab42884330a7fd1
Hannes Janetzek <hannes.janetzek@gmail.com>
2016-08-18 15:13:15 +0200

    cleanup platform_tizen.cpp

--- tizen/src/platform_tizen.cpp
+++ tizen/src/platform_tizen.cpp
@@ -8,12 +8,9 @@
 #include <algorithm>
 #include <vector>
 
-//#include "urlWorker.h"
 #include "platform_tizen.h"
 #include "platform_gl.h"
-
-#include <app_common.h>
-#include <dlog.h>
+#include "urlWorker.h"
 
 #include <libgen.h>
 #include <unistd.h>
@@ -21,7 +18,7 @@
 #include <sys/syscall.h>
 
 #include <fontconfig.h>
-
+#include <dlog.h>
 
 #define NUM_WORKERS 4
 
@@ -30,118 +27,6 @@ static std::function<void()> s_renderCallbackFunction = nullptr;
 
 static std::vector<std::string> s_fallbackFonts;
 static FcConfig* s_fcConfig = nullptr;
-
-#if USE_ECORE_CON
-#include <Eina.h>
-#include <Ecore.h>
-#include <Ecore_Con.h>
-
-struct UrlTask {
-    Ecore_Con_Url *url_con;
-    Eina_Binbuf *data;
-    UrlCallback cb;
-    bool inUse;
-
-    UrlTask (const std::string _url, UrlCallback _cb) : cb(_cb) {
-        data = eina_binbuf_new();
-        url_con = ecore_con_url_new(_url.c_str());
-
-        ecore_con_url_get(url_con);
-    }
-
-    ~UrlTask() {
-        eina_binbuf_free(data);
-        ecore_con_url_free(url_con);
-    }
-};
-
-static std::list<std::unique_ptr<UrlTask>> s_urlTaskQueue;
-
-static Eina_Bool data_callback(void *data, int type, void *event) {
-
-
-    auto *url_data = (Ecore_Con_Event_Url_Data*)event;
-
-    auto task = std::find_if(s_urlTaskQueue.begin(), s_urlTaskQueue.end(),
-                           [&](auto& t) { return t->url_con == url_data->url_con; });
-    if (task == s_urlTaskQueue.end()) { return true; }
-
-    if (url_data->size > 0) {
-        eina_binbuf_append_length((*task)->data, url_data->data, url_data->size);
-        //fprintf(stderr, "Appended %d \n", url_data->size);
-    }
-    return true;
-}
-
-static Eina_Bool completion_callback(void *data, int type, void *event) {
-
-
-    auto* url_complete = (Ecore_Con_Event_Url_Complete*)event;
-
-    auto it = std::find_if(s_urlTaskQueue.begin(), s_urlTaskQueue.end(),
-                           [&](auto& t) { return t->url_con == url_complete->url_con; });
-    if (it == s_urlTaskQueue.end()) { return true; }
-    auto task = std::move(*it);
-    s_urlTaskQueue.erase(it);
-
-    if (url_complete->status == 200) {
-        const unsigned char *buffer = eina_binbuf_string_get(task->data);
-        size_t size = eina_binbuf_length_get(task->data);
-        // fprintf(stderr, "Size of data = %d bytes\n", int(size));
-
-        std::vector<char> content(buffer, buffer + size);
-        task->cb(std::move(content));
-    } else{
-        task->cb({});
-    }
-
-    return true;
-}
-
-
-bool startUrlRequest(const std::string& _url, UrlCallback _callback) {
-    s_urlTaskQueue.push_back(std::make_unique<UrlTask>(_url, _callback));
-    return true;
-
-}
-
-void cancelUrlRequest(const std::string& _url) {
-
-    // Only clear this request if a worker has not started operating on it!!
-    // otherwise it gets too convoluted with curl!
-    // auto itr = s_urlTaskQueue.begin();
-    // while(itr != s_urlTaskQueue.end()) {
-    //     if((*itr)->url == _url) {
-    //         itr = s_urlTaskQueue.erase(itr);
-    //     } else {
-    //         itr++;
-    //     }
-    // }
-}
-
-static Ecore_Event_Handler *_url_complete_handler = nullptr;
-static Ecore_Event_Handler *_url_progress_handler = nullptr;
-
-void initUrlRequests() {
-    ecore_con_init();
-    _url_complete_handler = ecore_event_handler_add(ECORE_CON_EVENT_URL_COMPLETE,
-                                                    completion_callback, nullptr);
-
-    _url_progress_handler = ecore_event_handler_add(ECORE_CON_EVENT_URL_DATA,
-                                                    data_callback, nullptr);
-}
-
-void finishUrlRequests() {
-    ecore_event_handler_del(_url_complete_handler);
-    ecore_event_handler_del(_url_progress_handler);
-    ecore_con_shutdown();
-}
-#else
-
-#include "urlWorker.h"
-
-//static std::unique_ptr<UrlWorker> s_workers = nullptr;
-
 
 static UrlWorker s_workers;
 
@@ -155,17 +40,11 @@ void cancelUrlRequest(const std::string& _url) {
 
 void initUrlRequests() {
     s_workers.start(4);
-
-    //s_workers.reset(workers);
 }
 
 void finishUrlRequests() {
     s_workers.stop();
 }
-
-
-#endif
-
 
 static bool s_update = false;
 #ifdef  LOG_TAG
