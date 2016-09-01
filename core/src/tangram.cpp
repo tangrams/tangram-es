1f76858227bdf357b718d5463c3ec394ba9e58df
Karim Naaji <karim.naaji@gmail.com>
2016-09-01 12:21:53 -0400

    Add selection buffer pass and draw to render buffer

--- core/src/tangram.cpp
+++ core/src/tangram.cpp
@@ -364,10 +364,33 @@ void Map::render() {
     // Run render-thread tasks
     impl->renderState.jobQueue.runJobs();
 
-    // Set up openGL for new frame
-    impl->renderState.depthMask(GL_TRUE);
     auto& color = impl->scene->background();
     impl->renderState.clearColor(color.r / 255.f, color.g / 255.f, color.b / 255.f, color.a / 255.f);
+
+    // Feature selection pass
+    if (impl->featureSelection->pendingRequests()) {
+
+        impl->featureSelection->beginRenderPass(impl->renderState);
+
+        {
+            std::lock_guard<std::mutex> lock(impl->tilesMutex);
+
+            for (const auto& style : impl->scene->styles()) {
+
+                style->onBeginDrawSelectionFrame(impl->renderState, impl->view, *(impl->scene));
+
+                for (const auto& tile : impl->tileManager.getVisibleTiles()) {
+                    style->drawSelectionFrame(impl->renderState, *tile);
+                }
+            }
+        }
+
+        impl->featureSelection->endRenderPass(impl->renderState);
+
+    }
+
+    // Set up openGL for new frame
+    impl->renderState.depthMask(GL_TRUE);
     GL_CHECK(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
 
     for (const auto& style : impl->scene->styles()) {
