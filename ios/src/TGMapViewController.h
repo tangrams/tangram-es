a1e64d500db71b859cc9c1f0c95f2c7bea5dd9b0
Matt Smollinger <m.smollinger@gmail.com>
2016-09-02 17:33:52 -0400

    Expose Camera APIs in iOS SDK (#945)
    * Implement camera handling APIs in objective-c
    
    Includes tilt, rotation, zoom, and position management, as well as camera type switching. Fixes #849, #850, #851, #858, #897
    
    * Updated for consistency with android. Rebased against master
    
    * Re-set continuous rendering to be explicitly false.
    
    * Update ease type defaults, add animation functions for camera tilt.
    
    * Rename cameraTilt -> tilt
    
    * Tweak camera API names for consistency

--- ios/src/TGMapViewController.h
+++ ios/src/TGMapViewController.h
@@ -12,6 +12,26 @@
 #import <UIKit/UIKit.h>
 #import <GLKit/GLKit.h>
 
+typedef NS_ENUM(NSInteger, TangramCameraType) {
+  TangramCameraTypePerspective = 0,
+  TangramCameraTypeIsometric,
+  TangramCameraTypeFlat
+};
+
+typedef NS_ENUM(NSInteger, TangramEaseType){
+    TangramEaseTypeLinear = 0,
+    TangramEaseTypeCubic,
+    TangramEaseTypeQuint,
+    TangramEaseTypeSine
+};
+
+typedef struct {
+    double longitude;
+    double latitude;
+} TangramGeoPoint;
+
+NS_ASSUME_NONNULL_BEGIN
+
 @protocol TangramGestureRecognizerDelegate <NSObject>
 @optional
 - (void)recognizer:(UIGestureRecognizer *)recognizer didRecognizeSingleTap:(CGPoint)location;
@@ -22,13 +42,38 @@
 - (void)recognizer:(UIGestureRecognizer *)recognizer didRecognizeShoveGesture:(CGPoint)location;
 @end
 
+NS_ASSUME_NONNULL_END
+
 struct TileID;
 @interface TGMapViewController : GLKViewController <UIGestureRecognizerDelegate>
 
-@property (assign, nonatomic) Tangram::Map* map;
+@property (assign, nonatomic, nullable) Tangram::Map* map;
 @property (assign, nonatomic) BOOL continuous;
-@property (weak, nonatomic) id<TangramGestureRecognizerDelegate> gestureDelegate;
+@property (weak, nonatomic, nullable) id<TangramGestureRecognizerDelegate> gestureDelegate;
+
+// The following are computed property. They return sensible defaults when the above .map property is nil
+@property (assign, nonatomic) TangramCameraType cameraType;
+@property (assign, nonatomic) TangramGeoPoint position;
+@property (assign, nonatomic) float zoom;
+@property (assign, nonatomic) float rotation;
+@property (assign, nonatomic) float tilt;
 
 - (void)renderOnce;
+
+- (void)animateToPosition:(TangramGeoPoint)position withDuration:(float)seconds;
+
+- (void)animateToPosition:(TangramGeoPoint)position withDuration:(float)seconds withEaseType:(TangramEaseType)easeType;
+
+- (void)animateToZoomLevel:(float)zoomLevel withDuration:(float)seconds;
+
+- (void)animateToZoomLevel:(float)zoomLevel withDuration:(float)seconds withEaseType:(TangramEaseType)easeType;
+
+- (void)animateToRotation:(float)radians withDuration:(float)seconds;
+
+- (void)animateToRotation:(float)radians withDuration:(float)seconds withEaseType:(TangramEaseType)easeType;
+
+- (void)animateToTilt:(float)radians withDuration:(float)seconds;
+
+- (void)animateToTilt:(float)radians withDuration:(float)seconds withEaseType:(TangramEaseType)easeType;
 
 @end
