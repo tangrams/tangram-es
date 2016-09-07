43f52a15cd6f3bce3a49cca7594938d0d1c2bcdb
Matt Blair <blair1618@gmail.com>
2016-09-07 13:31:08 -0400

    Add step to download Android CMake package on Travis

--- scripts/travis/before_install.sh
+++ scripts/travis/before_install.sh
@@ -5,7 +5,11 @@ set -o pipefail
 
 if [[ ${PLATFORM} == "android" ]]; then
 
-    # Install android ndk
+    # Note: the right way to download these packages is through the Android Studio SDK manager,
+    # these steps should be removed when/if ndk-bundle and cmake become available from the
+    # command-line SDK update tool.
+
+    # Download android ndk
     echo "Downloading ndk..."
     curl -L https://dl.google.com/android/repository/android-ndk-r13b-linux-x86_64.zip -o ndk.zip
     echo "Done."
@@ -20,6 +24,17 @@ if [[ ${PLATFORM} == "android" ]]; then
     export ANDROID_NDK_HOME=${PWD}/android-ndk-r13b
     export PATH=${PATH}:${ANDROID_HOME}/tools:${ANDROID_NDK_HOME}
     echo $PATH
+    echo "Done."
+
+    # Download android cmake package
+    echo "Downloading android cmake package..."
+    curl -L https://dl.google.com/android/repository/cmake-3.6.3155560-linux-x86_64.zip -o cmake.zip
+    echo "Done."
+
+    # Extract android cmake package
+    echo "Extracting android cmake package..."
+    mkdir -p ${ANDROID_HOME}/cmake
+    unzip -qq cmake.zip -d ${ANDROID_HOME}/cmake
     echo "Done."
 
 fi
