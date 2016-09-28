2d4f205edc257aa3154c99f08455c14d85684073
Yonggang Luo <luoyonggang@gmail.com>
2016-09-29 07:19:15 +0800

    Add new files for Windows. Use scene position by default.

---
+++ core/api/gl_choose.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#if defined(PLATFORM_IOS) || defined(PLATFORM_WINDOWS)
+#undef glDeleteVertexArrays
+#undef glGenVertexArrays
+#undef glBindVertexArray
+#define glDeleteVertexArrays glDeleteVertexArraysOES
+#define glGenVertexArrays glGenVertexArraysOES
+#define glBindVertexArray glBindVertexArrayOES
+#endif
+
+#ifdef PLATFORM_LINUX
+#undef glClearDepthf
+#undef glDepthRangef
+#define glClearDepthf glClearDepth
+#define glDepthRangef glDepthRange
+#endif
+
+#if defined(PLATFORM_ANDROID) || defined(PLATFORM_IOS) || defined(PLATFORM_RPI) || defined(PLATFORM_WINDOWS)
+#undef glMapBuffer
+#undef glUnmapBuffer
+#define glMapBuffer glMapBufferOES
+#define glUnmapBuffer glUnmapBufferOES
+#endif
