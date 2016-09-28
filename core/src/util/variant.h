3ce53324294971e7115d4d5c953473f5f1f0c28a
Yonggang Luo <luoyonggang@gmail.com>
2016-09-29 07:21:56 +0800

    Fixes for msvc for Value. in properties.h & variant.h

--- core/src/util/variant.h
+++ core/src/util/variant.h
@@ -38,11 +38,7 @@ namespace detail {
 /* Common Value type for Feature Properties and Filter Values */
 using Value = variant<none_type, double, std::string>;
 }
-
-class Value : public detail::Value {
-    using Base = detail::Value;
-    using Base::Base;
-};
+using Value = detail::Value;
 
 const static Value NOT_A_VALUE(none_type{});
 
