19438e799a789cdad8ecaa6694e1abd578d033f0
Karim Naaji <karim.naaji@gmail.com>
2016-10-11 12:00:46 -0400

    Add ios-framework target to Makefile

--- toolchains/iOS.framework.cmake
+++ toolchains/iOS.framework.cmake
@@ -19,13 +19,10 @@ set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}
     -fobjc-arc
     -isysroot ${CMAKE_IOS_SDK_ROOT}")
 
-if(${IOS_PLATFORM} STREQUAL "SIMULATOR")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mios-simulator-version-min=6.0")
-    set(ARCH "i386")
-else()
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
-    set(ARCH "armv7 armv7s arm64")
-endif()
+set(ARCH "armv7 armv7s arm64")
+
+#set(ARCH_SIMULATOR)
+#set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mios-simulator-version-min=6.0")
 
 set(FRAMEWORKS CoreGraphics CoreFoundation QuartzCore UIKit OpenGLES Security CFNetwork GLKit)
 
@@ -53,9 +50,6 @@ add_bundle_resources(RESOURCES "${PROJECT_SOURCE_DIR}/ios/framework/Modules" "./
 add_library(${FRAMEWORK_NAME} SHARED ${SOURCES} ${HEADERS} ${RESOURCES})
 target_link_libraries(${FRAMEWORK_NAME} ${CORE_LIBRARY})
 
-# TODO
-# file(GLOB_RECURSE TANGRAM_BUNDLED_FONTS ${PROJECT_SOURCE_DIR}/scenes/fonts/**)
-# message(STATUS "Bundled fonts " ${TANGRAM_BUNDLED_FONTS})
 set(IOS_FRAMEWORK_RESOURCES ${PROJECT_SOURCE_DIR}/ios/framework/Info.plist)
 
 set_target_properties(${FRAMEWORK_NAME} PROPERTIES
@@ -67,10 +61,15 @@ set_target_properties(${FRAMEWORK_NAME} PROPERTIES
     VERSION 1.0.0
     SOVERSION 1.0.0
     PUBLIC_HEADER "${FRAMEWORK_HEADERS}"
-    XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY "iPhone Developer"
-    XCODE_ATTRIBUTE_ENABLE_BITCODE "YES"
     RESOURCE "${IOS_FRAMEWORK_RESOURCES}"
     )
+
+set_xcode_property(${FRAMEWORK_NAME} CODE_SIGN_IDENTITY "iPhone Developer")
+set_xcode_property(${FRAMEWORK_NAME} ENABLE_BITCODE "YES")
+set_xcode_property(${FRAMEWORK_NAME} SUPPORTED_PLATFORMS "iphonesimulator iphoneos")
+set_xcode_property(${FRAMEWORK_NAME} ONLY_ACTIVE_ARCH "NO")
+set_xcode_property(${FRAMEWORK_NAME} VALID_ARCHS "${ARCH}")
+set_xcode_property(${FRAMEWORK_NAME} ARCHS "${ARCH}")
 
 macro(add_framework FWNAME APPNAME LIBPATH)
     find_library(FRAMEWORK_${FWNAME} NAMES ${FWNAME} PATHS ${LIBPATH} PATH_SUFFIXES Frameworks NO_DEFAULT_PATH)
