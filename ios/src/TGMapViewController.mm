f8276f910ac555c150ecfbc080dd5eba844e8afa
Karim Naaji <karim.naaji@gmail.com>
2016-10-26 16:28:14 -0400

    Always use logical pixels from public interface

--- ios/src/TGMapViewController.mm
+++ ios/src/TGMapViewController.mm
@@ -100,6 +100,10 @@
     screenPosition.x *= self.pixelScale;
     screenPosition.y *= self.pixelScale;
 
+    const auto& items = self.map->pickFeaturesAt(screenPosition.x, screenPosition.y);
+
+    if (items.size() == 0) { return; }
+
     self.map->pickFeaturesAt(screenPosition.x, screenPosition.y, [self](const auto& items) {
         NSMutableDictionary* dictionary = [[NSMutableDictionary alloc] init];
         CGPoint position = CGPointMake(0.0, 0.0);
