baed9ba7a1ac2d1decb114d43810de495bf3cac6
Matt Blair <blair1618@gmail.com>
2016-11-06 01:23:45 -0400

    Use Url class in Importer, clean up related code

--- core/src/scene/importer.h
+++ core/src/scene/importer.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <map>
 #include <memory>
 #include <string>
 #include <vector>
@@ -10,11 +9,8 @@
 #include <atomic>
 #include <condition_variable>
 
-#include "util/fastmap.h"
-
-namespace YAML {
-    class Node;
-}
+#include "util/url.h"
+#include "yaml-cpp/yaml.h"
 
 namespace Tangram {
 
@@ -24,39 +20,33 @@ public:
 
     using Node = YAML::Node;
 
-    // Loads the main scene with deep merging dependentent imported scenes.
-    Node applySceneImports(const std::string& scenePath, const std::string& resourceRoot = "");
+    // Loads the main scene with deep merging dependent imported scenes.
+    Node applySceneImports(const Url& scenePath, const Url& resourceRoot = Url());
 
 // protected for testing purposes, else could be private
 protected:
-    virtual std::string getSceneString(const std::string& scenePath);
-    void processScene(const std::string& scenePath, const std::string& sceneString);
+    virtual std::string getSceneString(const Url& scenePath);
+    void processScene(const Url& scenePath, const std::string& sceneString);
 
     // Get the sequence of scene names that are designated to be imported into the
     // input scene node by its 'import' fields.
-    std::vector<std::string> getScenesToImport(const Node& scene);
+    std::vector<Url> getScenesToImport(const Node& scene);
 
     // loads all the imported scenes and the master scene and returns a unified YAML root node.
-    void importScenes(Node& root, const std::string& sceneName, std::vector<std::string>& sceneStack);
+    void importScenes(Node& root, const Url& scenePath, std::vector<Url>& sceneStack);
 
-    //void mergeMapFieldsTaskingLast(const std::string& key, Node target, const std::vector<Node>& imports);
     void mergeMapFields(Node& target, const Node& import);
 
-    void normalizeSceneTextures(Node& root, const std::string& parentPath);
-    void setNormalizedTexture(Node& texture, const std::vector<std::string>& names,
-            const std::string& parentPath);
-    void normalizeSceneImports(Node& root, const std::string& parentPath);
-    void normalizeSceneDataSources(Node& root, const std::string& parentPath);
-    void normalizeFonts(Node& root, const std::string& parentPath);
-    std::string normalizePath(const std::string& path, const std::string& parentPath);
+    void setResolvedTextureUrl(Node& textureNode, const Url& base);
+
+    void resolveSceneUrls(Node& root, const Url& base);
 
 private:
     // import scene to respective root nodes
-    std::map<std::string, Node> m_scenes;
+    std::unordered_map<Url, Node> m_scenes;
     std::unordered_set<std::string> m_globalTextures;
-    std::unordered_map<std::string, std::string> m_textureNames;
 
-    std::vector<std::string> m_sceneQueue;
+    std::vector<Url> m_sceneQueue;
     static std::atomic_uint progressCounter;
     std::mutex sceneMutex;
     std::condition_variable m_condition;
