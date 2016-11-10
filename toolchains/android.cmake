b86b23feebd06a79bcf02e3ea4aef4e519f4753f
Matt Blair <blair1618@gmail.com>
2016-11-10 13:01:21 -0500

    Move Android sources into standard locations

--- toolchains/android.cmake
+++ toolchains/android.cmake
@@ -12,8 +12,8 @@ set(LIB_NAME tangram) # in order to have libtangram.so
 
 add_library(${LIB_NAME} SHARED
   ${CMAKE_SOURCE_DIR}/core/common/platform_gl.cpp
-  ${CMAKE_SOURCE_DIR}/android/tangram/jni/jniExports.cpp
-  ${CMAKE_SOURCE_DIR}/android/tangram/jni/platform_android.cpp)
+  ${CMAKE_SOURCE_DIR}/android/tangram/src/main/cpp/jniExports.cpp
+  ${CMAKE_SOURCE_DIR}/android/tangram/src/main/cpp/platform_android.cpp)
 
 target_link_libraries(${LIB_NAME}
   PUBLIC
