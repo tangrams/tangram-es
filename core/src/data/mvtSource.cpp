1ac136e6ba86f2711e80a6cedbf8610c27dc720f
Hannes Janetzek <hannes.janetzek@gmail.com>
2016-11-28 14:09:49 +0100

    MVTSource: catch exceptions from protobuf parser (#1105)

--- core/src/data/mvtSource.cpp
+++ core/src/data/mvtSource.cpp
@@ -5,7 +5,7 @@
 #include "tile/tile.h"
 #include "tile/tileTask.h"
 #include "util/pbfParser.h"
-#include "platform.h"
+#include "log.h"
 
 namespace Tangram {
 
@@ -24,12 +24,22 @@ std::shared_ptr<TileData> MVTSource::parse(const TileTask& _task, const MapProje
     protobuf::message item(task.rawTileData->data(), task.rawTileData->size());
     PbfParser::ParserContext ctx(m_id);
 
-    while(item.next()) {
-        if(item.tag == 3) {
-            tileData->layers.push_back(PbfParser::getLayer(ctx, item.getMessage()));
-        } else {
-            item.skip();
+    try {
+        while(item.next()) {
+            if(item.tag == 3) {
+                tileData->layers.push_back(PbfParser::getLayer(ctx, item.getMessage()));
+            } else {
+                item.skip();
+            }
         }
+    } catch(const std::invalid_argument& e) {
+        LOGE("Cannot parse tile %s: %s", _task.tileId().toString().c_str(), e.what());
+        return {};
+    } catch(const std::runtime_error& e) {
+        LOGE("Cannot parse tile %s: %s", _task.tileId().toString().c_str(), e.what());
+        return {};
+    } catch(...) {
+        return {};
     }
     return tileData;
 }
