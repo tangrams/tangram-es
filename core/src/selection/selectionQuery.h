1d594b65e79aca0cbc2881b035c2a10c88aaa753
Karim Naaji <karim.naaji@gmail.com>
2016-12-09 16:27:24 -0500

    Use color cache to reduce framebuffer reads - Several queries of the same frame can have the same position for different kind of query type, store the color that have been read from the framebuffer for the current frame and clear it on next

--- core/src/selection/selectionQuery.h
+++ core/src/selection/selectionQuery.h
@@ -3,6 +3,7 @@
 #include "util/variant.h"
 #include "glm/vec2.hpp"
 #include "tangram.h"
+#include <map>
 
 namespace Tangram {
 
@@ -25,6 +26,8 @@ class SelectionQuery {
 public:
     SelectionQuery(glm::vec2 _position, QueryCallback _queryCallback, QueryType _type);
 
+    static void clearColorCache() { s_colorCache.clear(); }
+
     void process(const View& _view, const FrameBuffer& _framebuffer, const MarkerManager& _markerManager, const TileManager& _tileManager, const Labels& _labels) const;
 
     QueryType type() const { return m_type; }
@@ -33,6 +36,8 @@ private:
     glm::vec2 m_position;
     QueryCallback m_queryCallback;
     QueryType m_type;
+
+    static std::map<size_t, uint32_t> s_colorCache;
 
 };
 
