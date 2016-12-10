755e2b1817a7e5a126690d6253fdcf452f558dd8
Hannes Janetzek <hannes.janetzek@gmail.com>
2016-12-10 01:16:53 +0100

    show linker options

--- scripts/travis/script_build.sh
+++ scripts/travis/script_build.sh
@@ -12,7 +12,7 @@ fi
 if [[ ${PLATFORM} == "linux" ]]; then
     # Build linux project
     echo "Building linux project"
-    DEBUG=1 CMAKE_OPTIONS=" -DUNIT_TESTS=1 -DBENCHMARK=1" make -j 4 linux
+    VERBOSE=1 DEBUG=1 CMAKE_OPTIONS=" -DUNIT_TESTS=1 -DBENCHMARK=1" make -j 4 linux
 fi
 
 if [[ ${PLATFORM} == "ios" ]]; then
