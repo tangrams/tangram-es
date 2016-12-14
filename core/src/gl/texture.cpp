4a08d8f4aa1d8d6dca234b1be668930ebfb2ff31
Hannes Janetzek <hannes.janetzek@gmail.com>
2016-12-14 15:29:30 +0100

    disable unsupported image formats
    we should only handle those that also JS supports.
    this should also reduce our library size.
    
    https://en.wikipedia.org/wiki/Comparison_of_web_browsers#Image_format_support

--- core/src/gl/texture.cpp
+++ core/src/gl/texture.cpp
@@ -8,6 +8,12 @@
 #include "tangram.h"
 #include "log.h"
 
+// Enable only JPEG, PNG, GIF, TGA and PSD
+#define STBI_NO_BMP
+#define STBI_NO_HDR
+#define STBI_NO_PIC
+#define STBI_NO_PNM
+
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
