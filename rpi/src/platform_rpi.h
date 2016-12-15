07dd2deff644370a2b992e664683998ac3af1cae
Patricio Gonzalez Vivo <patriciogonzalezvivo@users.noreply.github.com>
2016-12-15 09:53:28 -0800

    fixng RPI compilation (#1169)

--- rpi/src/platform_rpi.h
+++ rpi/src/platform_rpi.h
@@ -1,11 +1,5 @@
 #pragma once
 
-//  Broadcom hardware library for hijacking the GPU card without window manager
-//
-#include "bcm_host.h"
-
-#include <GLES2/gl2.h>
-#include <EGL/egl.h>
-#include <EGL/eglext.h>
+#include "platform.h"
 
 void processNetworkQueue();
