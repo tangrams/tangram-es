bdbad7b79e7489ac9d3fbebda476823b954e2596
Karim Naaji <karim.naaji@gmail.com>
2017-01-17 18:09:38 -0500

    Fix stops for parameters placement_spacing and placement_min_length_ratio

--- core/src/style/pointStyleBuilder.cpp
+++ core/src/style/pointStyleBuilder.cpp
@@ -128,10 +128,18 @@ auto PointStyleBuilder::applyRule(const DrawRule& _rule, const Properties& _prop
     _rule.get(StyleParamKey::sprite_default, p.spriteDefault);
     _rule.get(StyleParamKey::placement, p.placement);
     StyleParam::Width placementSpacing;
-    if (_rule.get(StyleParamKey::placement_spacing, placementSpacing)) {
+    auto placementSpacingParam = _rule.findParameter(StyleParamKey::placement_spacing);
+    if (placementSpacingParam.stops) {
+        p.placementSpacing = placementSpacingParam.stops->evalFloat(m_styleZoom);
+    } else if (_rule.get(StyleParamKey::placement_spacing, placementSpacing)) {
         p.placementSpacing = placementSpacing.value;
     }
-    _rule.get(StyleParamKey::placement_min_length_ratio, p.placementMinLengthRatio);
+    auto placementMinLengthParam = _rule.findParameter(StyleParamKey::placement_min_length_ratio);
+    if (placementMinLengthParam.stops) {
+        p.placementMinLengthRatio = placementMinLengthParam.stops->evalFloat(m_styleZoom);
+    } else {
+        _rule.get(StyleParamKey::placement_min_length_ratio, p.placementMinLengthRatio);
+    }
     _rule.get(StyleParamKey::tile_edges, p.keepTileEdges);
     _rule.get(StyleParamKey::interactive, p.interactive);
     _rule.get(StyleParamKey::collide, p.labelOptions.collide);
