e7aa1bb9514836ec8b85723cf4bc3d4370bb5d9d
Karim Naaji <karim.naaji@gmail.com>
2017-02-01 18:25:15 -0500

    Update RPi platform

--- rpi/src/platform_rpi.cpp
+++ rpi/src/platform_rpi.cpp
@@ -23,11 +23,6 @@
 #define FONT_JA "fonts/DroidSansJapanese.ttf"
 #define FALLBACK "fonts/DroidSansFallback.ttf"
 
-static bool s_isContinuousRendering = false;
-
-static UrlWorker s_Workers[NUM_WORKERS];
-static std::list<std::unique_ptr<UrlTask>> s_urlTaskQueue;
-
 void logMsg(const char* fmt, ...) {
     va_list args;
     va_start(args, fmt);
@@ -35,131 +30,69 @@ void logMsg(const char* fmt, ...) {
     va_end(args);
 }
 
-void processNetworkQueue() {
+void RPiPlatform::processNetworkQueue() {
     // attach workers to NetWorkerData
-    auto taskItr = s_urlTaskQueue.begin();
-    for(auto& worker : s_Workers) {
-        if(taskItr == s_urlTaskQueue.end()) {
+    auto taskItr = m_urlTaskQueue.begin();
+    for(auto& worker : m_workers) {
+        if(taskItr == m_urlTaskQueue.end()) {
             break;
         }
         if(worker.isAvailable()) {
-            worker.perform(std::move(*taskItr));
-            taskItr = s_urlTaskQueue.erase(taskItr);
+            worker.perform(std::move(*taskItr), static_cast<const Platform&>(*this));
+            taskItr = m_urlTaskQueue.erase(taskItr);
         }
     }
 }
 
-void requestRender() {
-    setRenderRequest(true);
+void RPiPlatform::requestRender() const {
+    glfwPostEmptyEvent();
 }
 
-void setContinuousRendering(bool _isContinuous) {
-    s_isContinuousRendering = _isContinuous;
-}
-
-bool isContinuousRendering() {
-    return s_isContinuousRendering;
-}
-
-std::string stringFromFile(const char* _path) {
-
-    std::string out;
-    if (!_path || strlen(_path) == 0) { return out; }
-
-    std::ifstream resource(_path, std::ifstream::ate | std::ifstream::binary);
-
-    if(!resource.is_open()) {
-        logMsg("Failed to read file at path: %s\n", _path);
-        return out;
-    }
-
-    resource.seekg(0, std::ios::end);
-    out.resize(resource.tellg());
-    resource.seekg(0, std::ios::beg);
-    resource.read(&out[0], out.size());
-    resource.close();
-
-    return out;
-}
-
-std::vector<char> bytesFromFile(const char* _path) {
-    if (!_path || strlen(_path) == 0) { return {}; }
-
-    std::ifstream resource(_path, std::ifstream::ate | std::ifstream::binary);
-
-    if(!resource.is_open()) {
-        LOG("failed to read file at path: %s", _path);
-        return {};
-    }
-
-    std::vector<char> data;
-    size_t size = resource.tellg();
-    data.resize(size);
-
-    resource.seekg(std::ifstream::beg);
-
-    resource.read(data.data(), size);
-    resource.close();
-
-    return data;
-}
-
-FontSourceHandle getFontHandle(const char* _path) {
-    FontSourceHandle fontSourceHandle = [_path](size_t* _size) -> unsigned char* {
-        LOG("Loading font %s", _path);
-
-        auto cdata = bytesFromFile(_path, *_size);
-
-        return cdata;
-    };
-
-    return fontSourceHandle;
-}
-
-std::vector<FontSourceHandle> systemFontFallbacksHandle() {
+std::vector<FontSourceHandle> RPiPlatform::systemFontFallbacksHandle() const {
     std::vector<FontSourceHandle> handles;
 
-    handles.push_back(getFontHandle(DEFAULT));
-    handles.push_back(getFontHandle(FONT_AR));
-    handles.push_back(getFontHandle(FONT_HE));
-    handles.push_back(getFontHandle(FONT_JA));
-    handles.push_back(getFontHandle(FALLBACK));
+    handles.emplace_back(DEFAULT);
+    handles.emplace_back(FONT_AR);
+    handles.emplace_back(FONT_HE);
+    handles.emplace_back(FONT_JA);
+    handles.emplace_back(FALLBACK);
 
     return handles;
 }
 
-// System fonts are not available on Raspberry Pi yet, we will possibly use FontConfig in the future,
-// for references see the tizen platform implementation of system fonts
-std::vector<char> systemFont(const std::string& _name, const std::string& _weight, const std::string& _face) {
-    return {};
-}
-
-bool startUrlRequest(const std::string& _url, UrlCallback _callback) {
+bool RPiPlatform::startUrlRequest(const std::string& _url, UrlCallback _callback) {
 
     std::unique_ptr<UrlTask> task(new UrlTask(_url, _callback));
-    for(auto& worker : s_Workers) {
-        if(worker.isAvailable()) {
-            worker.perform(std::move(task));
+    for (auto& worker : m_workers) {
+        if (worker.isAvailable()) {
+            worker.perform(std::move(task), static_cast<const Platform&>(*this));
             return true;
         }
     }
-    s_urlTaskQueue.push_back(std::move(task));
+    m_urlTaskQueue.push_back(std::move(task));
     return true;
 
 }
 
-void cancelUrlRequest(const std::string& _url) {
+void RPiPlatform::cancelUrlRequest(const std::string& _url) {
 
-    // Only clear this request if a worker has not started operating on it!
+    // Only clear this request if a worker has not started operating on it!!
     // otherwise it gets too convoluted with curl!
-    auto itr = s_urlTaskQueue.begin();
-    while(itr != s_urlTaskQueue.end()) {
-        if((*itr)->url == _url) {
-            itr = s_urlTaskQueue.erase(itr);
+    auto itr = m_urlTaskQueue.begin();
+    while (itr != m_urlTaskQueue.end()) {
+        if ((*itr)->url == _url) {
+            itr = m_urlTaskQueue.erase(itr);
         } else {
             itr++;
         }
     }
+}
+
+RPiPlatform::~RPiPlatform() {
+    for(auto& worker : m_workers) {
+        worker.join();
+    }
+    m_urlTaskQueue.clear();
 }
 
 void setCurrentThreadPriority(int priority) {}
--- rpi/src/platform_rpi.h
+++ rpi/src/platform_rpi.h
@@ -3,3 +3,24 @@
 #include "platform.h"
 
 void processNetworkQueue();
+
+#define NUM_WORKERS 3
+
+class RPiPlatform : public Platform {
+
+public:
+
+    ~RPiPlatform() override;
+    void requestRender() const override;
+    std::vector<FontSourceHandle> systemFontFallbacksHandle() const override;
+    bool startUrlRequest(const std::string& _url, UrlCallback _callback) override;
+    void cancelUrlRequest(const std::string& _url) override;
+    void processNetworkQueue();
+
+private:
+
+    mutable UrlWorker m_workers[NUM_WORKERS];
+    mutable std::list<std::unique_ptr<UrlTask>> m_urlTaskQueue;
+
+};
+
