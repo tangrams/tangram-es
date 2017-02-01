0138508d55edabaafa2129c80435d3c993d89042
Karim Naaji <karim.naaji@gmail.com>
2017-02-01 11:14:21 -0500

    Update Linux platform

--- linux/src/platform_linux.cpp
+++ linux/src/platform_linux.cpp
@@ -21,18 +21,11 @@
 
 #include <GLFW/glfw3.h>
 
-#define NUM_WORKERS 3
-
 #define DEFAULT "fonts/NotoSans-Regular.ttf"
 #define FONT_AR "fonts/NotoNaskh-Regular.ttf"
 #define FONT_HE "fonts/NotoSansHebrew-Regular.ttf"
 #define FONT_JA "fonts/DroidSansJapanese.ttf"
 #define FALLBACK "fonts/DroidSansFallback.ttf"
-
-static bool s_isContinuousRendering = false;
-
-static UrlWorker s_Workers[NUM_WORKERS];
-static std::list<std::unique_ptr<UrlTask>> s_urlTaskQueue;
 
 void logMsg(const char* fmt, ...) {
     va_list args;
@@ -41,82 +34,25 @@ void logMsg(const char* fmt, ...) {
     va_end(args);
 }
 
-void processNetworkQueue() {
+void LinuxPlatform::processNetworkQueue() {
     // attach workers to NetWorkerData
-    auto taskItr = s_urlTaskQueue.begin();
-    for(auto& worker : s_Workers) {
-        if(taskItr == s_urlTaskQueue.end()) {
+    auto taskItr = m_urlTaskQueue.begin();
+    for(auto& worker : m_workers) {
+        if(taskItr == m_urlTaskQueue.end()) {
             break;
         }
         if(worker.isAvailable()) {
             worker.perform(std::move(*taskItr));
-            taskItr = s_urlTaskQueue.erase(taskItr);
+            taskItr = m_urlTaskQueue.erase(taskItr);
         }
     }
 }
 
-void requestRender() {
-
+void LinuxPlatform::requestRender() const {
     glfwPostEmptyEvent();
-
 }
 
-void setContinuousRendering(bool _isContinuous) {
-
-    s_isContinuousRendering = _isContinuous;
-
-}
-
-bool isContinuousRendering() {
-
-    return s_isContinuousRendering;
-
-}
-
-std::string stringFromFile(const char* _path) {
-
-    std::string out;
-    if (!_path || strlen(_path) == 0) { return out; }
-
-    std::ifstream resource(_path, std::ifstream::ate | std::ifstream::binary);
-
-    if(!resource.is_open()) {
-        logMsg("Failed to read file at path: %s\n", _path);
-        return out;
-    }
-
-    resource.seekg(0, std::ios::end);
-    out.resize(resource.tellg());
-    resource.seekg(0, std::ios::beg);
-    resource.read(&out[0], out.size());
-    resource.close();
-
-    return out;
-}
-
-std::vector<char> bytesFromFile(const char* _path) {
-    if (!_path || strlen(_path) == 0) { return {}; }
-
-    std::ifstream resource(_path, std::ifstream::ate | std::ifstream::binary);
-
-    if(!resource.is_open()) {
-        LOG("Failed to read file at path: %s", _path);
-        return {};
-    }
-
-    std::vector<char> data;
-    size_t size = resource.tellg();
-    data.resize(size);
-
-    resource.seekg(std::ifstream::beg);
-
-    resource.read(data.data(), size);
-    resource.close();
-
-    return data;
-}
-
-std::vector<FontSourceHandle> systemFontFallbacksHandle() {
+std::vector<FontSourceHandle> LinuxPlatform::systemFontFallbacksHandle() const {
     std::vector<FontSourceHandle> handles;
 
     handles.emplace_back(DEFAULT);
@@ -128,55 +64,44 @@ std::vector<FontSourceHandle> systemFontFallbacksHandle() {
     return handles;
 }
 
-// System fonts are not available on linux yet, we will possibly use FontConfig in the future, for
-// references see the tizen platform implementation of system fonts
-std::vector<char> systemFont(const std::string& _name, const std::string& _weight, const std::string& _face) {
-    return {};
-}
-
-bool startUrlRequest(const std::string& _url, UrlCallback _callback) {
+bool LinuxPlatform::startUrlRequest(const std::string& _url, UrlCallback _callback) const {
 
     std::unique_ptr<UrlTask> task(new UrlTask(_url, _callback));
-    for(auto& worker : s_Workers) {
+    for(auto& worker : m_workers) {
         if(worker.isAvailable()) {
             worker.perform(std::move(task));
             return true;
         }
     }
-    s_urlTaskQueue.push_back(std::move(task));
+    m_urlTaskQueue.push_back(std::move(task));
     return true;
 
 }
 
-void cancelUrlRequest(const std::string& _url) {
+void LinuxPlatform::cancelUrlRequest(const std::string& _url) const {
 
     // Only clear this request if a worker has not started operating on it!!
     // otherwise it gets too convoluted with curl!
-    auto itr = s_urlTaskQueue.begin();
-    while(itr != s_urlTaskQueue.end()) {
+    auto itr = m_urlTaskQueue.begin();
+    while(itr != m_urlTaskQueue.end()) {
         if((*itr)->url == _url) {
-            itr = s_urlTaskQueue.erase(itr);
+            itr = m_urlTaskQueue.erase(itr);
         } else {
             itr++;
         }
     }
 }
 
-void finishUrlRequests() {
-    for(auto& worker : s_Workers) {
+LinuxPlatform::~LinuxPlatform() {
+    for(auto& worker : m_workers) {
         worker.join();
     }
-    s_urlTaskQueue.clear();
+    m_urlTaskQueue.clear();
 }
 
-void setCurrentThreadPriority(int priority){
+void setCurrentThreadPriority(int priority) {
     int tid = syscall(SYS_gettid);
-    //int  p1 = getpriority(PRIO_PROCESS, tid);
-
     setpriority(PRIO_PROCESS, tid, priority);
-
-    //int  p2 = getpriority(PRIO_PROCESS, tid);
-    //logMsg("set niceness: %d -> %d\n", p1, p2);
 }
 
 void initGLExtensions() {
