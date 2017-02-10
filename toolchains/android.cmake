d986f1a27936ad9270e23e15826160e33bba0407
Matt Blair <blair1618@gmail.com>
2017-02-10 13:01:09 -0800

    Update CMake and Gradle scripts for Android

--- toolchains/android.cmake
+++ toolchains/android.cmake
@@ -1,19 +1,16 @@
 add_definitions(-DPLATFORM_ANDROID)
 
-# build external dependencies
-add_subdirectory(${PROJECT_SOURCE_DIR}/external)
-
 # load core library
 add_subdirectory(${PROJECT_SOURCE_DIR}/core)
 
-set(ANDROID_PROJECT_DIR ${PROJECT_SOURCE_DIR}/android/tangram)
+set(ANDROID_PROJECT_DIR ${PROJECT_SOURCE_DIR}/platforms/android/tangram)
 
 set(LIB_NAME tangram) # in order to have libtangram.so
 
 add_library(${LIB_NAME} SHARED
-  ${CMAKE_SOURCE_DIR}/core/common/platform_gl.cpp
-  ${CMAKE_SOURCE_DIR}/android/tangram/src/main/cpp/jniExports.cpp
-  ${CMAKE_SOURCE_DIR}/android/tangram/src/main/cpp/platform_android.cpp)
+  ${CMAKE_SOURCE_DIR}/platforms/common/platform_gl.cpp
+  ${CMAKE_SOURCE_DIR}/platforms/android/tangram/src/main/cpp/jniExports.cpp
+  ${CMAKE_SOURCE_DIR}/platforms/android/tangram/src/main/cpp/platform_android.cpp)
 
 target_link_libraries(${LIB_NAME}
   PUBLIC
