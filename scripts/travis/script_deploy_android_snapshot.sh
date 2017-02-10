9314aa166503d0c3591515831546e4a267a9ac38
Matt Blair <blair1618@gmail.com>
2017-02-10 14:25:58 -0800

    Update Makefile and Travis scripts for Android

--- scripts/travis/script_deploy_android_snapshot.sh
+++ scripts/travis/script_deploy_android_snapshot.sh
@@ -10,7 +10,7 @@ if [ "${PLATFORM}" = "android" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ 
 
     make android-sdk
 
-    cd "$TRAVIS_BUILD_DIR"/android
+    cd "$TRAVIS_BUILD_DIR"/platforms/android
     git clone https://github.com/mapzen/android-config.git
     ./gradlew uploadArchives -PdoFullRelease -PsonatypeUsername="$SONATYPE_USERNAME" -PsonatypePassword="$SONATYPE_PASSWORD" -Psigning.keyId="$SIGNING_KEY_ID" -Psigning.password="$SIGNING_PASSWORD" -Psigning.secretKeyRingFile="$SIGNING_SECRET_KEY_RING_FILE"
     cd "$TRAVIS_BUILD_DIR"
