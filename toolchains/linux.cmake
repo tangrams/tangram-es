6964ebd04ff8271f006678a2b8cc7d2ef17babc6
Hannes Janetzek <hannes.janetzek@gmail.com>
2017-03-01 14:51:00 +0100

    fix ignored const qualifier warnings

--- toolchains/linux.cmake
+++ toolchains/linux.cmake
@@ -2,7 +2,7 @@
 set(PLATFORM_LINUX ON)
 
 # global compile options
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -std=c++1y")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wignored-qualifiers -std=c++1y")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-omit-frame-pointer")
 
 if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
