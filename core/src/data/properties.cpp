7b9b0ddb6041624b004cb434c07599e6e0ae3076
Karim Naaji <karim.naaji@gmail.com>
2017-03-02 14:16:01 -0500

    Remove trailing 0s from double to string conversions (#1336)
    - Fixes #1283

--- core/src/data/properties.cpp
+++ core/src/data/properties.cpp
@@ -4,6 +4,18 @@
 
 namespace Tangram {
 
+// Helper to cleanup double string values from trailing 0s
+std::string doubleToString(double _doubleValue) {
+    std::string value = std::to_string(_doubleValue);
+
+    // Remove trailing '0's
+    value.erase(value.find_last_not_of('0') + 1, std::string::npos);
+    // If last value is '.', clear it too
+    value.erase(value.find_last_not_of('.') + 1, std::string::npos);
+
+    return value;
+}
+
 Properties::Properties() : sourceId(0) {}
 
 Properties::~Properties() {}
@@ -88,7 +100,7 @@ const bool Properties::getAsString(const std::string& key, std::string& value) c
         value = it.get<std::string>();
         return true;
     } else if (it.is<double>()) {
-        value = std::to_string(it.get<double>());
+        value = doubleToString(it.get<double>());
         return true;
     }
 
@@ -99,7 +111,7 @@ std::string Properties::asString(const Value& value) const {
     if (value.is<std::string>()) {
         return value.get<std::string>();
     } else if (value.is<double>()) {
-        return std::to_string(value.get<double>());
+        return doubleToString(value.get<double>());
     }
     return "";
 }
