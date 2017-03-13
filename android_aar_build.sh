08ac15e04559ad23656da743b6ca65d497ad0965
Tim Engler <engler@gmail.com>
2017-03-13 11:24:07 +0800

    Fixed fdroid build script

--- android_aar_build.sh
+++ android_aar_build.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 cd `dirname $0`
 
-CMAKE_OPTIONS="-DCMAKE_INSTALL_PREFIX=build" RELEASE=1 make android-native-lib ANDROID_ARCH=x86 && \
-CMAKE_OPTIONS="-DCMAKE_INSTALL_PREFIX=build" RELEASE=1 make android-native-lib ANDROID_ARCH=armeabi && \
-CMAKE_OPTIONS="-DCMAKE_INSTALL_PREFIX=build" RELEASE=1 make android-native-lib ANDROID_ARCH=arm64-v8a && \
-CMAKE_OPTIONS="-DCMAKE_INSTALL_PREFIX=build" RELEASE=1 make android-native-lib ANDROID_ARCH=armeabi-v7a && \
-RELEASE=1 make android-tangram-apk && \
-echo "Output is `dirname $0`/android/tangram/build/outputs/aar/tangram-release.aar"
+make android-sdk
+echo "Output is `dirname $0`/platforms/android/tangram/build/outputs/aar/tangram-full-release.aar"
 
 
