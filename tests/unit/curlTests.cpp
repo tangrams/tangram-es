9ca7a26e28b3c1e2df40e2212690d3b4b27b1201
Peter Richardson <peter@mapzen.com>
2017-03-15 11:40:53 -0400

    update datasource urls

--- tests/unit/curlTests.cpp
+++ tests/unit/curlTests.cpp
@@ -13,20 +13,20 @@ TEST_CASE( "URL Name Check for MapzenVectorTileJson", "[CURL][DataSource][Mapzen
     TileID tileCoord = TileID(0,0,0);
     std::string url;
     *dataSource.constructURL(tileCoord, url);
-    REQUIRE( url == "http://vector.mapzen.com/osm/all/0/0/0.json");
+    REQUIRE( url == "http://tile.mapzen.com/mapzen/vector/v1/all/0/0/0.json");
     TileID tileCoord2 = TileID(19293,24641,16);
     *dataSource.constructURL(tileCoord2, url);
-    REQUIRE( url == "http://vector.mapzen.com/osm/all/16/19293/24641.json");
+    REQUIRE( url == "http://tile.mapzen.com/mapzen/vector/v1/all/16/19293/24641.json");
     TileID tileCoord3 = TileID(19293,24641,14);
     *dataSource.constructURL(tileCoord3, url);
-    REQUIRE( url == "http://vector.mapzen.com/osm/all/14/19293/24641.json");*/
+    REQUIRE( url == "http://tile.mapzen.com/mapzen/vector/v1/all/14/19293/24641.json");*/
 }
 
 TEST_CASE( "Extract tile coordinates from URL check for MapzenVectorTileJson", "[CURL][DataSource][MapzenVectorTileJson]" ) {
     /*MapzenVectorTileJson dataSource;
-    REQUIRE( dataSource.extractIDFromUrl("http://vector.mapzen.com/osm/all/16/19293/24641.json") == TileID(19293, 24641, 16) );
-    REQUIRE( dataSource.extractIDFromUrl("http://vector.mapzen.com/osm/all/0/0/0.json") == TileID(0,0,0) );
-    REQUIRE( dataSource.extractIDFromUrl("http://vector.mapzen.com/osm/all/14/19293/24641.json") == TileID(19293, 24641, 14) );*/
+    REQUIRE( dataSource.extractIDFromUrl("http://tile.mapzen.com/mapzen/vector/v1/all/16/19293/24641.json") == TileID(19293, 24641, 16) );
+    REQUIRE( dataSource.extractIDFromUrl("http://tile.mapzen.com/mapzen/vector/v1/all/0/0/0.json") == TileID(0,0,0) );
+    REQUIRE( dataSource.extractIDFromUrl("http://tile.mapzen.com/mapzen/vector/v1/all/14/19293/24641.json") == TileID(19293, 24641, 14) );*/
 }
 
 TEST_CASE( "MapzenVectorTileJson::LoadTile check", "[CURL][DataSource][MapzenVectorTileJson]" ) {
