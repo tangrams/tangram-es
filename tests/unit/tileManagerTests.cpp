ef8fce14aa7b32c12725c7ce2001728630e26eee
tallytalwar@gmail.com <tallytalwar@gmail.com>
2017-04-13 11:00:47 -0400

    Fix tileManager test
    - mimeType is no lomger overriden

--- tests/unit/tileManagerTests.cpp
+++ tests/unit/tileManagerTests.cpp
@@ -79,8 +79,6 @@ struct TestTileSource : TileSource {
         bool hasData() const override { return gotData; }
     };
 
-    virtual const char* mimeType() override { return ""; };
-
     int tileTaskCount = 0;
 
     TestTileSource() : TileSource("", nullptr) {
