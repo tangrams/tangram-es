af64602e881b6601ff16be597aef8b0d83ce6ff3
Varun Talwar <tallytalwar@gmail.com>
2017-04-24 09:20:19 -0400

    Update travis yaml and scripts to build patch release

--- scripts/travis/script_deploy_android_demo.sh
+++ scripts/travis/script_deploy_android_demo.sh
@@ -6,7 +6,7 @@ set -o pipefail
 if [ "${PLATFORM}" = "android" ]; then
     printf "[default]\naccess_key = $S3_ACCESS_KEY\n secret_key = $S3_SECRET_KEY" > ~/.s3cfg
 
-    if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
+    if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "0.6.2-patch" ]; then
         url_latest="s3://android.mapzen.com/tangram-latest.apk"
         echo "Uploading latest build to $url_latest"
         s3cmd put platforms/android/demo/build/outputs/apk/demo-debug.apk $url_latest
@@ -14,7 +14,7 @@ if [ "${PLATFORM}" = "android" ]; then
         url_snapshot="s3://android.mapzen.com/tangram-snapshots/master-$TRAVIS_BUILD_NUMBER.apk"
         echo "Uploading snapshot build to $url_snapshot"
         s3cmd put platforms/android/demo/build/outputs/apk/demo-debug.apk $url_snapshot
-    elif ! [ "${TRAVIS_BRANCH}" = "master" ]; then
+    elif ! [ "${TRAVIS_BRANCH}" = "0.6.2-patch" ]; then
         url_dev="s3://android.mapzen.com/tangram-development/$TRAVIS_BRANCH-$TRAVIS_BUILD_NUMBER.apk"
         echo "Uploading development build to $url_dev"
         s3cmd put platforms/android/demo/build/outputs/apk/demo-debug.apk $url_dev
