ea1d288ef6faf3fa94b57aa6fbc0530c30d8a5e5
Varun Talwar <tallytalwar@gmail.com>
2017-04-27 15:50:40 -0400

    We missed adding korean fallback fonts

--- platforms/ios/src/TangramMap/platform_ios.mm
+++ platforms/ios/src/TangramMap/platform_ios.mm
@@ -118,7 +118,7 @@ std::string iOSPlatform::stringFromFile(const char* _path) const {
 bool allowedFamily(NSString* familyName) {
     const NSArray<NSString *> *allowedFamilyList = @[ @"Hebrew", @"Kohinoor", @"Gumurki", @"Thonburi", @"Tamil",
                                                     @"Gurmukhi", @"Kailasa", @"Sangam", @"PingFang", @"Geeza",
-                                                    @"Mishafi", @"Farah", @"Hiragino" ];
+                                                    @"Mishafi", @"Farah", @"Hiragino", @"Gothic" ];
 
     for (NSString* allowedFamily in allowedFamilyList) {
         if ( [familyName containsString:allowedFamily] ) { return true; }
