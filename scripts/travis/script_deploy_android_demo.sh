12f4d010398e670488b983f8c840fe5a3a26eeae
Matt Blair <blair1618@gmail.com>
2017-05-15 18:23:25 -0400

    Edit Travis scripts to build 0.6.3 patch release

--- scripts/travis/script_deploy_android_demo.sh
+++ scripts/travis/script_deploy_android_demo.sh
@@ -6,7 +6,7 @@ set -o pipefail
 if [ "${PLATFORM}" = "android" ]; then
     printf "[default]\naccess_key = $S3_ACCESS_KEY\n secret_key = $S3_SECRET_KEY" > ~/.s3cfg
 
-    if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
+    if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "0.6.3-patch" ]; then
         url_latest="s3://android.mapzen.com/tangram-latest.apk"
         echo "Uploading latest build to $url_latest"
         s3cmd put platforms/android/demo/build/outputs/apk/demo-debug.apk $url_latest
--- scripts/travis/script_deploy_android_snapshot.sh
+++ scripts/travis/script_deploy_android_snapshot.sh
@@ -3,7 +3,7 @@
 set -e
 set -o pipefail
 
-if [ "${PLATFORM}" = "android" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
+if [ "${PLATFORM}" = "android" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "0.6.3-patch" ]; then
 
     # Configure private repository credentials (used to sign release artifacts)
     echo -e "machine github.com\n  login $GITHUB_USERNAME\n  password $GITHUB_PASSWORD" >> ~/.netrc
