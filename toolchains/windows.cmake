c12b4d1e05c9c0e6d7d28fee8f7b046b1b1f8786
KaiN <kain@piwnica.ws>
2017-07-14 17:07:22 +0200

    fixed strdup compile problem in harfbuzz

--- toolchains/windows.cmake
+++ toolchains/windows.cmake
@@ -1,5 +1,5 @@
 # global compile options
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++1y")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=gnu++1y")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wignored-qualifiers -Wtype-limits -Wmissing-field-initializers")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fno-omit-frame-pointer")
 
