eb39f9134f1af170090dcde7cb2e2c7ba1334307
KaiN <kain@piwnica.ws>
2017-07-14 15:10:43 +0200

    Added windows platform stub based on linux

---
+++ platforms/windows/src/windowsPlatform.cpp
@@ -0,0 +1,87 @@
+#include "windowsPlatform.h"
+#include "gl/hardware.h"
+#include "log.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <libgen.h>
+#include <unistd.h>
+#include <sys/resource.h>
+#include <sys/syscall.h>
+
+#if defined(TANGRAM_LINUX)
+#include <GLFW/glfw3.h>
+#elif defined(TANGRAM_RPI)
+#include "context.h"
+#endif
+
+#define DEFAULT "fonts/NotoSans-Regular.ttf"
+#define FONT_AR "fonts/NotoNaskh-Regular.ttf"
+#define FONT_HE "fonts/NotoSansHebrew-Regular.ttf"
+#define FONT_JA "fonts/DroidSansJapanese.ttf"
+#define FALLBACK "fonts/DroidSansFallback.ttf"
+
+namespace Tangram {
+
+void logMsg(const char* fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    vfprintf(stderr, fmt, args);
+    va_end(args);
+}
+
+WindowsPlatform::WindowsPlatform() :
+    m_urlClient(UrlClient::Options{}) {}
+
+WindowsPlatform::WindowsPlatform(UrlClient::Options urlClientOptions) :
+    m_urlClient(urlClientOptions) {}
+
+void WindowsPlatform::requestRender() const {
+#if defined(PLATFORM_LINUX)
+    glfwPostEmptyEvent();
+#elif defined(PLATFORM_RPI)
+    setRenderRequest(true);
+#endif
+}
+
+std::vector<FontSourceHandle> WindowsPlatform::systemFontFallbacksHandle() const {
+    std::vector<FontSourceHandle> handles;
+
+    handles.emplace_back(DEFAULT);
+    handles.emplace_back(FONT_AR);
+    handles.emplace_back(FONT_HE);
+    handles.emplace_back(FONT_JA);
+    handles.emplace_back(FALLBACK);
+
+    return handles;
+}
+
+bool WindowsPlatform::startUrlRequest(const std::string& _url, UrlCallback _callback) {
+
+    return m_urlClient.addRequest(_url, _callback);
+}
+
+void WindowsPlatform::cancelUrlRequest(const std::string& _url) {
+
+    m_urlClient.cancelRequest(_url);
+}
+
+WindowsPlatform::~WindowsPlatform() {}
+
+void setCurrentThreadPriority(int priority) {
+#if defined(TANGRAM_LINUX)
+    int tid = syscall(SYS_gettid);
+    setpriority(PRIO_PROCESS, tid, priority);
+#elif defined(TANGRAM_RPI)
+    // no-op
+#endif
+}
+
+void initGLExtensions() {
+#if defined(TANGRAM_LINUX)
+    Tangram::Hardware::supportsMapBuffer = true;
+#elif defined(TANGRAM_RPI)
+    // no-op
+#endif
+}
+
+} // namespace Tangram
---
+++ platforms/windows/src/windowsPlatform.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "platform.h"
+#include "urlClient.h"
+
+namespace Tangram {
+
+class WindowsPlatform : public Platform {
+
+public:
+
+    WindowsPlatform();
+    WindowsPlatform(UrlClient::Options urlClientOptions);
+    ~WindowsPlatform() override;
+    void requestRender() const override;
+    std::vector<FontSourceHandle> systemFontFallbacksHandle() const override;
+    bool startUrlRequest(const std::string& _url, UrlCallback _callback) override;
+    void cancelUrlRequest(const std::string& _url) override;
+
+protected:
+
+    UrlClient m_urlClient;
+
+};
+
+} // namespace Tangram
