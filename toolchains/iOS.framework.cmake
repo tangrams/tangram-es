15e61deb0bafd4c79eb7760b8866579629d5f818
Matt Blair <blair1618@gmail.com>
2017-07-14 15:38:56 -0700

    Prepare next development cycle

--- toolchains/iOS.framework.cmake
+++ toolchains/iOS.framework.cmake
@@ -1,6 +1,6 @@
 include(${CMAKE_SOURCE_DIR}/toolchains/iOS.toolchain.cmake)
 
-set(FRAMEWORK_VERSION "0.7.2")
+set(FRAMEWORK_VERSION "0.8.0-dev")
 
 message(STATUS "Build for iOS archs " ${IOS_ARCH})
 
