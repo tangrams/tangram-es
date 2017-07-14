b0684bda1d2946855c49e0d3cebd461ffab54636
KaiN <kain@piwnica.ws>
2017-07-14 15:16:07 +0200

    Removed linux-style ctrl+c handler

--- platforms/windows/src/main.cpp
+++ platforms/windows/src/main.cpp
@@ -26,9 +26,6 @@ int main(int argc, char* argv[]) {
     // Create the windowed app.
     GlfwApp::create(platform, sceneFile, 1024, 768);
 
-    // Give it a chance to shutdown cleanly on CTRL-C
-    signal(SIGINT, &GlfwApp::stop);
-
     // Loop until the user closes the window
     GlfwApp::run();
 
