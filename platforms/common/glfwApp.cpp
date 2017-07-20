7202a4539d8ed2e9bef0089cb96ce4692a8c63fa
Varun Talwar <tallytalwar@gmail.com>
2017-07-20 11:38:23 -0700

    Do not override scene file api key with an empty string if environment variable is not set

--- platforms/common/glfwApp.cpp
+++ platforms/common/glfwApp.cpp
@@ -80,8 +80,12 @@ void create(std::shared_ptr<Platform> p, std::string f, int w, int h) {
     // Setup tangram
     if (!map) {
         map = new Tangram::Map(platform);
-        map->loadSceneAsync(sceneFile.c_str(), true, {}, nullptr,
-                {SceneUpdate("global.sdk_mapzen_api_key", mapzenApiKey)});
+        if (mapzenApiKey.empty()) {
+            map->loadSceneAsync(sceneFile.c_str(), true, {}, nullptr, {});
+        } else {
+            map->loadSceneAsync(sceneFile.c_str(), true, {}, nullptr,
+                    {SceneUpdate("global.sdk_mapzen_api_key", mapzenApiKey)});
+        }
     }
 
     // Create a windowed mode window and its OpenGL context
