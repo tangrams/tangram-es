e92efa003ba3e5e978887df9349bb9a91957e1a7
Matt Blair <blair1618@gmail.com>
2017-07-20 19:58:08 -0700

    Configure opaque display surface for RPi demo

--- platforms/rpi/src/context.cpp
+++ platforms/rpi/src/context.cpp
@@ -251,6 +251,7 @@ void initGL(int argc, char **argv) {
     DISPMANX_ELEMENT_HANDLE_T dispman_element;
     DISPMANX_DISPLAY_HANDLE_T dispman_display;
     DISPMANX_UPDATE_HANDLE_T dispman_update;
+    VC_DISPMANX_ALPHA_T dispman_alpha;
     VC_RECT_T dst_rect;
     VC_RECT_T src_rect;
 
@@ -265,8 +266,10 @@ void initGL(int argc, char **argv) {
         EGL_DEPTH_SIZE, 16,
         EGL_SAMPLE_BUFFERS, 1,
         EGL_SAMPLES, 4,
+        EGL_CONFORMANT, EGL_OPENGL_ES2_BIT,
         EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
         EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
+        EGL_TRANSPARENT_TYPE, EGL_NONE,
         EGL_NONE
     };
 
@@ -322,12 +325,17 @@ void initGL(int argc, char **argv) {
     src_rect.width = viewport.z << 16;
     src_rect.height = viewport.w << 16;
 
+    // Configure the display layer to have full opacity.
+    dispman_alpha.flags = DISPMANX_FLAGS_ALPHA_FIXED_ALL_PIXELS;
+    dispman_alpha.opacity = 0xFF;
+    dispman_alpha.mask = NULL;
+
     dispman_display = vc_dispmanx_display_open( 0 /* LCD */);
     dispman_update = vc_dispmanx_update_start( 0 );
 
     dispman_element = vc_dispmanx_element_add( dispman_update, dispman_display,
                                        0/*layer*/, &dst_rect, 0/*src*/,
-                                       &src_rect, DISPMANX_PROTECTION_NONE, 0 /*alpha*/, 0/*clamp*/, (DISPMANX_TRANSFORM_T)0/*transform*/);
+                                       &src_rect, DISPMANX_PROTECTION_NONE, &dispman_alpha, 0/*clamp*/, (DISPMANX_TRANSFORM_T)0/*transform*/);
 
     nativeviewport.element = dispman_element;
     nativeviewport.width = viewport.z;
