891689a982e9682e2e244477fa0bff31d9b2c4ae
Matt Blair <blair1618@gmail.com>
2017-09-19 18:01:26 -0700

    Remove deprecated 'bundled font' code paths
    The use cases for this are covered by fallback fonts now.

--- core/src/text/fontContext.h
+++ core/src/text/fontContext.h
@@ -40,22 +40,14 @@ struct GlyphTexture {
 struct FontDescription {
     std::string uri;
     std::string alias;
-    std::string bundleAlias;
 
     FontDescription(std::string family, std::string style, std::string weight, std::string uri)
         : uri(uri) {
         alias = Alias(family, style, weight);
-        bundleAlias = BundleAlias(family, style, weight);
     }
 
     static std::string Alias(const std::string& family, const std::string& style, const std::string& weight) {
         return family + "_" + getNumericFontWeight(weight) + "_" + style;
-    }
-
-    static std::string BundleAlias(const std::string& family, const std::string& style, const std::string& weight) {
-        // TODO: support .woff on bundle fonts
-        std::string alias = family + "-" + getNumericFontWeight(weight) + style + ".ttf";
-        return alias;
     }
 
     static std::string getNumericFontWeight(const std::string& weight) {
