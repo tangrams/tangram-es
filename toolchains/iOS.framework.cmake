32e680e18470a863e46b810c543d01e307011d1d
Matt Blair <blair1618@gmail.com>
2017-09-28 13:29:28 -0700

    Release 0.8.1

--- toolchains/iOS.framework.cmake
+++ toolchains/iOS.framework.cmake
@@ -1,6 +1,6 @@
 include(${CMAKE_SOURCE_DIR}/toolchains/iOS.toolchain.cmake)
 
-set(FRAMEWORK_VERSION "0.8.1-dev")
+set(FRAMEWORK_VERSION "0.8.1")
 
 message(STATUS "Build for iOS archs " ${IOS_ARCH})
 
