f98ca713cd6540780012451e2f0199fb76aeb2d0
Varun Talwar <tallytalwar@gmail.com>
2017-10-11 11:35:37 -0700

    Include new urdu font which has better typeface support for urdu language
    - comes with ios 11

--- platforms/common/appleAllowedFonts.mm
+++ platforms/common/appleAllowedFonts.mm
@@ -3,7 +3,7 @@
 bool allowedFamily(NSString* familyName) {
     const NSArray<NSString *> *allowedFamilyList = @[ @"Hebrew", @"Kohinoor", @"Gumurki", @"Thonburi", @"Tamil",
                                                     @"Gurmukhi", @"Kailasa", @"Sangam", @"PingFang", @"Geeza",
-                                                    @"Mishafi", @"Farah", @"Hiragino", @"Gothic" ];
+                                                    @"Mishafi", @"Farah", @"Hiragino", @"Gothic", @"Nastaliq" ];
 
     for (NSString* allowedFamily in allowedFamilyList) {
         if ( [familyName containsString:allowedFamily] ) { return true; }
