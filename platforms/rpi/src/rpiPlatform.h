86f317d5fbad1035e43ec8334520ce95190e4f0e
Varun Talwar <tallytalwar@gmail.com>
2017-10-23 14:25:23 -0700

    Rpi platform uses system fonts for drawng fallbacks
    - uses the linux system font helper methods
    - updates the rpi platform build to use fontconfig system library

--- platforms/rpi/src/rpiPlatform.h
+++ platforms/rpi/src/rpiPlatform.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <fontconfig/fontconfig.h>
+
 #include "platform.h"
 #include "urlClient.h"
 
@@ -16,9 +18,12 @@ public:
     std::vector<FontSourceHandle> systemFontFallbacksHandle() const override;
     UrlRequestHandle startUrlRequest(Url _url, UrlCallback _callback) override;
     void cancelUrlRequest(UrlRequestHandle _url) override;
+    FontSourceHandle systemFont(const std::string& _name, const std::string& _weight,
+            const std::string& _face) const override;
 
 protected:
 
+    FcConfig* m_fcConfig = nullptr;
     UrlClient m_urlClient;
 
 };
