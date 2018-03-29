fc869a9b059aceb9839289a30098f33ddbec6032
Matt Blair <blair1618@gmail.com>
2018-03-29 15:01:02 -0700

    Android snapshot deploy uses artifactoryUpload

--- scripts/travis/script_deploy_android_snapshot.sh
+++ scripts/travis/script_deploy_android_snapshot.sh
@@ -5,13 +5,9 @@ set -o pipefail
 
 if [ "${PLATFORM}" = "android" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
 
-    # Configure private repository credentials (used to sign release artifacts)
-    echo -e "machine github.com\n  login $GITHUB_USERNAME\n  password $GITHUB_PASSWORD" >> ~/.netrc
-
-    make android-sdk
-
     cd "$TRAVIS_BUILD_DIR"/platforms/android
-    ./gradlew bintrayUpload -bintray.user="$BINTRAY_USERNAME" -bintray.apikey="$BINTRAY_APIKEY" -bintray.gpg.password="$BINTRAY_PASSWORD"
+
+    ./gradlew artifactoryUpload -PbuildNumber=${TRAVIS_BUILD_NUMBER}
 
     cd "$TRAVIS_BUILD_DIR"
 fi
