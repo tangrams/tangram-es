05ee80c12f9b08c2fcaf43d3fbd759db9b87dc28
Matt Blair <blair1618@gmail.com>
2018-03-29 15:20:42 -0700

    drop: deploy android snapshots on every branch

--- scripts/travis/script_deploy_android_snapshot.sh
+++ scripts/travis/script_deploy_android_snapshot.sh
@@ -3,8 +3,8 @@
 set -e
 set -o pipefail
 
-if [ "${PLATFORM}" = "android" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
-
+# if [ "${PLATFORM}" = "android" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
+if [ "${PLATFORM}" = "android" ]; then
     cd "$TRAVIS_BUILD_DIR"/platforms/android
 
     ./gradlew artifactoryUpload -PbuildNumber=${TRAVIS_BUILD_NUMBER}
