c68843db09fc3f688781d40520576d3422ea535f
Matt Blair <blair1618@gmail.com>
2018-04-06 11:50:31 -0400

    Android builds are promoted to bintray when commit has a tag

--- scripts/travis/script_deploy_android_snapshot.sh
+++ scripts/travis/script_deploy_android_snapshot.sh
@@ -7,7 +7,13 @@ set -o pipefail
 if [ "${PLATFORM}" = "android" ]; then
     cd "$TRAVIS_BUILD_DIR"/platforms/android
 
-    ./gradlew artifactoryPublish -PbuildNumber=${TRAVIS_BUILD_NUMBER}
+    ./gradlew artifactoryPublish -PbuildName=tangram-android -PbuildNumber=${TRAVIS_BUILD_NUMBER}
+
+    # If the git tag for the build is not empty then promote the build to bintray.
+    if [ -n "${TRAVIS_TAG}" ]; then
+        # Bintray REST call: https://www.jfrog.com/confluence/display/RTF/Deploying+Snapshots+to+oss.jfrog.org#DeployingSnapshotstooss.jfrog.org-PromotingaReleaseBuild
+        curl -X POST -u ${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD} https://oss.jfrog.org/api/plugins/build/promote/snapshotsToBintray/tangram-android/${TRAVIS_BUILD_NUMBER}
+    fi
 
     cd "$TRAVIS_BUILD_DIR"
 fi
