3fbfb97c9b8d74d5b8f16afd061bfef0368bfb9d
Matt Blair <blair1618@gmail.com>
2018-04-09 22:13:15 -0400

    Remove signing steps from Android deployment
    Not required for jcenter hosting

--- scripts/travis/script_deploy_android_snapshot.sh
+++ scripts/travis/script_deploy_android_snapshot.sh
@@ -5,13 +5,9 @@ set -o pipefail
 
 if [ "${PLATFORM}" = "android" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
 
-    # Configure private repository credentials (used to sign release artifacts)
-    echo -e "machine github.com\n  login $GITHUB_USERNAME\n  password $GITHUB_PASSWORD" >> ~/.netrc
-
-    make android-sdk
-
     cd "$TRAVIS_BUILD_DIR"/platforms/android
-    git clone https://github.com/mapzen/android-config.git
-    ./gradlew uploadArchives -PsonatypeUsername="$SONATYPE_USERNAME" -PsonatypePassword="$SONATYPE_PASSWORD" -Psigning.keyId="$SIGNING_KEY_ID" -Psigning.password="$SIGNING_PASSWORD" -Psigning.secretKeyRingFile="$SIGNING_SECRET_KEY_RING_FILE"
+
+    ./gradlew uploadArchives
+
     cd "$TRAVIS_BUILD_DIR"
 fi
