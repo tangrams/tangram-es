46f9758c333076c08ba0361591d1a4703f9046cd
Hannes Janetzek <hannes.janetzek@gmail.com>
2018-04-12 18:25:32 +0200

    Fix build for gcc 7.3 (#1780)

--- core/deps/glm/glm/simd/platform.h
+++ core/deps/glm/glm/simd/platform.h
@@ -283,6 +283,8 @@
 #		define GLM_COMPILER (GLM_COMPILER_GCC71)
 #	elif (__GNUC__ == 7) && (__GNUC_MINOR__ == 2)
 #		define GLM_COMPILER (GLM_COMPILER_GCC72)
+#	elif (__GNUC__ == 7) && (__GNUC_MINOR__ == 3)
+#		define GLM_COMPILER (GLM_COMPILER_GCC72)
 #	elif (__GNUC__ >= 8)
 #		define GLM_COMPILER (GLM_COMPILER_GCC80)
 #	else
