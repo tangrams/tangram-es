c50466905abd09f85cfc24df40b463968286e418
Hannes Janetzek <hannes.janetzek@gmail.com>
2018-07-09 14:45:33 +0200

    fixup: prevent recursion in callback

--- platforms/android/tangram/src/main/java/com/mapzen/tangram/MapController.java
+++ platforms/android/tangram/src/main/java/com/mapzen/tangram/MapController.java
@@ -469,8 +469,11 @@ public class MapController implements Renderer {
         checkPointer(mapPointer);
 
         if (cameraAnimationCallback != null) {
-            cameraAnimationCallback.onCancel();
+            // NB: Prevent recursion loop when setCameraPositionEased is called from onCancel callback
+            CameraAnimationCallback prev = cameraAnimationCallback;
             cameraAnimationCallback = null;
+            prev.onCancel();
+
         }
         cameraAnimationCallback = cb;
 
