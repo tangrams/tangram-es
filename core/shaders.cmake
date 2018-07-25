694e536bf4f58cfb9f1b615415d6bd963137d147
Hannes Janetzek <hannes.janetzek@gmail.com>
2018-07-25 19:28:34 +0200

    squash: safer raw string escape

--- core/shaders.cmake
+++ core/shaders.cmake
@@ -3,4 +3,4 @@ file(READ "${in}" file_content)
 string(REGEX REPLACE "([\n][ \t]*(//)[^\n]*)" "" content "${file_content}")
 # drop empty lines
 string(REGEX REPLACE "[\n]+" "\n" content "${content}")
-file(WRITE "${out}" "const static char* ${name} = R\"(\n${content})\";")
+file(WRITE "${out}" "const static char* ${name} = R\"RAW_GLSL(\n${content})RAW_GLSL\";")
