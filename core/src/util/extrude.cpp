d585ec63c2ef8d656b24030bddf2edd7c3c5cb88
Matt Blair <blair1618@gmail.com>
2018-10-08 14:27:05 -0400

    Clean up Extrude parsing

--- core/src/util/extrude.cpp
+++ core/src/util/extrude.cpp
@@ -1,71 +1,16 @@
 #include "util/extrude.h"
 
-#include "util/floatFormatter.h"
-
-#include "yaml-cpp/node/node.h"
-#include "yaml-cpp/node/convert.h"
+#include "util/yamlUtil.h"
 #include <cmath>
-#include <cstdlib>
 
 namespace Tangram {
-
-Extrude parseExtrudeString(const std::string& _str) {
-
-    // Values specified from the stylesheet are assumed to be meters with no unit suffix
-    float first = 0, second = 0;
-
-    if (_str == "true") {
-        // "true" means use default properties for both heights, we indicate this with NANs
-        return Extrude(NAN, NAN);
-    }
-
-    if (_str == "false") {
-        // "false" means perform no extrusion
-        return Extrude(0, 0);
-    }
-
-    // Parse the first of two possible numbers
-    const char* str = _str.c_str();
-
-    // Get a float if possible & advance pos to the end of the number
-    const int length = _str.length();
-    int count = 0;
-    int offset = 0;
-
-    first = ff::stof(str, length, &count);
-    if (count == 0) {
-        // No numbers found, return zero extrusion
-        return Extrude(0, 0);
-    }
-
-    // Advance and skip the delimiter character
-    offset += count;
-    if (length - offset <= 0) {
-        return Extrude(0, first);
-    }
-    while (str[offset] == ' ') { offset++; }
-    if (str[offset++] != ',') {
-        return Extrude(0, first);
-    }
-
-    // Get a float if possible & advance pos to the end of the number
-    second = ff::stof(str + offset, length - offset, &count);
-    if (count == 0) {
-        // No second number, so return an extrusion from 0 to the first number
-        return Extrude(0, first);
-    }
-
-    // Got two numbers, so return an extrusion from first to second
-    return Extrude(first, second);
-
-}
 
 Extrude parseExtrudeNode(const YAML::Node& node) {
     // Values specified from the stylesheet are assumed to be meters with no unit suffix
     float first = 0, second = 0;
 
     bool extrudeBoolean = false;
-    if (YAML::convert<bool>::decode(node, extrudeBoolean)) {
+    if (YamlUtil::getBool(node, extrudeBoolean)) {
         if (extrudeBoolean) {
             // "true" means use default properties for both heights, we indicate this with NANs
             return Extrude(NAN, NAN);
@@ -74,16 +19,16 @@ Extrude parseExtrudeNode(const YAML::Node& node) {
         return Extrude(0, 0);
     }
 
-    if (YAML::convert<float>::decode(node, first)) {
-        // No second number, so return an extrusion from 0 to the first number
-        return Extrude(0, first);
-    }
-
     if (node.IsSequence() && node.size() >= 2) {
-        if (YAML::convert<float>::decode(node[0], first) && YAML::convert<float>::decode(node[1], second)) {
+        if (YamlUtil::getFloat(node[0], first) && YamlUtil::getFloat(node[1], second)) {
             // Got two numbers, so return an extrusion from first to second
             return Extrude(first, second);
         }
+    }
+
+    if (YamlUtil::getFloat(node, first)) {
+        // No second number, so return an extrusion from 0 to the first number
+        return Extrude(0, first);
     }
 
     // No numbers found, return zero extrusion
