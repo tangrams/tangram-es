f06a55ebc4a477d1c612283a5f2d5d5bb1bbbc23
Matt Blair <blair1618@gmail.com>
2018-10-30 18:32:47 -0400

    Force Ninja on CircleCI to only use 4 concurrent jobs

--- platforms/android/config.cmake
+++ platforms/android/config.cmake
@@ -1,5 +1,11 @@
 add_definitions(-DTANGRAM_ANDROID)
 
+if (CIRCLECI)
+  # Force Ninja on CircleCI to use only 4 concurrent jobs.
+  # Otherwise it guesses concurrency based on the physical CPU (which has a lot of cores!) and runs out of memory.
+  set(CMAKE_MAKE_PROGRAM "${CMAKE_MAKE_PROGRAM} -j 4")
+endif(CIRCLECI)
+
 add_library(tangram SHARED
   platforms/common/platform_gl.cpp
   platforms/android/tangram/src/main/cpp/jniExports.cpp
