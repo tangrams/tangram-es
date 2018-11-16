5f012b3fe0498e36ed8b22c4fa8638524166e27b
Hannes Janetzek <hannes.janetzek@gmail.com>
2018-11-16 16:27:03 +0100

    Add Platform::shutdown() to stop workers before going to delete Map

--- platforms/linux/src/linuxPlatform.h
+++ platforms/linux/src/linuxPlatform.h
@@ -8,25 +8,22 @@
 namespace Tangram {
 
 class LinuxPlatform : public Platform {
-
 public:
-
     LinuxPlatform();
-    LinuxPlatform(UrlClient::Options urlClientOptions);
+    explicit LinuxPlatform(UrlClient::Options urlClientOptions);
     ~LinuxPlatform() override;
+    void shutdown() override;
     void requestRender() const override;
     std::vector<FontSourceHandle> systemFontFallbacksHandle() const override;
     FontSourceHandle systemFont(const std::string& _name, const std::string& _weight,
-            const std::string& _face) const override;
+                                const std::string& _face) const override;
     UrlRequestHandle startUrlRequest(Url _url, UrlCallback _callback) override;
     void cancelUrlRequest(UrlRequestHandle _request) override;
 
 protected:
-
-
     FcConfig* m_fcConfig = nullptr;
-    UrlClient m_urlClient;
-
+    std::unique_ptr<UrlClient> m_urlClient;
+    bool m_shutdown = false;
 };
 
 } // namespace Tangram
