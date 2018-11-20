234c6e37581afb84f93c62cafaecb57f50f78589
Hannes Janetzek <hannes.janetzek@gmail.com>
2018-11-20 17:40:37 +0100

    Add benchmark template
    - Copy and replace 'Template' to quickly write your benchmarks

---
+++ bench/src/template.cpp
@@ -0,0 +1,27 @@
+#include "benchmark/benchmark_api.h"
+#include "benchmark/benchmark.h"
+
+
+using namespace Tangram;
+
+class TemplateFixture : public benchmark::Fixture {
+public:
+    // std::unique_ptr<Template> ctx;
+
+    void SetUp() override {
+        ctx = std::make_unique<TestContext>();
+    }
+    void TearDown() override {
+        ctx.reset();
+    }
+};
+
+BENCHMARK_DEFINE_F(TemplateFixture, TemplateMethod)(benchmark::State& st) {
+    while (st.KeepRunning()) {
+        ctx.doSuff();
+    }
+}
+
+BENCHMARK_REGISTER_F(TemplateFixture, TemplateMethod);
+
+BENCHMARK_MAIN();
