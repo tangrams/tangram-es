f7eb8421b49a33a2464ed9d3ff60a3d3e5a7cb78
Hannes Janetzek <hannes.janetzek@gmail.com>
2018-11-21 15:07:07 +0100

    Move stb_image implementation to separate file
    - Faster texture.cpp recompile

--- core/src/gl/texture.cpp
+++ core/src/gl/texture.cpp
@@ -8,13 +8,6 @@
 #include "platform.h"
 #include "util/geom.h"
 
-// Enable only JPEG, PNG, GIF, TGA and PSD
-#define STBI_NO_BMP
-#define STBI_NO_HDR
-#define STBI_NO_PIC
-#define STBI_NO_PNM
-
-#define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
 #include <cassert>
