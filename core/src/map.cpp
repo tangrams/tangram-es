8e5ef86d6da4ac2fd9b3d16a20ef3d908e032de3
Hannes Janetzek <hannes.janetzek@gmail.com>
2018-11-21 14:17:25 +0100

    Timing from scene load to complete view

--- core/src/map.cpp
+++ core/src/map.cpp
@@ -184,6 +184,7 @@ void Map::Impl::setScene(std::shared_ptr<Scene>& _scene) {
 SceneID Map::loadScene(std::shared_ptr<Scene> scene,
                        const std::vector<SceneUpdate>& _sceneUpdates) {
 
+    LOGTOInit();
     {
         std::unique_lock<std::mutex> lock(impl->sceneMutex);
 
@@ -250,6 +251,7 @@ SceneID Map::loadSceneYamlAsync(const std::string& _yaml, const std::string& _re
 SceneID Map::loadSceneAsync(std::shared_ptr<Scene> nextScene,
                             const std::vector<SceneUpdate>& _sceneUpdates) {
 
+    LOGTOInit();
     impl->sceneLoadBegin();
 
     runAsyncTask([nextScene, _sceneUpdates, this](){
@@ -392,6 +394,8 @@ bool Map::update(float _dt) {
     // Wait until font and texture resources are fully loaded
     if (impl->scene->pendingFonts > 0 || impl->scene->pendingTextures > 0) {
         platform->requestRender();
+        LOGTO("Waiting for Scene fonts:%d textures:%d",
+              impl->scene->pendingFonts.load(), impl->scene->pendingTextures.load());
         return false;
     }
 
@@ -458,7 +462,7 @@ bool Map::update(float _dt) {
     bool tilesLoading = impl->tileManager.hasLoadingTiles();
     bool labelsNeedUpdate = impl->labels.needUpdate();
 
-    if (viewChanged || tilesChanged || tilesLoading || labelsNeedUpdate || impl->sceneLoadTasks > 0) {
+    if (viewChanged || tilesLoading || labelsNeedUpdate || impl->sceneLoadTasks > 0) {
         viewComplete = false;
     }
 
@@ -466,6 +470,10 @@ bool Map::update(float _dt) {
     if (impl->isCameraEasing || labelsNeedUpdate || markersNeedUpdate) {
         platform->requestRender();
     }
+
+    LOGTO("View complete:%d vc:%d tl:%d tc:%d easing:%d label:%d maker:%d ",
+          viewComplete, viewChanged, tilesLoading, tilesChanged,
+          impl->isCameraEasing, labelsNeedUpdate, markersNeedUpdate);
 
     return viewComplete;
 }
