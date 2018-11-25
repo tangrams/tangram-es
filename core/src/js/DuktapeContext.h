2e63032cc890703f82655c68384ef46f2417ae43
Hannes Janetzek <hannes.janetzek@gmail.com>
2018-11-25 19:50:18 +0100

    Duktape optimizations pt1
    - cache ptr to Tangram::Value for the last requested properties

--- core/src/js/DuktapeContext.h
+++ core/src/js/DuktapeContext.h
@@ -16,6 +16,8 @@ namespace Duktape {
 const static char INSTANCE_ID[] = "\xff""\xff""obj";
 const static char FUNC_ID[] = "\xff""\xff""fns";
 
+struct Context;
+
 struct Value {
 
     duk_context* _ctx = nullptr;
@@ -77,7 +79,13 @@ struct Value {
     }
 
     std::string toString() {
-        return std::string(duk_to_string(_ctx, _index));
+        std::string result;
+        size_t len;
+        // NB this requires the value to be a string. Rename to getString()?
+        if (const char* data = duk_get_lstring(_ctx, _index, &len)) {
+            if (len > 0) { result = std::string(data, len); }
+        }
+        return result;
     }
 
     size_t getLength() {
@@ -115,20 +123,29 @@ struct Value {
 };
 
 
+static Context* getContext(duk_context* _ctx) {
+    duk_memory_functions funcs;
+    duk_get_memory_functions(_ctx, &funcs);
+    return reinterpret_cast<Context*>(funcs.udata);
+}
+
 struct Context {
 
     duk_context* _ctx = nullptr;
     const Feature* _feature = nullptr;
 
-    Value getStackTopValue() {
-        return {_ctx, duk_normalize_index(_ctx, -1)};
+    std::vector<std::pair<JSFunctionIndex, void*>> m_functions;
+
+    ~Context() {
+        duk_destroy_heap(_ctx);
+        //printf("gets:%d reused:%d - %f%%\n", fetchCnt+_reuseCnt, _reuseCnt, float(_reuseCnt) / float(fetchCnt + _reuseCnt) * 100 );
     }
 
     Context() {
         // Create duktape heap with default allocation functions and
         // custom fatal error handler.
-        _ctx = duk_create_heap(nullptr, nullptr, nullptr, nullptr,
-                               fatalErrorHandler);
+        _ctx = duk_create_heap(nullptr, nullptr, nullptr,
+                               this, fatalErrorHandler);
 
         //// Create global geometry constants
         // TODO make immutable
@@ -180,10 +197,6 @@ struct Context {
         }
     }
 
-    ~Context() {
-        duk_destroy_heap(_ctx);
-    }
-
     void setGlobalValue(const std::string& name, Value value) {
         value.ensureExistsOnStackTop();
         duk_put_global_lstring(_ctx, name.data(), name.length());
@@ -191,6 +204,39 @@ struct Context {
 
     void setCurrentFeature(const Feature* feature) {
         _feature = feature;
+        _lastFeature = nullptr;
+    }
+
+    Value getStackTopValue() {
+        return {_ctx, duk_normalize_index(_ctx, -1)};
+    }
+
+    JSScopeMarker getScopeMarker() {
+        return duk_get_top(_ctx);
+    }
+
+    void resetToScopeMarker(JSScopeMarker marker) {
+        duk_set_top(_ctx, marker);
+    }
+
+    bool evaluateBooleanFunction(JSFunctionIndex index) {
+        if (!evaluateFunction(index)) {
+            return false;
+        }
+        // Evaluate the "truthiness" of the function result at the top
+        //  of the stack.
+        bool result = duk_to_boolean(_ctx, -1) != 0;
+
+        // pop result
+        duk_pop(_ctx);
+        return result;
+    }
+
+    Value getFunctionResult(JSFunctionIndex index) {
+        if (!evaluateFunction(index)) {
+            return {nullptr, 0};
+        }
+        return getStackTopValue();
     }
 
     bool setFunction(JSFunctionIndex index, const std::string& fn) {
@@ -205,7 +251,17 @@ struct Context {
         duk_push_string(_ctx, "");
 
         if (duk_pcompile(_ctx, DUK_COMPILE_FUNCTION) == 0) {
+            void* fnPtr = duk_get_heapptr(_ctx, -1);
+            if (m_functions.size() == index) {
+                m_functions.emplace_back(index, fnPtr);
+            } else {
+                m_functions.resize(index+1);
+                m_functions[index] = {index, fnPtr};
+            }
+            // Store function in global.functions to make sure it will not be
+            // garbage collected.
             duk_put_prop_index(_ctx, -2, index);
+
         } else {
             LOGW("Compile failed: %s\n%s\n---",
                  duk_safe_to_string(_ctx, -1), fn.c_str());
@@ -218,24 +274,29 @@ struct Context {
         return true;
     }
 
-    bool evaluateBooleanFunction(uint32_t index) {
-        if (!evaluateFunction(index)) {
+    bool evaluateFunction(JSFunctionIndex index) {
+
+        if (m_functions.size() <= index) {
+            LOGN("Functions array not initialized. index:%d size:%d",
+                 index, m_functions.size());
             return false;
         }
-        // Evaluate the "truthiness" of the function result at the top
-        //  of the stack.
-        bool result = duk_to_boolean(_ctx, -1) != 0;
-
-        // pop result
-        duk_pop(_ctx);
-        return result;
-    }
-
-    Value getFunctionResult(uint32_t index) {
-        if (!evaluateFunction(index)) {
-            return {nullptr, 0};
+        if (m_functions[index].second == nullptr) {
+            LOGN("Function not set. index:%d size:%d",
+                 index, m_functions.size());
+            return false;
         }
-        return getStackTopValue();
+
+        duk_push_heapptr(_ctx, m_functions[index].second);
+
+        if (duk_pcall(_ctx, 0) != 0) {
+            LOGN("Error: %s, function:%d feaure:%p",
+                 duk_safe_to_string(_ctx, -1), index, _feature);
+
+            duk_pop(_ctx);
+            return false;
+        }
+        return true;
     }
 
     Value newNull() {
@@ -280,93 +341,95 @@ struct Context {
         return getStackTopValue();
     }
 
-    JSScopeMarker getScopeMarker() {
-        return duk_get_top(_ctx);
-    }
+private:
 
-    void resetToScopeMarker(JSScopeMarker marker) {
-        duk_set_top(_ctx, marker);
-    }
+    // Cache Feature property indexes
+    const Feature* _lastFeature = nullptr;
+    using prop_key = const char*;
+    using prop_val = const Tangram::Value*;
+    std::array<std::pair<prop_key, prop_val>, 16> _propertyCache {};
+    uint32_t _propertyCacheUse = 0;
 
-   // Implements Proxy handler.has(target_object, key)
-   static int jsHasProperty(duk_context *_ctx) {
+    int _reuseCnt = 0;
+    int fetchCnt = 0;
 
-        duk_get_prop_string(_ctx, 0, INSTANCE_ID);
-        auto context = static_cast<const Context*>(duk_to_pointer(_ctx, -1));
-        if (!context || !context->_feature) {
-            LOGE("Error: no context set %p %p", context,
-                 context ? context->_feature : nullptr);
-            duk_pop(_ctx);
-            return 0;
-        }
+    static const Tangram::Value* getProperty(Context* context) {
+        // Get the requested object key
+        const char* key = duk_require_string(context->_ctx, 1);
 
-        const char* key = duk_require_string(_ctx, 1);
-        auto result = static_cast<duk_bool_t>(context->_feature->props.contains(key));
-        duk_push_boolean(_ctx, result);
-
-        return 1;
-    }
-
-   // Implements Proxy handler.get(target_object, key)
-   static int jsGetProperty(duk_context *_ctx) {
-
-        // Get the JavaScriptContext instance from JS Feature object
-        //  (first parameter).
-        duk_get_prop_string(_ctx, 0, INSTANCE_ID);
-        auto context = static_cast<const Context*>(duk_to_pointer(_ctx, -1));
-        if (!context || !context->_feature) {
-            LOGE("Error: no context set %p %p",  context,
-                 context ? context->_feature : nullptr);
-            duk_pop(_ctx);
-            return 0;
-        }
-
-        // Get the property name (second parameter)
-        const char* key = duk_require_string(_ctx, 1);
-
-        auto it = context->_feature->props.get(key);
-        if (it.is<std::string>()) {
-            duk_push_string(_ctx, it.get<std::string>().c_str());
-        } else if (it.is<double>()) {
-            duk_push_number(_ctx, it.get<double>());
+        if (context->_lastFeature == context->_feature) {
+            auto used = context->_propertyCacheUse;
+            auto cache = context->_propertyCache;
+            for (auto i = 0; i < used; i++) {
+                if (cache[i].first == key) {
+                    context->_reuseCnt++;
+                    return cache[i].second;
+                }
+            }
         } else {
-            duk_push_undefined(_ctx);
+            context->_propertyCacheUse = 0;
         }
-        // FIXME: Distinguish Booleans here as well
-        return 1;
+
+        context->fetchCnt++;
+        context->_lastFeature = context->_feature;
+        auto val = &context->_feature->props.get(key);
+
+        auto use = context->_propertyCacheUse;
+        if (use < context->_propertyCache.size()) {
+            context->_propertyCache[use] = {key, val};
+            context->_propertyCacheUse++;
+        }
+        return val;
     }
 
-    static void fatalErrorHandler(void*, const char* message) {
+    // Implements Proxy handler.has(target_object, key)
+    static int jsHasProperty(duk_context *_ctx) {
+
+        auto context = getContext(_ctx);
+
+        if (context && context->_feature) {
+            bool hasProp = !(getProperty(context)->is<none_type>());
+
+            duk_push_boolean(_ctx, static_cast<duk_bool_t>(hasProp));
+            return 1;
+        }
+        LOGN("Error: no context found for %p / %p %p",  _ctx, context,
+             context ? context->_feature : nullptr);
+        return 0;
+    }
+
+    // Implements Proxy handler.get(target_object, key)
+    static int jsGetProperty(duk_context *_ctx) {
+
+        auto context = getContext(_ctx);
+
+        if (context && context->_feature) {
+            // Get the property name (second parameter)
+            const Tangram::Value* val = getProperty(context);
+
+            if (val->is<std::string>()) {
+                const auto& str = val->get<std::string>();
+
+                duk_push_lstring(_ctx, str.c_str(), str.length());
+
+            } else if (val->is<double>()) {
+                duk_push_number(_ctx, val->get<double>());
+            } else {
+                duk_push_undefined(_ctx);
+            }
+            // FIXME: Distinguish Booleans here as well
+            return 1;
+        }
+        LOGN("Error: no context found for %p / %p %p",  _ctx, context,
+             context ? context->_feature : nullptr);
+        return 0;
+    }
+
+    static void fatalErrorHandler(void* udata, const char* message) {
+        //Context* context = reinterpret_cast<Context>(udata);
+
         LOGE("Fatal Error in DuktapeJavaScriptContext: %s", message);
         abort();
-    }
-
-    bool evaluateFunction(uint32_t index) {
-        // Get all functions (array) in context
-        if (!duk_get_global_string(_ctx, FUNC_ID)) {
-            LOGE("EvalFilterFn - functions array not initialized");
-            duk_pop(_ctx); // pop [undefined] sitting at stack top
-            return false;
-        }
-        // Get function at index `id` from functions array, put it at stack top
-        if (!duk_get_prop_index(_ctx, -1, index)) {
-            LOGE("EvalFilterFn - function %d not set", index);
-            duk_pop(_ctx); // pop "undefined" sitting at stack top
-            duk_pop(_ctx); // pop functions (array) now sitting at stack top
-            return false;
-        }
-        // pop fns array
-        duk_remove(_ctx, -2);
-
-        // call popped function (sitting at stack top), evaluated value is put
-        // on stack top
-        if (duk_pcall(_ctx, 0) != 0) {
-            LOGE("EvalFilterFn: %s", duk_safe_to_string(_ctx, -1));
-            duk_pop(_ctx);
-            return false;
-        }
-
-        return true;
     }
 };
 
