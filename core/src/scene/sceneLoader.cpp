1fe5fa6abdcdd7e40328b1647d6ddec4b93b5407
Varun Talwar <tallytalwar@gmail.com>
2018-12-10 12:12:24 -0800

    Disable tile loading for datasources with layer disabled
    Fixes #1973

--- core/src/scene/sceneLoader.cpp
+++ core/src/scene/sceneLoader.cpp
@@ -1768,12 +1768,14 @@ void SceneLoader::loadLayer(const std::pair<Node, Node>& layer, const std::share
     std::string source;
     std::vector<std::string> collections;
 
+    auto sublayer = loadSublayer(layer.second, name, scene);
+
     if (Node data = layer.second["data"]) {
         if (Node data_source = data["source"]) {
             if (data_source.IsScalar()) {
                 source = data_source.Scalar();
                 auto dataSource = scene->getTileSource(source);
-                if (dataSource) {
+                if (dataSource && sublayer.enabled()) {
                     dataSource->generateGeometry(true);
                 } else {
                     LOGW("Can't find data source %s for layer %s", source.c_str(), name.c_str());
@@ -1799,7 +1801,6 @@ void SceneLoader::loadLayer(const std::pair<Node, Node>& layer, const std::share
         collections.push_back(name);
     }
 
-    auto sublayer = loadSublayer(layer.second, name, scene);
 
     scene->layers().push_back({ std::move(sublayer), source, collections });
 }
