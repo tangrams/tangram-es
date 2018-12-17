0e7f97d68d05706724d9363fec7e7ab99020606a
Hannes Janetzek <hannes.janetzek@gmail.com>
2018-12-17 23:20:33 +0100

    Fix zero ease duration (#1983)
    * Fix: TGMapView.mm - don't ease on 0 duration
    
    * Ease: Divide by zero safety
    
    * Float conversion warnings
    
    * Use setCameraPositionEased for setCameraPosition:withDuration:

--- core/src/util/ease.h
+++ core/src/util/ease.h
@@ -33,8 +33,13 @@ struct Ease {
     bool finished() const { return t >= d; }
 
     void update(float _dt) {
-        t = t < 0 ? 0 : std::fmin(t + _dt, d);
-        cb(std::fmin(1, t / d));
+        if (d > 0.f) {
+            t = t < 0.f ? 0.f : std::fmin(t + _dt, d);
+            cb(std::fmin(1.f, t / d));
+        } else {
+            t = d;
+            cb(1.f);
+        }
     }
 
 };
