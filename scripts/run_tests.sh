fdd2a95a689d0d9280636504eb4dfca0ee580b31
Matt Blair <blair1618@gmail.com>
2019-10-06 13:52:12 -0700

    Publish Catch test results on CircleCI (#2093)
    * Publish Catch test results on CircleCI
    
    * Add test result directory parameter to run_tests.sh

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eo pipefail
 # USAGE
-#   run_tests.sh [test_build_dir]
+#   run_tests.sh [test_build_dir] [test_results_dir]
 #
 build_dir="build/tests"
 if [[ $1 ]]; then
@@ -8,9 +8,17 @@ if [[ $1 ]]; then
 fi
 echo "Running unit tests from: ${build_dir}"
 pushd ${build_dir}
+if [[ $2 ]]; then
+    results_dir=$2
+    mkdir -p ${results_dir}
+fi
 for file in tests/*.out
     do
         echo "Running ${file}"
-        $file
+        if [[ ${results_dir} ]]; then
+            $file -r junit -o ${results_dir}/$(basename ${file}).xml
+        else
+            $file
+        fi
     done
 popd
