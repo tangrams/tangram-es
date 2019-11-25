e84793e68c1df9405964ed9c5d7dc5f006b53194
Matt Blair <blair1618@gmail.com>
2019-11-25 01:16:07 -0500

    Release 0.11.1

--- core/include/tangram/tangram.h
+++ core/include/tangram/tangram.h
@@ -7,7 +7,7 @@
 
 #define TANGRAM_VERSION_MAJOR 0
 #define TANGRAM_VERSION_MINOR 11
-#define TANGRAM_VERSION_PATCH 0
+#define TANGRAM_VERSION_PATCH 1
 
 #include "data/clientDataSource.h"
 #include "data/properties.h"
