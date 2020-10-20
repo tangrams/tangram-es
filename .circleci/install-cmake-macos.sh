ba9b2896d157ff3b76238e5496eba3e0a4f26bd5
Matt Blair <blair1618@gmail.com>
2020-10-20 19:26:15 -0400

    Fixup CMake install script

--- .circleci/install-cmake-macos.sh
+++ .circleci/install-cmake-macos.sh
@@ -1,11 +1,13 @@
+#!/bin/bash -eo pipefail
+set -x
 # Get version number from shell variable or a default.
 CMAKE_VERSION=${CMAKE_VERSION-3.18.4}
 # Download and unpack the CMake release.
-curl --location https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-Darwin-x86_64.tar.gz | tar -xz
+curl --location https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-Darwin-x86_64.tar.gz | tar -xz -C ~
 # Create a link for the CMake binaries on the user $PATH.
-for file in cmake-${CMAKE_VERSION}-Darwin-x86_64/CMake.app/Contents/bin/*
+for file in ~/cmake-${CMAKE_VERSION}-Darwin-x86_64/CMake.app/Contents/bin/*
 do
 	ln -s ${file} /usr/local/bin/$(basename ${file})
 done
 # Check that the shell finds the CMake binary.
-./cmake --version
+cmake --version
