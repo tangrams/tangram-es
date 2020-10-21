8f7566071c104a287332578c0f281bc4a2a6d7ba
Matt Blair <blair1618@gmail.com>
2020-10-21 02:04:35 -0400

    Harfbuzz libraries update (#2191)
    * Update harfbuzz-icu-freetype with latest library versions
    
    * Add more location presets for checking text in GlfwApp
    
    * Revise and document Apple fallback font list
    
    * Store iOS CMake log on CI
    
    * Store iOS CMake errors on CI
    
    * Update harfbuzz-icu-freetype submodule with build fixes
    
    * Revert "Store iOS CMake errors on CI" and "Store iOS CMake log on CI"
    
    More practical to debug these issues by accessing the build container with SSH
    
    * Remove references to UCDN
    
    No longer needed separately from harfbuzz

--- platforms/common/appleAllowedFonts.mm
+++ platforms/common/appleAllowedFonts.mm
@@ -3,9 +3,29 @@
 namespace Tangram {
 
 bool allowedFamily(NSString* familyName) {
-    const NSArray<NSString *> *allowedFamilyList = @[ @"Hebrew", @"Kohinoor", @"Gumurki", @"Thonburi", @"Tamil",
-                                                    @"Gurmukhi", @"Kailasa", @"Sangam", @"PingFang", @"Geeza",
-                                                    @"Mishafi", @"Farah", @"Hiragino", @"Gothic", @"Nastaliq" ];
+    // macOS and iOS have many pre-installed fonts, supporting many scripts.
+    // Evaluating all fonts to find glyphs is slow, so limit the set of fonts loaded to one family per script.
+    // Apple provides a list of pre-installed fonts on the latest versions of macOS and iOS. These lists may not
+    // reflect the fonts available on earlier OS versions, so check multiple versions when possible.
+    // https://developer.apple.com/fonts/system-fonts/#preinstalled
+    const NSArray<NSString *> *allowedFamilyList =
+            @[ @"Apple SD Gothic Neo", // Hangul
+               @"Arial Hebrew", // Hebrew
+               @"Geeza Pro", // Arabic
+               @"Gurmukhi", // Gurmukhi
+               @"Hiragino", // Hiragana, Katakana
+               @"Kailasa", // Tibetan
+               @"Kannada Sangam", // Kannada
+               @"Kohinoor", // Bangla, Devanagari, Telugu
+               @"Malayalam Sangam", // Malayalam
+               @"Mishafi", // Arabic, Najdi Arabic
+               @"Myanmar Sangam", // Myanmar
+               @"Noto Nastaliq Urdu", // Arabic, Urdu
+               @"Oriya Sangam", // Odia
+               @"PingFang", // Hiragana, Katakana, Simplified Han, Traditional Han
+               @"Tamil", // Tamil
+               @"Thonburi", // Cyrillic, Thai
+            ];
 
     for (NSString* allowedFamily in allowedFamilyList) {
         if ( [familyName containsString:allowedFamily] ) { return true; }
