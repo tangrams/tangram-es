845420c1233cfcf3b570793508f8562c269dc3fd
Mathis Logemann <mathisloge@gmail.com>
2021-03-23 20:40:07 +0100

    impl paradigm

--- platforms/magnum/include/tangram/platform_magnum.hpp
+++ platforms/magnum/include/tangram/platform_magnum.hpp
@@ -1,36 +1,13 @@
 #pragma once
 #include "map.h"
 #include "platform.h"
-#include <Magnum/GL/Framebuffer.h>
-#include <Magnum/GL/Renderbuffer.h>
 #include <Magnum/GL/Texture.h>
 #include <memory>
 #include <tangram_export.h>
 
 namespace Tangram {
 
-class UrlClient;
-
 void TANGRAM_EXPORT setContext(Magnum::GL::Context& ctx);
-
-class PlatformMagnum : public Platform {
-public:
-    explicit PlatformMagnum(uint32_t maxActiveTasks = 20, uint32_t connectionTimeoutMs = 3000,
-                            uint32_t requestTimeoutMs = 30000);
-    ~PlatformMagnum() override;
-    void shutdown() override;
-    void requestRender() const override;
-
-    bool startUrlRequestImpl(const Url& _url, const UrlRequestHandle _request, UrlRequestId& _id) override;
-    void cancelUrlRequestImpl(const UrlRequestId _id) override;
-
-    bool isDirty() const;
-    void setDirty(bool dirty);
-
-private:
-    std::unique_ptr<UrlClient> url_client_;
-    mutable bool needs_render_;
-};
 
 class TANGRAM_EXPORT MagnumTexture {
 public:
@@ -39,27 +16,14 @@ public:
     Magnum::GL::Texture2D& texture();
     void render(const double time);
 
+    ~MagnumTexture();
+
 private:
     void loadSceneFile(bool setPosition = false, const std::vector<SceneUpdate>& updates = {});
-    void createBuffers();
-    void create(uint32_t maxActiveTasks, uint32_t connectionTimeoutMs, uint32_t requestTimeoutMs);
 
 private:
-    std::string apiKey;
-    std::string sceneYaml;
-    std::string sceneFile;
-    std::vector<SceneUpdate> sceneUpdates;
-    std::unique_ptr<Tangram::Map> map_;
-    double last_time_;
-    bool need_scene_reload_;
-
-    int msaa_level_;
-    Magnum::Vector2i scene_size_;
-    Magnum::GL::Framebuffer framebuffer_;
-    Magnum::GL::Framebuffer renderbuffer_;
-    Magnum::GL::Renderbuffer color_;
-    Magnum::GL::Renderbuffer depth_stencil_;
-    Magnum::GL::Texture2D render_texture_;
+    class Impl;
+    Impl* impl_;
 };
 
 
--- platforms/magnum/src/platform_magnum.cpp
+++ platforms/magnum/src/platform_magnum.cpp
@@ -1,9 +1,13 @@
 #include "platform_magnum.hpp"
+#include "platform_magnum_priv.hpp"
+
 #include "gl/hardware.h"
 #include "log.h"
 #include "urlClient.h"
 #include <Magnum/GL/Context.h>
 #include <Magnum/GL/DefaultFramebuffer.h>
+#include <Magnum/GL/Framebuffer.h>
+#include <Magnum/GL/Renderbuffer.h>
 #include <Magnum/GL/RenderbufferFormat.h>
 #include <Magnum/GL/TextureFormat.h>
 #include <dirent.h>
@@ -56,132 +60,162 @@ bool PlatformMagnum::isDirty() const { return needs_render_; }
 void PlatformMagnum::setDirty(bool dirty) { needs_render_ = dirty; }
 
 /////////// MagnumTexture
+
+class MagnumTexture::Impl {
+public:
+    Impl(uint32_t maxActiveTasks, uint32_t connectionTimeoutMs, uint32_t requestTimeoutMs)
+        : msaa_level_{4}, scene_size_{500, 500}, framebuffer_{{{}, scene_size_}}, renderbuffer_{{{}, scene_size_}},
+          need_scene_reload_{false} {
+        create(maxActiveTasks, connectionTimeoutMs, requestTimeoutMs);
+    }
+
+private:
+    void create(uint32_t maxActiveTasks, uint32_t connectionTimeoutMs, uint32_t requestTimeoutMs) {
+        char* nextzenApiKeyEnvVar = getenv("NEXTZEN_API_KEY");
+        if (nextzenApiKeyEnvVar && strlen(nextzenApiKeyEnvVar) > 0) {
+            apiKey = nextzenApiKeyEnvVar;
+        } else {
+            LOGW("No API key found!\n\nNextzen data sources require an API key. "
+                 "Sign up for a key at https://developers.nextzen.org/about.html and then set it from the command line "
+                 "with: "
+                 "\n\n\texport NEXTZEN_API_KEY=YOUR_KEY_HERE"
+                 "\n\nOr, if using an IDE on macOS, with: "
+                 "\n\n\tlaunchctl setenv NEXTZEN_API_KEY YOUR_API_KEY\n");
+        }
+
+        const char* apiKeyScenePath = "global.sdk_api_key";
+
+        if (!apiKey.empty()) { sceneUpdates.push_back(SceneUpdate(apiKeyScenePath, apiKey)); }
+
+        createBuffers();
+
+        sceneFile = "scene.yaml";
+        {
+            Url baseUrl("file:///");
+            LOG("curr URL: %s", std::filesystem::current_path().generic_string().c_str());
+            baseUrl = baseUrl.resolve(Url(std::filesystem::current_path().generic_string() + "/"));
+
+            LOG("Base URL: %s", baseUrl.string().c_str());
+
+            Url sceneUrl = baseUrl.resolve(Url(sceneFile));
+            sceneFile = sceneUrl.string();
+            LOG("Scene URL: %s", sceneFile.c_str());
+        }
+
+        sceneFile = "file://D:/dev/tangram-test/build/scene.yaml";
+
+
+        map_ = std::make_unique<Tangram::Map>(
+            std::make_unique<PlatformMagnum>(maxActiveTasks, connectionTimeoutMs, requestTimeoutMs));
+
+
+        map_->setupGL();
+
+        need_scene_reload_ = true;
+    }
+
+    void createBuffers() {
+        using namespace Magnum;
+        framebuffer_ = GL::Framebuffer{{{}, scene_size_}};
+        renderbuffer_ = GL::Framebuffer{{{}, scene_size_}};
+
+        render_texture_.setStorage(1, GL::TextureFormat::RGBA8, scene_size_);
+        renderbuffer_.attachTexture(GL::Framebuffer::ColorAttachment{0}, render_texture_, 0);
+
+        color_.setStorageMultisample(msaa_level_, GL::RenderbufferFormat::RGBA8, scene_size_);
+        depth_stencil_.setStorageMultisample(msaa_level_, GL::RenderbufferFormat::Depth24Stencil8, scene_size_);
+
+        framebuffer_.attachRenderbuffer(GL::Framebuffer::ColorAttachment{0}, color_);
+        framebuffer_.attachRenderbuffer(GL::Framebuffer::BufferAttachment::DepthStencil, depth_stencil_);
+    }
+
+private:
+    friend MagnumTexture;
+    std::string apiKey;
+    std::string sceneYaml;
+    std::string sceneFile;
+    std::vector<SceneUpdate> sceneUpdates;
+    std::unique_ptr<Tangram::Map> map_;
+    double last_time_;
+    bool need_scene_reload_;
+
+    int msaa_level_;
+    Magnum::Vector2i scene_size_;
+    Magnum::GL::Framebuffer framebuffer_;
+    Magnum::GL::Framebuffer renderbuffer_;
+    Magnum::GL::Renderbuffer color_;
+    Magnum::GL::Renderbuffer depth_stencil_;
+    Magnum::GL::Texture2D render_texture_;
+};
+
+
 MagnumTexture::MagnumTexture(uint32_t maxActiveTasks, uint32_t connectionTimeoutMs, uint32_t requestTimeoutMs)
-    : msaa_level_{4}, scene_size_{500, 500}, framebuffer_{{{}, scene_size_}}, renderbuffer_{{{}, scene_size_}},
-      need_scene_reload_{false} {
-    create(maxActiveTasks, connectionTimeoutMs, requestTimeoutMs);
-}
+    : impl_{new Impl{maxActiveTasks, connectionTimeoutMs, requestTimeoutMs}} {}
 
 void MagnumTexture::render(const double time) {
     loadSceneFile();
 
-    auto& platform = static_cast<PlatformMagnum&>(map_->getPlatform());
+    auto& platform = static_cast<PlatformMagnum&>(impl_->map_->getPlatform());
     if (platform.isDirty()) {
 
 
         platform.setDirty(false);
-        const double delta = time - last_time_;
-        last_time_ = time;
+        const double delta = time - impl_->last_time_;
+        impl_->last_time_ = time;
 
-        MapState state = map_->update(static_cast<float>(delta));
+        MapState state = impl_->map_->update(static_cast<float>(delta));
 
 
-        framebuffer_.clear(Magnum::GL::FramebufferClear::Color | Magnum::GL::FramebufferClear::Depth).bind();
+        impl_->framebuffer_.clear(Magnum::GL::FramebufferClear::Color | Magnum::GL::FramebufferClear::Depth).bind();
 
         Magnum::GL::Context::current().resetState(Magnum::GL::Context::State::EnterExternal);
-        map_->render();
+        impl_->map_->render();
         Magnum::GL::Context::current().resetState(Magnum::GL::Context::State::ExitExternal);
 
-        renderbuffer_.clear(Magnum::GL::FramebufferClear::Color).bind();
-        Magnum::GL::Framebuffer::blit(framebuffer_, renderbuffer_, {{}, scene_size_},
+        impl_->renderbuffer_.clear(Magnum::GL::FramebufferClear::Color).bind();
+        Magnum::GL::Framebuffer::blit(impl_->framebuffer_, impl_->renderbuffer_, {{}, impl_->scene_size_},
                                       Magnum::GL::FramebufferBlit::Color);
 
         Magnum::GL::defaultFramebuffer.bind();
     }
 }
 
-Magnum::GL::Texture2D& MagnumTexture::texture() { return render_texture_; }
+Magnum::GL::Texture2D& MagnumTexture::texture() { return impl_->render_texture_; }
 
-void MagnumTexture::create(uint32_t maxActiveTasks, uint32_t connectionTimeoutMs, uint32_t requestTimeoutMs) {
-    char* nextzenApiKeyEnvVar = getenv("NEXTZEN_API_KEY");
-    if (nextzenApiKeyEnvVar && strlen(nextzenApiKeyEnvVar) > 0) {
-        apiKey = nextzenApiKeyEnvVar;
-    } else {
-        LOGW("No API key found!\n\nNextzen data sources require an API key. "
-             "Sign up for a key at https://developers.nextzen.org/about.html and then set it from the command line "
-             "with: "
-             "\n\n\texport NEXTZEN_API_KEY=YOUR_KEY_HERE"
-             "\n\nOr, if using an IDE on macOS, with: "
-             "\n\n\tlaunchctl setenv NEXTZEN_API_KEY YOUR_API_KEY\n");
-    }
-
-    const char* apiKeyScenePath = "global.sdk_api_key";
-
-    if (!apiKey.empty()) { sceneUpdates.push_back(SceneUpdate(apiKeyScenePath, apiKey)); }
-
-    createBuffers();
-
-    sceneFile = "scene.yaml";
-    {
-        Url baseUrl("file:///");
-        LOG("curr URL: %s", std::filesystem::current_path().generic_string().c_str());
-        baseUrl = baseUrl.resolve(Url(std::filesystem::current_path().generic_string() + "/"));
-
-        LOG("Base URL: %s", baseUrl.string().c_str());
-
-        Url sceneUrl = baseUrl.resolve(Url(sceneFile));
-        sceneFile = sceneUrl.string();
-        LOG("Scene URL: %s", sceneFile.c_str());
-    }
-
-    sceneFile = "file://D:/dev/tangram-test/build/scene.yaml";
-
-
-    map_ = std::make_unique<Tangram::Map>(
-        std::make_unique<PlatformMagnum>(maxActiveTasks, connectionTimeoutMs, requestTimeoutMs));
-
-
-    map_->setupGL();
-
-    need_scene_reload_ = true;
-}
-
-void MagnumTexture::createBuffers() {
-    using namespace Magnum;
-    framebuffer_ = GL::Framebuffer{{{}, scene_size_}};
-    renderbuffer_ = GL::Framebuffer{{{}, scene_size_}};
-
-    render_texture_.setStorage(1, GL::TextureFormat::RGBA8, scene_size_);
-    renderbuffer_.attachTexture(GL::Framebuffer::ColorAttachment{0}, render_texture_, 0);
-
-    color_.setStorageMultisample(msaa_level_, GL::RenderbufferFormat::RGBA8, scene_size_);
-    depth_stencil_.setStorageMultisample(msaa_level_, GL::RenderbufferFormat::Depth24Stencil8, scene_size_);
-
-    framebuffer_.attachRenderbuffer(GL::Framebuffer::ColorAttachment{0}, color_);
-    framebuffer_.attachRenderbuffer(GL::Framebuffer::BufferAttachment::DepthStencil, depth_stencil_);
-}
 
 void MagnumTexture::loadSceneFile(bool setPosition, const std::vector<SceneUpdate>& updates) {
     for (auto& update : updates) {
         bool found = false;
-        for (auto& prev : sceneUpdates) {
+        for (auto& prev : impl_->sceneUpdates) {
             if (update.path == prev.path) {
                 prev = update;
                 found = true;
                 break;
             }
         }
-        if (!found) { sceneUpdates.push_back(update); }
+        if (!found) { impl_->sceneUpdates.push_back(update); }
     }
 
-    if (need_scene_reload_) {
-        need_scene_reload_ = false;
+    if (impl_->need_scene_reload_) {
+        impl_->need_scene_reload_ = false;
         bool load_async = false;
         bool setPosition = true;
         if (load_async) {
-            if (!sceneYaml.empty()) {
-                map_->loadSceneYamlAsync(sceneYaml, sceneFile, setPosition, sceneUpdates);
+            if (!impl_->sceneYaml.empty()) {
+                impl_->map_->loadSceneYamlAsync(impl_->sceneYaml, impl_->sceneFile, setPosition, impl_->sceneUpdates);
             } else {
-                map_->loadSceneAsync(sceneFile, setPosition, sceneUpdates);
+                impl_->map_->loadSceneAsync(impl_->sceneFile, setPosition, impl_->sceneUpdates);
             }
         } else {
-            if (!sceneYaml.empty()) {
-                map_->loadSceneYaml(sceneYaml, sceneFile, setPosition, sceneUpdates);
+            if (!impl_->sceneYaml.empty()) {
+                impl_->map_->loadSceneYaml(impl_->sceneYaml, impl_->sceneFile, setPosition, impl_->sceneUpdates);
             } else {
-                map_->loadScene(sceneFile, setPosition, sceneUpdates);
+                impl_->map_->loadScene(impl_->sceneFile, setPosition, impl_->sceneUpdates);
             }
         }
     }
 }
+
+MagnumTexture::~MagnumTexture() { delete impl_; }
 
 } // namespace Tangram
