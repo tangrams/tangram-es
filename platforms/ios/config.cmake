ebd78d8fed3bddc03c2fc965846c76523f47536f
Matt Blair <blair1618@gmail.com>
2021-04-19 03:52:59 -0400

    Release 0.16.1

--- platforms/ios/config.cmake
+++ platforms/ios/config.cmake
@@ -1,6 +1,6 @@
 add_definitions(-DTANGRAM_IOS)
 
-set(TANGRAM_FRAMEWORK_VERSION "0.16.1-dev")
+set(TANGRAM_FRAMEWORK_VERSION "0.16.1")
 set(TANGRAM_BUNDLE_IDENTIFIER "com.mapzen.TangramMap")
 
 ### Configure iOS toolchain.
