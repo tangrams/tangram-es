c467127ab930e5214f25d3e51842a2cc655fd11d
Matt Blair <blair1618@gmail.com>
2021-07-04 15:30:57 -0400

    Add default user agent string to requests in UrlClient

--- platforms/common/urlClient.cpp
+++ platforms/common/urlClient.cpp
@@ -63,6 +63,7 @@ struct UrlClient::Task {
         curl_easy_setopt(handle, CURLOPT_FOLLOWLOCATION, 1);
         curl_easy_setopt(handle, CURLOPT_MAXREDIRS, 20);
         curl_easy_setopt(handle, CURLOPT_TCP_NODELAY, 1);
+        curl_easy_setopt(handle, CURLOPT_USERAGENT, _options.userAgentString);
     }
 
     void setup() {
