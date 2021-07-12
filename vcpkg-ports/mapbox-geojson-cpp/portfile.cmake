c063d95df1b76c9352ae7bf835373240b02088f7
Mathis Logemann <mathisloge@gmail.com>
2021-07-12 23:47:09 +0200

    functional

--- vcpkg-ports/mapbox-geojson-cpp/portfile.cmake
+++ vcpkg-ports/mapbox-geojson-cpp/portfile.cmake
@@ -6,6 +6,8 @@ vcpkg_from_github(
     REF v0.3.2
     SHA512 4bda8d68f6425dd11b3ee4e2b39b55b9993abbab859519430046d0a131a1074de27f0449c5b852802b30214accebef4d8087c5e1f4a79ecc70871f05a3111016
     HEAD_REF master
+    PATCHES 
+        tangram.patch
 )
 
 # Copy header files
