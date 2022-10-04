f41051a045bbe9f7db750a4c5299826ef4487187
Joel Winarske <joel.winarske@gmail.com>
2022-10-04 14:00:52 -0700

    Wayland Support
    -enable Wayland using -DTANGRAM_USE_WAYLAND=ON -DTANGRAM_USE_X11=OFF
    
    Signed-off-by: Joel Winarske <joel.winarske@gmail.com>

--- cmake/glfw.cmake
+++ cmake/glfw.cmake
@@ -8,6 +8,11 @@ else()
   set(GLFW_BUILD_TESTS OFF CACHE BOOL "Build the GLFW test programs")
   set(GLFW_BUILD_DOCS OFF CACHE BOOL "Build the GLFW documentation")
   set(GLFW_INSTALL OFF CACHE BOOL "Generate installation target")
+  # Select Windowing subsystem
+  option(TANGRAM_USE_WAYLAND "Use Wayland for Linux windowing system" OFF)
+  option(TANGRAM_USE_X11 "Use x11 for Linux windowing system" ON)
+  set(GLFW_BUILD_WAYLAND ${TANGRAM_USE_WAYLAND})
+  set(GLFW_BUILD_X11 ${TANGRAM_USE_X11})
   add_subdirectory(platforms/common/glfw)
   if(APPLE)
     # Turn off noisy warnings from clang on macOS.
